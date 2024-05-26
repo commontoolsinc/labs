e4bb2cedb35f6aa093a499b6a77f53f6e7e8f6fc
Chris Joel <0xcda7a@gmail.com>
2024-05-25 23:27:00 -0700

    chore: Remove old example projects

--- rust/example-crate/src/bin/cowsay.rs
+++
@@ -1,6 +0,0 @@
-use example_crate::{Cows, Cowsayer};
-
-pub fn main() {
-    println!("{}", Cowsayer::say("moo".into(), None));
-    println!("{}", Cowsayer::say("hoo".into(), Some(Cows::Owl)));
-}
--- typescript/packages/example-package/vite.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  build: {
-    target: 'esnext'
-  },
-  resolve: {
-    preserveSymlinks: true
-  }
-};
