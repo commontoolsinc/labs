f48ff566ba85ebce74b794565bbc3f9c0f900707
Ben Follington <5009316+bfollington@users.noreply.github.com>
2024-05-30 15:51:43 -0700

    Appending messages to conversation

--- sketches/2024-05-30-lookslike-prototype/src/components/com-app.ts
+++ sketches/2024-05-30-lookslike-prototype/src/components/com-app.ts
@@ -1,5 +1,5 @@
 import { LitElement, html } from 'lit-element'
-import { customElement, property } from 'lit/decorators.js'
+import { customElement, state } from 'lit/decorators.js'
 import { base } from '../styles'
 
 import { todoAppMockup } from '../data'
@@ -8,17 +8,36 @@ import { todoAppMockup } from '../data'
 export class ComApp extends LitElement {
   static styles = [base]
 
+  @state() graph = todoAppMockup as any
 
+  appendMessage() {
+    const newGraph = { ...this.graph }
+    const id = 'new' + (Math.floor(Math.random() * 1000))
+
+    newGraph.nodes.push({
+      id,
+      messages: [
+        {
+          role: 'user',
+          content: 'new message'
+        }
+      ],
+      definition: {}
+    });
+
+    newGraph.order.push(id);
+    this.graph = newGraph;
+  }
 
   render() {
     return html`
       <com-app-grid>
         <com-chat slot="main">
-            <com-thread slot="main" .graph=${todoAppMockup}></com-thread>
+            <com-thread slot="main" .graph=${this.graph}></com-thread>
             <div slot="footer">
                 <com-unibox>
                     <com-editor slot="main"></com-editor>
-                    <com-button slot="end">Send</com-button>
+                    <com-button slot="end" .action=${() => this.appendMessage()}>Send</com-button>
                 </com-unibox>
             </div>
         </com-chat>
