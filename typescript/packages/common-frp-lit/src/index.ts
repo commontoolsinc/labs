662ad7dbb11702582bf1109c4d1adbf9a349db69
Gordon Brander <gordonbrander@users.noreply.github.com>
2024-06-07 15:47:06 -0700

    Remove watch signal
    ...we'll need to think more carefully about how this should work in the
    world where Signals are not automatically cleaned up.

--- typescript/packages/common-frp-lit/src/index.ts
+++ typescript/packages/common-frp-lit/src/index.ts
@@ -1,32 +1,22 @@
 import { signal } from "@commontools/common-frp";
-import {directive} from 'lit/directive.js';
-import {AsyncDirective} from 'lit/async-directive.js';
+import { directive } from 'lit/directive.js';
+import { AsyncDirective } from 'lit/async-directive.js';
 
-const {state, effect} = signal;
+const { effect } = signal;
 
 class WatchDirective extends AsyncDirective {
-  #isWatching
+  #cancel: (() => void) | undefined = undefined;
 
   constructor(part: any) {
     super(part);
-    this.#isWatching = state(true);
   }
 
   override render(signal: any) {
-    effect(this.#isWatching, isWatching => {
-      if (isWatching) {
-        this.setValue(signal.get());
-      }
+    this.#cancel?.();
+    this.#cancel = effect(signal, value => {
+      this.setValue(value);
     });
     return signal.get();
-  }
-
-  protected override disconnected(): void {
-    this.#isWatching.send(false)
-  }
-
-  protected override reconnected(): void {
-    this.#isWatching.send(true)
   }
 }
 
