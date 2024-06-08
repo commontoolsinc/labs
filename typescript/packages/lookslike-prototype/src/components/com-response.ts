15be3f5bc575cad1ac8f63c22c69a93f0372e4d9
Ben Follington <5009316+bfollington@users.noreply.github.com>
2024-06-07 20:26:00 -0700

    Basic formatter config
    I am using this, no-one else has to use this

--- typescript/packages/lookslike-prototype/src/components/com-response.ts
+++ typescript/packages/lookslike-prototype/src/components/com-response.ts
@@ -1,118 +1,131 @@
-import { LitElement, html, css } from 'lit-element'
-import { customElement, property, state } from 'lit/decorators.js'
-import { base } from '../styles'
-import { unsafeHTML } from 'lit/directives/unsafe-html.js'
-import pretty from 'pretty'
-import { RecipeNode } from '../data.js'
-import { SignalSubject } from '../../../common-frp/lib/signal.js'
-import { signal } from '@commontools/common-frp'
+import { LitElement, html, css } from "lit-element";
+import { customElement, property, state } from "lit/decorators.js";
+import { base } from "../styles";
+import { unsafeHTML } from "lit/directives/unsafe-html.js";
+import pretty from "pretty";
+import { RecipeNode } from "../data.js";
+import { SignalSubject } from "../../../common-frp/lib/signal.js";
+import { signal } from "@commontools/common-frp";
 
-function definitionToHtml(node: RecipeNode, value: any, dispatch: (event: CustomEvent) => void) {
+function definitionToHtml(
+  node: RecipeNode,
+  value: any,
+  dispatch: (event: CustomEvent) => void
+) {
   if (!node) {
-    return html`<pre>loading...</pre>`
+    return html`<pre>loading...</pre>`;
   }
 
-  if (node.contentType === 'text/javascript') {
+  if (node.contentType === "text/javascript") {
     const codeChanged = (ev) => {
-      node.body = ev.detail.code
-      const event = new CustomEvent('updated', {
+      node.body = ev.detail.code;
+      const event = new CustomEvent("updated", {
         detail: {
           body: node.body
         }
       });
       dispatch(event);
-    }
+    };
 
     const dataChanged = (ev) => {
-      const event = new CustomEvent('overriden', {
+      const event = new CustomEvent("overriden", {
         detail: {
           data: ev.detail.data
         }
       });
       dispatch(event);
-    }
+    };
 
     return html`
       <com-code .code=${node.body} @updated=${codeChanged}></com-code>
-      <com-data .data=${JSON.stringify(value, null, 2)} @updated=${dataChanged}></com-data>
-    `
+      <com-data
+        .data=${JSON.stringify(value, null, 2)}
+        @updated=${dataChanged}
+      ></com-data>
+    `;
   }
 
-  if (node.contentType === 'application/json+vnd.common.ui') {
+  if (node.contentType === "application/json+vnd.common.ui") {
     if (!value) {
-      return html`<pre>loading...</pre>`
+      return html`<pre>loading...</pre>`;
     }
     // const el = createElement(node.body, context || {})
-    const sourceHtml = value.outerHTML
+    const sourceHtml = value.outerHTML;
 
-    console.log('sourceHtml', sourceHtml)
+    console.log("sourceHtml", sourceHtml);
     return html`<div>${unsafeHTML(sourceHtml)}</div>
       <com-toggle>
-      <com-data .data=${pretty(sourceHtml)}></com-data>
-      <com-data .data=${JSON.stringify(node.body, null, 2)}></com-data>
-      </com-toggle>`
+        <com-data .data=${pretty(sourceHtml)}></com-data>
+        <com-data .data=${JSON.stringify(node.body, null, 2)}></com-data>
+      </com-toggle>`;
   }
 
-  if (node.contentType === 'application/json') {
+  if (node.contentType === "application/json") {
     return html`
       <com-data .data=${node.body}></com-data>
       <com-data .data=${JSON.stringify(value, null, 2)}></com-data>
-    `
+    `;
   }
 
-  return html`<pre>${JSON.stringify(node, null, 2)}</pre>`
+  return html`<pre>${JSON.stringify(node, null, 2)}</pre>`;
 }
 
 const styles = css`
-:host {
-  display: block;
-}
-
-.main {
-  background-color: var(--color-secondary-background);
-  padding: var(--gap);
-
-  &:focus {
-      outline: none;
+  :host {
+    display: block;
   }
-}
-`
 
-@customElement('com-response')
+  .main {
+    background-color: var(--color-secondary-background);
+    padding: var(--gap);
+
+    &:focus {
+      outline: none;
+    }
+  }
+`;
+
+@customElement("com-response")
 export class ComResponse extends LitElement {
-  static styles = [base, styles]
+  static styles = [base, styles];
 
-  @property({ type: Object }) node: RecipeNode | null
-  @property({ type: Object }) output: SignalSubject<any> = signal.state(null)
-  onCancel: () => void = () => { }
-  @state() value: any = {}
-  cancel: () => void = () => { }
+  @property({ type: Object }) node: RecipeNode | null;
+  @property({ type: Object }) output: SignalSubject<any> = signal.state(null);
+  onCancel: () => void = () => {};
+  @state() value: any = {};
+  cancel: () => void = () => {};
 
-  override willUpdate(changedProperties: Map<string | number | symbol, unknown>) {
-    if (changedProperties.has('output')) {
-      console.log('output changed', this.node?.id, this.output)
+  override willUpdate(
+    changedProperties: Map<string | number | symbol, unknown>
+  ) {
+    if (changedProperties.has("output")) {
+      console.log("output changed", this.node?.id, this.output);
       // trigger a re-render if any output changes
-      this.cancel = signal.effect(this.output, value => {
+      this.cancel = signal.effect(this.output, (value) => {
         // if (!value) return
-        this.value = value
-        console.log('updated value', this.node?.id, value)
-      })
+        this.value = value;
+        console.log("updated value", this.node?.id, value);
+      });
     }
   }
 
   override render() {
     if (!this.node) {
-      return html`<pre>loading...</pre>`
+      return html`<pre>loading...</pre>`;
     }
 
-    const defintion = definitionToHtml(this.node, this.value, this.dispatchEvent.bind(this))
-    console.log('value', this.node.id, this.value)
+    const defintion = definitionToHtml(
+      this.node,
+      this.value,
+      this.dispatchEvent.bind(this)
+    );
+    console.log("value", this.node.id, this.value);
 
     return html`
-    <div class="main">
-      ${defintion}
-      <slot></slot>
-    </div>
-    `
+      <div class="main">
+        ${defintion}
+        <slot></slot>
+      </div>
+    `;
   }
 }
--- typescript/packages/lookslike-prototype/src/llm.ts
+++ typescript/packages/lookslike-prototype/src/llm.ts
@@ -1,232 +1,245 @@
-import Instructor from "@instructor-ai/instructor";
-import OpenAI from "openai";
-import { fetchApiKey } from "./apiKey";
-import { ChatCompletionMessage, ChatCompletionMessageParam, ChatCompletionTool } from "openai/resources";
+import Instructor from '@instructor-ai/instructor';
+import OpenAI from 'openai';
+import { fetchApiKey } from './apiKey.js';
+import {
+  ChatCompletionMessage,
+  ChatCompletionMessageParam,
+  ChatCompletionTool
+} from 'openai/resources';
 
-const apiKey = fetchApiKey() as string
+const apiKey = fetchApiKey() as string;
 
 const openai = new OpenAI({
   apiKey: apiKey,
-  dangerouslyAllowBrowser: true,
+  dangerouslyAllowBrowser: true
 });
 
 const toolSpec: ChatCompletionTool[] = [
   {
-    "type": "function",
-    "function": {
-      "name": "listNodes",
-      "description": "Lists all nodes in the graph.",
-      "parameters": {}
+    type: 'function',
+    function: {
+      name: 'listNodes',
+      description: 'Lists all nodes in the graph.',
+      parameters: {}
     }
   },
   {
-    "type": "function",
-    "function": {
-      "name": "addCodeNode",
-      "description": "Adds a new code node to the graph written in javascript.",
-      "parameters": {
-        "type": "object",
-        "properties": {
-          "id": { "type": "string" },
-          "node": {
-            "type": "object",
-            "properties": {
-              "in": {
-                "type": "object",
+    type: 'function',
+    function: {
+      name: 'addCodeNode',
+      description: 'Adds a new code node to the graph written in javascript.',
+      parameters: {
+        type: 'object',
+        properties: {
+          id: { type: 'string' },
+          node: {
+            type: 'object',
+            properties: {
+              in: {
+                type: 'object'
               },
-              "outputType": {
-                "type": "object"
-              },
+              outputType: {
+                type: 'object'
+              }
             }
           },
-          "code": { "type": "string" }
+          code: { type: 'string' }
         }
       }
     }
   },
   {
-    "type": "function",
-    "function": {
-      "name": "addUiNode",
-      "description": "Adds a new ui node to the graph written using a hyperscript style.",
-      "parameters": {
-        "type": "object",
-        "properties": {
-          "id": { "type": "string" },
-          "node": {
-            "type": "object",
-            "properties": {
-              "in": {
-                "type": "object",
+    type: 'function',
+    function: {
+      name: 'addUiNode',
+      description:
+        'Adds a new ui node to the graph written using a hyperscript style.',
+      parameters: {
+        type: 'object',
+        properties: {
+          id: { type: 'string' },
+          node: {
+            type: 'object',
+            properties: {
+              in: {
+                type: 'object'
               },
-              "outputType": {
-                "type": "object"
-              },
+              outputType: {
+                type: 'object'
+              }
             }
           },
-          "body": { "type": "object" }
+          body: { type: 'object' }
         }
       }
     }
   },
   {
-    "type": "function",
-    "function": {
-      "name": "addFetchNode",
-      "description": "Adds a new fetch node to the graph to retrieve (GET) data from the web.",
-      "parameters": {
-        "type": "object",
-        "properties": {
-          "id": { "type": "string" },
-          "url": { "type": "string" }
+    type: 'function',
+    function: {
+      name: 'addFetchNode',
+      description:
+        'Adds a new fetch node to the graph to retrieve (GET) data from the web.',
+      parameters: {
+        type: 'object',
+        properties: {
+          id: { type: 'string' },
+          url: { type: 'string' }
         }
       }
     }
   },
   {
-    "type": "function",
-    "function": {
-      "name": "addMusicSearchNode",
-      "description": `Adds a new fetch node to the graph to search last.fm.
+    type: 'function',
+    function: {
+      name: 'addMusicSearchNode',
+      description: `Adds a new fetch node to the graph to search last.fm.
 
       Results are stored in result.albumsmatches.album
       `,
-      "parameters": {
-        "type": "object",
-        "properties": {
-          "id": { "type": "string" },
-          "query": { "type": "string" }
+      parameters: {
+        type: 'object',
+        properties: {
+          id: { type: 'string' },
+          query: { type: 'string' }
         }
       }
     }
   },
   {
-    "type": "function",
-    "function": {
-      "name": "replaceNode",
-      "description": "Replaces an existing node in the graph.",
-      "parameters": {
-        "type": "object",
-        "properties": {
-          "id": { "type": "string" },
-          "newNode": {
-            "type": "object",
-            "properties": {
-              "id": { "type": "string" },
-              "data": { "type": "object" }
+    type: 'function',
+    function: {
+      name: 'replaceNode',
+      description: 'Replaces an existing node in the graph.',
+      parameters: {
+        type: 'object',
+        properties: {
+          id: { type: 'string' },
+          newNode: {
+            type: 'object',
+            properties: {
+              id: { type: 'string' },
+              data: { type: 'object' }
             },
-            "required": ["id", "data"]
+            required: ['id', 'data']
           }
         }
       }
     }
   },
   {
-    "type": "function",
-    "function": {
-      "name": "deleteNode",
-      "description": "Deletes a node from the graph.",
-      "parameters": {
-        "type": "object",
-        "properties": {
-          "id": { "type": "string" }
+    type: 'function',
+    function: {
+      name: 'deleteNode',
+      description: 'Deletes a node from the graph.',
+      parameters: {
+        type: 'object',
+        properties: {
+          id: { type: 'string' }
         }
       }
     }
   },
   {
-    "type": "function",
-    "function": {
-      "name": "getNodeOutputValue",
-      "description": "Snapshot the current value of node from the graph.",
-      "parameters": {
-        "type": "object",
-        "properties": {
-          "id": { "type": "string" }
+    type: 'function',
+    function: {
+      name: 'getNodeOutputValue',
+      description: 'Snapshot the current value of node from the graph.',
+      parameters: {
+        type: 'object',
+        properties: {
+          id: { type: 'string' }
         }
       }
     }
   }
-]
+];
 
-
-let model = "gpt-4o";
+let model = 'gpt-4o';
 // let model = "gpt-4-turbo-preview";
 export const client = Instructor({
   client: openai,
-  mode: "JSON",
+  mode: 'JSON'
 });
 
 export async function generateImage(prompt: string) {
   const response = await openai.images.generate({
-    model: "dall-e-3",
+    model: 'dall-e-3',
     prompt: prompt,
     n: 1,
-    size: "1024x1024",
+    size: '1024x1024'
   });
   return response.data[0].url;
 }
 
-export async function processUserInput(input: string, system: string, availableFunctions: { [key: string]: Function }) {
+export async function processUserInput(
+  input: string,
+  system: string,
+  availableFunctions: { [key: string]: Function }
+) {
   let messages: ChatCompletionMessageParam[] = [
-    { role: "system", content: system },
-    { role: "user", content: input }
-  ]
+    { role: 'system', content: system },
+    { role: 'user', content: input }
+  ];
 
-
-  let running = true
+  let running = true;
   while (running) {
-    console.log("messages", messages)
+    console.log('messages', messages);
     const response = await client.chat.completions.create({
       messages,
       model,
       tools: toolSpec,
-      tool_choice: "auto",
+      tool_choice: 'auto',
       temperature: 0
     });
 
     if (response.choices[0].finish_reason === 'stop') {
-      return response
+      return response;
     }
 
-    const latest = response.choices[0].message
-    messages.push(latest)
-    console.log(latest)
+    const latest = response.choices[0].message;
+    messages.push(latest);
 
-    const toolCalls = latest.tool_calls
+    const toolCalls = latest.tool_calls;
     if (toolCalls) {
       for (const toolCall of toolCalls) {
-        console.log("toolCall", toolCall)
+        console.log('toolCall', toolCall);
         const functionName = toolCall.function.name;
         const functionToCall = availableFunctions[functionName];
         // escape all newlines in arguments string
-        const functionArgs = JSON.parse(toolCall.function.arguments.replace(/\n/g, "\n"));
+        const functionArgs = JSON.parse(
+          toolCall.function.arguments.replace(/\n/g, '\n')
+        );
         const functionResponse = functionToCall(functionArgs);
-        console.log("response", toolCall.id, functionResponse)
+        console.log('response', toolCall.id, functionResponse);
         messages.push({
           tool_call_id: toolCall.id,
-          role: "tool",
-          content: functionResponse,
+          role: 'tool',
+          content: functionResponse
         });
       }
     }
   }
 }
 
-export async function doLLM(input: string, system: string, response_model: any) {
+export async function doLLM(
+  input: string,
+  system: string,
+  response_model: any
+) {
   try {
-    console.log("input", input);
-    console.log("system", system);
+    console.log('input', input);
+    console.log('system', system);
 
     return await client.chat.completions.create({
       messages: [
-        { role: "system", content: system },
-        { role: "user", content: input },
+        { role: 'system', content: system },
+        { role: 'user', content: input }
       ],
-      model,
+      model
     });
   } catch (error) {
-    console.error("Error analyzing text:", error);
+    console.error('Error analyzing text:', error);
     return null;
   }
 }
