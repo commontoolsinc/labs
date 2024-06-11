374de7df22cbb089e48d1b9fe9e5be4050d58361
Gordon Brander <gordonbrander@users.noreply.github.com>
2024-06-11 16:10:52 -0700

    Clean up syntax

--- typescript/packages/common-ui/src/hyperscript/known-tags.ts
+++ typescript/packages/common-ui/src/hyperscript/known-tags.ts
@@ -1,11 +1,15 @@
 import * as Tags from './tags.js';
 import { View } from './view.js';
 
-const viewByName: Record<string, View> = Object.freeze({...Tags})
+const {freeze, values, fromEntries} = Object;
+
+const viewByName: Record<string, View> = freeze({...Tags});
 
 /** Index of factory functions by HTML tag name */
-const viewByTag = Object.fromEntries(
-  Object.values(viewByName).map((factory) => [factory.tag, factory])
-)
+const viewByTag = freeze(
+  fromEntries(
+    values(viewByName).map((factory) => [factory.tag, factory])
+  )
+);
 
-export const getViewByTag = (tag: string) => viewByTag[tag]
+export const getViewByTag = (tag: string) => viewByTag[tag];
