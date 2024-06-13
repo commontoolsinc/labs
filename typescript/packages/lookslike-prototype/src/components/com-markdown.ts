82351440ece034bed104e1bc18eee7975a08076a
Ben Follington <5009316+bfollington@users.noreply.github.com>
2024-06-13 15:29:46 -0700

    Simplify markdown component

--- typescript/packages/lookslike-prototype/src/components/com-markdown.ts
+++ typescript/packages/lookslike-prototype/src/components/com-markdown.ts
@@ -1,32 +1,28 @@
 import { LitElement, html } from "lit-element";
 import { customElement, property } from "lit/decorators.js";
+import { until } from "lit/directives/until.js";
 import { unsafeHTML } from "lit/directives/unsafe-html.js";
 import { marked } from "marked";
 
-export async function markdownToHtml(markdown: string) {
+export function markdownToHtml(markdown: string) {
   return marked(markdown);
 }
 
 @customElement("com-markdown")
 export default class MarkdownElement extends LitElement {
-  @property() renderedMarkdown: any = html``;
-  @property({ type: Boolean }) safe = false;
   @property({ type: String }) markdown = "";
+  @property({ type: Boolean }) safe = false;
+
+  async renderMarkdown() {
+    return unsafeHTML(await marked(this.markdown));
+  }
 
   override render() {
     return html`
       <style>
         font-family: serif;
       </style>
-      ${this.renderedMarkdown}
+      ${until(this.renderMarkdown(), html``)}
     `;
-  }
-
-  override updated(changedProperties: Map<string | number | symbol, unknown>) {
-    if (changedProperties.has("markdown")) {
-      markdownToHtml(this.markdown).then(
-        (r) => (this.renderedMarkdown = html`${unsafeHTML(r)}`)
-      );
-    }
   }
 }
