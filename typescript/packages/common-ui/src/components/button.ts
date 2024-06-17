b9d066a6c3cfac798b4cf5f116778f2d029a380f
Gordon Brander <gordonbrander@users.noreply.github.com>
2024-06-17 11:23:34 -0700

    Make vnode properties liberal in what they accept
    Postel's Law: be conservative in what you do, be liberal in what
    you accept from others.
    
    Properties were previously being validated and would complain if you
    included a property that was not allowed. Instead, we want to validate
    properties, while allowing additional properties, but ignoring those
    additional properties when rendering. This will make us more robust to
    LLM hallucinations.

--- typescript/packages/common-ui/src/components/button.ts
+++ typescript/packages/common-ui/src/components/button.ts
@@ -1,12 +1,9 @@
 import { view } from "../hyperscript/render.js";
 
 export const button = view("button", {
-  type: "object",
-  properties: {
-    id: { type: "string" },
-    "@click": {
-      type: "object",
-      properties: { "@type": { type: "string" }, name: { type: "string" } },
-    },
+  id: { type: "string" },
+  "@click": {
+    type: "object",
+    properties: { "@type": { type: "string" }, name: { type: "string" } },
   },
 });
