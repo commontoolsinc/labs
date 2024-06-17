b4c38d69c74bd7ffbacaad32e9737e2ccd55774c
Gordon Brander <gordonbrander@users.noreply.github.com>
2024-06-17 14:40:50 -0700

    Add schema helpers
    Factories for creating commonly-used JSON Schemas

---
+++ typescript/packages/common-ui/src/hyperscript/schema-helpers.ts
@@ -0,0 +1,35 @@
+/** Schema helpers */
+
+// NOTE: these are written as factories for JSONSchema objects, rather than
+// frozen schema objects because the JSONSchema validator mutates the object
+// to add metadata about the schema.
+
+/** JSONSchema for binding */
+export const binding = () => ({
+  type: "object",
+  properties: {
+    "@type": { type: "string" },
+    name: { type: "string" }
+  },
+  required: ["@type", "name"]
+});
+
+/** Mixin for list of allowed basic events */
+export const eventProps = () => ({
+  "@click": binding(),
+  "@change": binding(),
+  "@focus": binding(),
+  "blur": binding(),
+  "@focusin": binding(),
+  "@focusout": binding(),
+  "@input": binding(),
+  "@keydown": binding(),
+  "@keyup": binding(),
+  "@mousedown": binding(),
+  "@mouseenter": binding(),
+  "@mouseleave": binding(),
+  "@mousemove": binding(),
+  "@mouseout": binding(),
+  "@mouseover": binding(),
+  "@mouseup": binding()
+});
