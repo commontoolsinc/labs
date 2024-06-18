c779a0851abcd2f3328d3e3690ebf474a04b7e7c
Bernhard Seefeld <seefeld@gmail.com>
2024-06-18 10:43:17 -0700

    Add include component, that transcludes another vdom

---
+++ typescript/packages/common-ui/src/components/include.ts
@@ -0,0 +1,22 @@
+import { LitElement, html } from "lit-element";
+import { customElement, property } from "lit-element/decorators.js";
+import { view, render, RenderContext } from "../hyperscript/render.js";
+import { VNode } from "../hyperscript/view.js";
+import { eventProps } from "../hyperscript/schema-helpers.js";
+
+export const datatable = view("common-include", {
+  ...eventProps(),
+  content: { type: "array" },
+});
+
+@customElement("common-include")
+export class IncludeElement extends LitElement {
+  @property({ type: Array })
+  content: [VNode, RenderContext];
+
+  override render() {
+    const element = render(...this.content);
+
+    return html`${element}`;
+  }
+}
