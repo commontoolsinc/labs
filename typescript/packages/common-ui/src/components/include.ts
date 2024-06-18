33550d168ca4e435f395eebbfab692ea13f5b4d3
Bernhard Seefeld <seefeld@gmail.com>
2024-06-18 15:05:14 -0700

    Add include component, that transcludes another vdom (#55)
    * Add include component, that transcludes another vdom
    
    * add it to the exported tags list
    
    * add todo per code review

---
+++ typescript/packages/common-ui/src/components/include.ts
@@ -0,0 +1,23 @@
+import { LitElement, html } from "lit-element";
+import { customElement, property } from "lit-element/decorators.js";
+import { view, render, RenderContext } from "../hyperscript/render.js";
+import { VNode } from "../hyperscript/view.js";
+import { eventProps } from "../hyperscript/schema-helpers.js";
+
+export const include = view("common-include", {
+  ...eventProps(),
+  // TODO; This should be split into two properties, vdom and context
+  content: { type: "array" },
+});
+
+@customElement("common-include")
+export class IncludeElement extends LitElement {
+  @property({ type: Array })
+  content: [VNode, RenderContext];
+
+  override render() {
+    const element = render(...this.content);
+
+    return html`${element}`;
+  }
+}
