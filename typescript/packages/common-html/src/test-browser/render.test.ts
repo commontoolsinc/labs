e91b014be98fae0fb62b5883b1135aceea45b461
Gordon Brander <gordonbrander@users.noreply.github.com>
2024-07-10 13:58:46 -0400

    Add tests for attributes and properties

--- typescript/packages/common-html/src/test-browser/render.test.ts
+++ typescript/packages/common-html/src/test-browser/render.test.ts
@@ -2,7 +2,7 @@ import { assert, equal as assertEqual } from "./assert.js";
 import { render } from "../render.js";
 import { html } from "../html.js";
 import { setDebug } from "../log.js";
-//import state from "../state.js";
+import state from "../state.js";
 
 setDebug(true);
 
@@ -22,6 +22,46 @@ describe("render", () => {
     assert(element instanceof HTMLButtonElement);
     assertEqual(element.getAttribute("title"), "100");
   });
+
+  it("binds reactive attributes", () => {
+    const title = state("world");
+    const template = html`<button title="${title}">Hello</div>`;
+
+    const element = render(template);
+    assert(element instanceof HTMLButtonElement);
+    assertEqual(element.getAttribute("title"), "world");
+
+    title.send("reactivity");
+    assertEqual(element.getAttribute("title"), "reactivity");
+  });
+
+  it("renders properties", () => {
+    const hidden = true;
+    const template = html`<button .hidden=${hidden}>Hello</div>`;
+    const element = render(template);
+    assert(element instanceof HTMLButtonElement);
+    // @ts-ignore - inacurate types. Element does have hidden property.
+    assertEqual(element.hidden, true);
+    assert(element.hasAttribute("hidden"));
+  });
+
+  it("binds reactive properties", () => {
+    const hidden = state(true);
+    const template = html`<button .hidden=${hidden}>Hello</div>`;
+
+    const element = render(template);
+    assert(element instanceof HTMLButtonElement);
+    // @ts-ignore - inacurate types. Element does have hidden property.
+    assertEqual(element.hidden, true);
+    assert(element.hasAttribute("hidden"));
+
+    hidden.send(false);
+
+    // @ts-ignore - inacurate types. Element does have hidden property.
+    assertEqual(element.hidden, false);
+    assert(!element.hasAttribute("hidden"));
+  });
+
 
   // it("renders children as text nodes", () => {
   //   const name = "world";
