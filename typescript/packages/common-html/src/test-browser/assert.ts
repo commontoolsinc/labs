3c57f93843fa60a2232c0d671023eb564f019ffb
Gordon Brander <gordonbrander@users.noreply.github.com>
2024-07-10 13:20:47 -0400

    Throw assertion errors for failed tests

--- typescript/packages/common-html/src/test-browser/assert.ts
+++ typescript/packages/common-html/src/test-browser/assert.ts
@@ -1,16 +1,50 @@
-export const assert = (condition: boolean, msg = "") => {
-  console.assert(condition, msg);
+export class AssertionError<A, E> extends Error {
+  actual: A | undefined;
+  expected: E | undefined;
+
+  constructor({
+    message,
+    actual = undefined,
+    expected = undefined,
+  }: {
+    message: string;
+    actual?: A | undefined;
+    expected?: E | undefined;
+  }) {
+    super(message);
+    this.name = "AssertionError";
+    this.actual = actual;
+    this.expected = expected;
+  }
+}
+
+export const assert = (condition: boolean, message = "") => {
+  if (!condition) {
+    throw new AssertionError({
+      message,
+      actual: false,
+      expected: true,
+    });
+  }
 };
 
-export const equal = (a: unknown, b: unknown, msg = "") => {
-  console.assert(a === b, `${msg} ${a} !== ${b}`);
+export const equal = (actual: unknown, expected: unknown, message = "") => {
+  if (actual !== expected) {
+    throw new AssertionError({
+      message,
+      actual,
+      expected,
+    });
+  }
 };
 
-export const throws = (run: () => void, msg = "") => {
+export const throws = (run: () => void, message = "") => {
   try {
     run();
   } catch (e) {
     return;
   }
-  console.assert(false, msg);
+  throw new AssertionError({
+    message
+  });
 };
