a2541076085b694bd67defd4abffb7bc480d0c43
Gordon Brander <gordonbrander@users.noreply.github.com>
2024-07-15 19:37:21 -0400

    WIP: nest tags with blocks

--- typescript/packages/common-html/src/parser.ts
+++ typescript/packages/common-html/src/parser.ts
@@ -1,18 +1,68 @@
 import { Parser } from "htmlparser2";
-import * as hole from "./hole.js";
-import * as vnode from "./vnode.js";
-import { VNode, Props } from "./vnode.js";
 import * as logger from "./logger.js";
+
+export type Props = { [key: string]: string | Var };
+
+export type Node = VNode | Block | Var | string;
+
+export type VNode = {
+  type: "vnode";
+  name: string;
+  props: Props;
+  children: Array<Node>;
+};
+
+export const createVNode = (
+  name: string,
+  props: Props = {},
+  children: Array<Node> = [],
+): VNode => {
+  return { type: "vnode", name, props, children };
+};
+
+export const isVNode = (value: unknown): value is VNode => {
+  return (value as VNode)?.type === "vnode";
+};
+
+export type Var = {
+  type: "var";
+  name: string;
+};
+
+export const createVar = (name: string): Var => {
+  return { type: "var", name };
+};
+
+export const isVar = (value: unknown): value is Var => {
+  return (value as Var)?.type === "var";
+};
+
+export type Block = {
+  type: "block";
+  name: string;
+  children: Array<Node>;
+};
+
+export const createBlock = (
+  name: string,
+  children: Array<Node> = [],
+): Block => {
+  return { type: "block", name, children };
+};
+
+export const isBlock = (value: unknown): value is Block => {
+  return (value as Block)?.type === "block";
+};
 
 export type TagOpenToken = {
   type: "tagopen";
-  tag: string;
+  name: string;
   props: Props;
 };
 
 export type TagCloseToken = {
   type: "tagclose";
-  tag: string;
+  name: string;
 };
 
 export type TextToken = {
@@ -48,16 +98,16 @@ export const tokenize = (markup: string): Array<Token> => {
 
   const parser = new Parser(
     {
-      onopentag(tag, attrs) {
+      onopentag(name, attrs) {
         // We've turned off the namespace feature, so node attributes will
         // contain only string values, not QualifiedAttribute objects.
         const props = parseProps(attrs as { [key: string]: string });
-        const token: TagOpenToken = { type: "tagopen", tag, props };
+        const token: TagOpenToken = { type: "tagopen", name, props };
         logger.debug("tagopen", token);
         tokens.push(token);
       },
-      onclosetag(tag) {
-        const token: TagCloseToken = { type: "tagclose", tag };
+      onclosetag(name) {
+        const token: TagCloseToken = { type: "tagclose", name };
         logger.debug("tagclose", token);
         tokens.push(token);
       },
@@ -95,20 +145,20 @@ const tokenizeMustaches = (text: string): Array<Token> => {
       logger.debug("text", token);
       tokens.push(token);
     }
-    const stash = match[1];
-    if (stash.startsWith("#")) {
-      const token: BlockOpenToken = { type: "blockopen", name: stash.slice(1) };
+    const body = match[1];
+    if (body.startsWith("#")) {
+      const token: BlockOpenToken = { type: "blockopen", name: body.slice(1) };
       logger.debug("blockopen", token);
       tokens.push(token);
-    } else if (stash.startsWith("/")) {
+    } else if (body.startsWith("/")) {
       const token: BlockCloseToken = {
         type: "blockclose",
-        name: stash.slice(1),
+        name: body.slice(1),
       };
       logger.debug("blockclose", token);
       tokens.push(token);
     } else {
-      const token: VarToken = { type: "var", name: stash };
+      const token: VarToken = { type: "var", name: body };
       logger.debug("var", token);
       tokens.push(token);
     }
@@ -131,22 +181,35 @@ const tokenizeMustaches = (text: string): Array<Token> => {
 
 /** Parse a template into a simple JSON markup representation */
 export const parse = (markup: string): VNode => {
-  let root: VNode = vnode.create("documentfragment");
-  let stack: Array<VNode> = [root];
+  let root: VNode = createVNode("documentfragment");
+  let stack: Array<VNode | Block> = [root];
 
   for (const token of tokenize(markup)) {
     const top = getTop(stack);
     switch (token.type) {
       case "tagopen": {
-        const next = vnode.create(token.tag, token.props);
+        const next = createVNode(token.name, token.props);
         top.children.push(next);
         stack.push(next);
         break;
       }
       case "tagclose": {
-        const vnode = stack.pop();
-        if (vnode.tag !== token.tag) {
-          throw new ParseError(`Unexpected closing tag ${token.tag}`);
+        const top = stack.pop();
+        if (!isVNode(top) || top.name !== token.name) {
+          throw new ParseError(`Unexpected closing tag ${token.name}`);
+        }
+        break;
+      }
+      case "blockopen": {
+        const next = createBlock(token.name);
+        top.children.push(next);
+        stack.push(next);
+        break;
+      }
+      case "blockclose": {
+        const top = stack.pop();
+        if (!isBlock(top) || top.name !== token.name) {
+          throw new ParseError(`Unexpected closing block ${token.name}`);
         }
         break;
       }
@@ -155,7 +218,7 @@ export const parse = (markup: string): VNode => {
         break;
       }
       case "var": {
-        top.children.push(hole.create(token.name));
+        top.children.push(createVar(token.name));
         break;
       }
       default: {
@@ -169,16 +232,17 @@ export const parse = (markup: string): VNode => {
 
 export default parse;
 
-const getTop = (stack: Array<VNode>): VNode | null => stack.at(-1) ?? null;
+const getTop = (stack: Array<VNode | Block>): VNode | Block | null =>
+  stack.at(-1) ?? null;
 
 const parseProps = (attrs: { [key: string]: string }): Props => {
   const result: Props = {};
   for (const [key, value] of Object.entries(attrs)) {
-    const parsed = hole.parse(value);
+    const parsed = tokenizeMustaches(value);
     const first = parsed.at(0);
     if (parsed.length !== 1) {
       result[key] = "";
-    } else if (hole.isHole(first)) {
+    } else if (isVar(first)) {
       result[key] = first;
     } else {
       result[key] = `${value}`;
