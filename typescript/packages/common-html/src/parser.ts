703892bdb9354fb9e2e9801e1ae70bec5cbc6b97
Gordon Brander <gordonbrander@users.noreply.github.com>
2024-07-15 18:54:34 -0400

    WIP: do a lexing pass
    This will let us create block structures.

--- typescript/packages/common-html/src/parser.ts
+++ typescript/packages/common-html/src/parser.ts
@@ -3,36 +3,66 @@ import { parse as parseMustaches, isHole } from "./hole.js";
 import { create as createVNode, VNode, Props } from "./vnode.js";
 import * as logger from "./logger.js";
 
-/** Parse a template into a simple JSON markup representation */
-export const parse = (markup: string): VNode => {
-  let root: VNode = createVNode("documentfragment");
-  let stack: Array<VNode> = [root];
+export type TagOpenToken = {
+  type: "tagopen";
+  tag: string;
+  props: Props;
+};
+
+export type TagCloseToken = {
+  type: "tagclose";
+  tag: string;
+};
+
+export type TextToken = {
+  type: "text";
+  value: string;
+};
+
+export type VarToken = {
+  type: "var";
+  name: string;
+};
+
+export type BlockOpenToken = {
+  type: "blockopen";
+  name: string;
+};
+
+export type BlockCloseToken = {
+  type: "blockclose";
+  name: string;
+};
+
+export type Token =
+  | TagOpenToken
+  | TagCloseToken
+  | TextToken
+  | VarToken
+  | BlockOpenToken
+  | BlockCloseToken;
+
+export const tokenize = (markup: string): Array<Token> => {
+  const tokens: Array<Token> = [];
 
   const parser = new Parser(
     {
-      onopentag(name, attrs) {
-        logger.debug("Open", name, attrs);
+      onopentag(tag, attrs) {
         // We've turned off the namespace feature, so node attributes will
         // contain only string values, not QualifiedAttribute objects.
         const props = parseProps(attrs as { [key: string]: string });
-        const next = createVNode(name, props);
-        const top = getTop(stack);
-        if (!top) {
-          throw new ParseError(`No parent tag for ${name}`);
-        }
-        top.children.push(next);
-        stack.push(next);
+        const token: TagOpenToken = { type: "tagopen", tag, props };
+        logger.debug("tagopen", token);
+        tokens.push(token);
       },
-      onclosetag(name) {
-        const vnode = stack.pop();
-        if (!vnode) {
-          throw new ParseError(`Unexpected closing tag ${name}`);
-        }
+      onclosetag(tag) {
+        const token: TagCloseToken = { type: "tagclose", tag };
+        logger.debug("tagopen", token);
+        tokens.push(token);
       },
       ontext(text) {
-        const top = getTop(stack);
-        const parsed = parseMustaches(text.trim());
-        top.children.push(...parsed);
+        const parsed = tokenizeMustaches(text.trim());
+        tokens.push(...parsed);
       },
     },
     {
@@ -44,6 +74,95 @@ export const parse = (markup: string): VNode => {
 
   parser.write(markup);
   parser.end();
+
+  return tokens;
+};
+
+const mustacheRegex = /{{(\w+)}}/g;
+
+const tokenizeMustaches = (text: string): Array<Token> => {
+  const tokens: Array<Token> = [];
+  let lastIndex = 0;
+  let match: RegExpMatchArray | null = null;
+
+  while ((match = mustacheRegex.exec(text)) !== null) {
+    if (match.index > lastIndex) {
+      const token: TextToken = {
+        type: "text",
+        value: text.slice(lastIndex, match.index),
+      };
+      logger.debug("text", token);
+      tokens.push(token);
+    }
+    const stash = match[1];
+    if (stash.startsWith("#")) {
+      const token: BlockOpenToken = { type: "blockopen", name: stash.slice(1) };
+      logger.debug("blockopen", token);
+      tokens.push(token);
+    } else if (stash.startsWith("/")) {
+      const token: BlockCloseToken = {
+        type: "blockclose",
+        name: stash.slice(1),
+      };
+      logger.debug("blockclose", token);
+      tokens.push(token);
+    } else {
+      const token: VarToken = { type: "var", name: stash };
+      logger.debug("var", token);
+      tokens.push(token);
+    }
+    lastIndex = mustacheRegex.lastIndex;
+  }
+
+  if (lastIndex < text.length) {
+    const token: TextToken = {
+      type: "text",
+      value: text.slice(lastIndex, match.index),
+    };
+    logger.debug("text", token);
+    tokens.push(token);
+  }
+
+  mustacheRegex.lastIndex = 0;
+
+  return tokens;
+};
+
+/** Parse a template into a simple JSON markup representation */
+export const parse = (markup: string): VNode => {
+  let root: VNode = createVNode("documentfragment");
+  let stack: Array<VNode> = [root];
+
+  for (const token of tokenize(markup)) {
+    const top = getTop(stack);
+    switch (token.type) {
+      case "tagopen": {
+        const next = createVNode(token.tag, token.props);
+        top.children.push(next);
+        stack.push(next);
+        break;
+      }
+      case "tagclose": {
+        const vnode = stack.pop();
+        if (vnode.tag !== token.tag) {
+          throw new ParseError(`Unexpected closing tag ${token.tag}`);
+        }
+        break;
+      }
+      case "text": {
+        top.children.push(token.value);
+        break;
+      }
+      case "var": {
+        top.children.push(token);
+        break;
+      }
+      default: {
+        throw new ParseError(`Unexpected token ${JSON.stringify(token)}`);
+      }
+    }
+  }
+
   return root;
 };
 
