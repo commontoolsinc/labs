511adcd8625a12ee3edd71f907eca7f3d6dc3df4
Gordon Brander <gordonbrander@users.noreply.github.com>
2024-07-24 14:40:52 -0400

    Add lift function that solves diamond problem
    Uses vector clock to solve diamond problem.

--- typescript/packages/common-html/src/propagator.ts
+++ typescript/packages/common-html/src/propagator.ts
@@ -1,18 +1,28 @@
-import { Cancellable, useCancelGroup } from "./cancel.js";
+import { Cancel, Cancellable, useCancelGroup } from "./cancel.js";
 import * as logger from "./logger.js";
 import { Lens } from "./lens.js";
-
-const advanceClock = (a: number, b: number) => Math.max(a, b) + 1;
-
-export const lww = <T>(_state: T, next: T) => next;
+import cid from "./cid.js";
 
 export type LamportTime = number;
+
+const advanceClock = (...times: LamportTime[]) => Math.max(...times) + 1;
+
+export const lww = <T>(_state: T, next: T) => next;
 
 /**
  * A cell is a reactive value that can be updated and subscribed to.
  */
 export class Cell<Value> {
-  #name: string = "";
+  static get<Value>(cell: Cell<Value>) {
+    return cell.get();
+  }
+
+  static time<Value>(cell: Cell<Value>) {
+    return cell.time;
+  }
+
+  #id = cid();
+  #name = "";
   #neighbors = new Set<(value: Value, time: LamportTime) => void>();
   #time: LamportTime = 0;
   #update: (state: Value, next: Value) => Value;
@@ -30,10 +40,25 @@ export class Cell<Value> {
     this.#name = name;
     this.#update = update;
     this.#value = value;
+    logger.debug({
+      msg: "Cell created",
+      cell: this.id,
+      name: this.name,
+      value: this.#value,
+      time: this.#time,
+    });
+  }
+
+  get id() {
+    return this.#id;
   }
 
   get name() {
     return this.#name;
+  }
+
+  get time() {
+    return this.#time;
   }
 
   get() {
@@ -41,55 +66,73 @@ export class Cell<Value> {
   }
 
   send(value: Value, time: LamportTime = this.#time + 1): number {
-    logger.debug(`cell#${this.name}`, "Message", value, time);
+    logger.debug({
+      msg: "Sent value",
+      cell: this.id,
+      value,
+      time,
+    });
 
-    // We ignore old news
+    // We ignore old news.
+    // If times are equal, we ignore incoming value.
     if (this.#time >= time) {
-      logger.debug(
-        `cell#${this.name}`,
-        "Message out of date. Ignoring.",
+      logger.debug({
+        msg: "Value out of date. Ignoring.",
+        cell: this.id,
         value,
         time,
-      );
+      });
+      return this.#time;
+    }
+
+    // We only advance clock if value changes state
+    if (this.#value === value) {
+      logger.debug({
+        msg: "Value unchanged. Ignoring.",
+        cell: this.id,
+        value,
+        time,
+      });
       return this.#time;
     }
 
     this.#time = advanceClock(this.#time, time);
-    logger.debug(`cell#${this.name}`, "Advanced clock", this.#time);
 
     const next = this.#update(this.#value, value);
-
-    // We only advance clock if msg changed state
-    if (this.#value === next) {
-      logger.debug(`cell#${this.name}`, "Value unchanged.", this.#value);
-      return this.#time;
-    }
-
     const prev = this.#value;
     this.#value = next;
-    logger.debug(`cell#${this.name}`, "Value updated", prev, next);
+
+    logger.debug({
+      msg: "Value updated",
+      cell: this.id,
+      prev,
+      value,
+      time: this.#time,
+    });
 
     // Notify neighbors
     for (const neighbor of this.#neighbors) {
       neighbor(next, this.#time);
     }
-    logger.debug(
-      `cell#${this.name}`,
-      "Notified neighbors",
-      this.#neighbors.size,
-    );
+
+    logger.debug({
+      msg: "Notified neighbors",
+      cell: this.id,
+      neighbors: this.#neighbors.size,
+    });
 
     return this.#time;
   }
 
   /** Disconnect all neighbors */
   disconnect() {
+    const size = this.#neighbors.size;
     this.#neighbors.clear();
-    logger.debug(
-      `cell#${this.name}`,
-      "Disconnected all neighbors",
-      this.#neighbors.size,
-    );
+    logger.debug({
+      msg: "Disconnected all neighbors",
+      cell: this.id,
+      neighbors: size,
+    });
   }
 
   sink(callback: (value: Value, time: LamportTime) => void) {
@@ -137,14 +180,18 @@ export const lens = <B, S>(
   const [cancel, addCancel] = useCancelGroup();
 
   // Propagate writes from parent to child
-  const cancelBigToSmall = big.sink((parentValue, time) => {
-    small.send(lens.get(parentValue), time);
+  const cancelBigToSmall = big.sink((bigValue, time) => {
+    small.send(lens.get(bigValue), time);
   });
   addCancel(cancelBigToSmall);
 
   // Propagate writes from child to parent
-  const cancelSmallToBig = small.sink((value, time) => {
-    big.send(lens.update(big.get(), value), time);
+  const cancelSmallToBig = small.sink((smallValue, time) => {
+    const bigValue = big.get();
+    const currSmallValue = lens.get(bigValue);
+    if (currSmallValue !== smallValue) {
+      big.send(lens.update(bigValue, smallValue), time);
+    }
   });
   addCancel(cancelSmallToBig);
 
@@ -162,3 +209,101 @@ export const key = <T, K extends keyof T>(
     get: (big) => big[key],
     update: (big, small) => ({ ...big, [key]: small }),
   });
+
+export function lift<A, B>(fn: (a: A) => B): (a: Cell<A>, b: Cell<B>) => Cancel;
+export function lift<A, B, C>(
+  fn: (a: A, b: B) => C,
+): (a: Cell<A>, b: Cell<B>, c: Cell<C>) => Cancel;
+export function lift<A, B, C, D>(
+  fn: (a: A, b: B, c: C) => D,
+): (a: Cell<A>, b: Cell<B>, c: Cell<C>, d: Cell<D>) => Cancel;
+export function lift<A, B, C, D, E>(
+  fn: (a: A, b: B, c: C, d: D) => E,
+): (a: Cell<A>, b: Cell<B>, c: Cell<C>, d: Cell<D>, e: Cell<E>) => Cancel;
+export function lift<A, B, C, D, E, F>(
+  fn: (a: A, b: B, c: C, d: D, e: E) => F,
+): (
+  a: Cell<A>,
+  b: Cell<B>,
+  c: Cell<C>,
+  d: Cell<D>,
+  e: Cell<E>,
+  f: Cell<F>,
+) => Cancel;
+export function lift<A, B, C, D, E, F, G>(
+  fn: (a: A, b: B, c: C, d: D, e: E, f: F) => G,
+): (
+  a: Cell<A>,
+  b: Cell<B>,
+  c: Cell<C>,
+  d: Cell<D>,
+  e: Cell<E>,
+  f: Cell<F>,
+  g: Cell<G>,
+) => Cancel;
+export function lift<A, B, C, D, E, F, G, H>(
+  fn: (a: A, b: B, c: C, d: D, e: E, f: F, g: G) => H,
+): (
+  a: Cell<A>,
+  b: Cell<B>,
+  c: Cell<C>,
+  d: Cell<D>,
+  e: Cell<E>,
+  f: Cell<F>,
+  g: Cell<G>,
+  h: Cell<H>,
+) => Cancel;
+export function lift<A, B, C, D, E, F, G, H, I>(
+  fn: (a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H) => I,
+): (
+  a: Cell<A>,
+  b: Cell<B>,
+  c: Cell<C>,
+  d: Cell<D>,
+  e: Cell<E>,
+  f: Cell<F>,
+  g: Cell<G>,
+  h: Cell<H>,
+  i: Cell<I>,
+) => Cancel;
+export function lift(fn: (...args: unknown[]) => unknown) {
+  return (...cells: Cell<unknown>[]): Cancel => {
+    if (cells.length < 2) {
+      throw new TypeError("lift requires at least 2 cells");
+    }
+
+    const [cancel, addCancel] = useCancelGroup();
+
+    const output = cells.pop()!;
+
+    // Create a map of cell IDs to times.
+    // We use this as a vector clock when updating.
+    // All cells are initialized to t=-1 since we will have never seen
+    // an update from any of the cells. This will get immediately replaced
+    // by the first immediate update we get from sink.
+    const clock = new Map(cells.map((cell) => [cell.id, -1]));
+
+    for (const cell of cells) {
+      const cancel = cell.sink((_value, time) => {
+        // Get the last time we got an update from this cell
+        const lastTime = clock.get(cell.id);
+        if (lastTime == null) {
+          // This should never happen
+          throw Error(`Cell not found in clock: ${cell.id}`);
+        }
+        // If this cell has updated (e.g. not a "diamond problem"" update)
+        // then update the entry in the clock and send the calculated output.
+        if (time > lastTime) {
+          clock.set(cell.id, time);
+          output.send(
+            fn(...cells.map(Cell.get)),
+            advanceClock(time, output.time),
+          );
+        }
+      });
+      addCancel(cancel);
+    }
+
+    return cancel;
+  };
+}
--- typescript/packages/common-html/src/test/propagator.test.ts
+++ typescript/packages/common-html/src/test/propagator.test.ts
@@ -2,7 +2,7 @@ import {
   equal as assertEqual,
   deepEqual as assertDeepEqual,
 } from "node:assert/strict";
-import { cell, lens } from "../propagator.js";
+import { cell, state, lens, lift } from "../propagator.js";
 
 describe("cell()", () => {
   it("synchronously sets the value", () => {
@@ -31,13 +31,14 @@ describe("cell()", () => {
 
 describe("lens()", () => {
   it("lenses over a cell", () => {
-    const x = cell({ value: { a: { b: { c: 10 } } } });
+    const x = cell({ name: "x", value: { a: { b: { c: 10 } } } });
 
-    const c = lens({
-      cell: x,
+    const c = lens(x, {
       get: (state) => state.a.b.c,
       update: (state, next) => ({ ...state, a: { b: { c: next } } }),
     });
+
+    assertEqual(c.get(), 10);
 
     c.send(20);
 
@@ -84,5 +85,87 @@ describe("cell.key()", () => {
     assertDeepEqual(a.get(), { b: { c: 20 } });
     assertDeepEqual(b.get(), { c: 20 });
     assertDeepEqual(c.get(), 20);
+  });
+});
+
+describe("lift()", () => {
+  it("lifts a function into a function that reads from and writes to cells", () => {
+    const addCells = lift((a: number, b: number) => a + b);
+
+    const a = state(1, "lift.a");
+    const b = state(2, "lift.b");
+    const out = state(0, "lift.out");
+
+    const cancel = addCells(a, b, out);
+
+    assertEqual(typeof cancel, "function", "returns a cancel function");
+
+    assertEqual(out.get(), 3);
+  });
+
+  it("updates the out cell whenever an input cell updates", () => {
+    const addCells = lift((a: number, b: number) => a + b);
+
+    const a = state(1, "a");
+    const b = state(1, "b");
+    const out = state(0, "out");
+
+    addCells(a, b, out);
+    assertEqual(out.get(), 2);
+
+    a.send(2, out.time);
+    assertEqual(out.get(), 3);
+
+    b.send(2, out.time);
+    assertEqual(out.get(), 4);
+  });
+
+  it("solves the diamond problem", () => {
+    const addCells = lift((a: number, b: number) => a + b);
+
+    const a = state(1, "a");
+    const out = state(0, "out");
+
+    addCells(a, a, out);
+    assertEqual(out.get(), 2);
+
+    let calls = 0;
+    out.sink((_value) => {
+      calls++;
+    });
+
+    a.send(2);
+    assertEqual(out.get(), 4);
+
+    assertEqual(
+      calls,
+      2,
+      "calls neighbors once per upstream output of the diamond",
+    );
+  });
+
+  it("solves the diamond problem (2)", () => {
+    const add3 = lift((a: number, b: number, c: number) => a + b + c);
+
+    const a = state(1, "a");
+    const b = state(1, "b");
+    const out = state(0, "out");
+
+    add3(a, b, b, out);
+    assertEqual(out.get(), 3);
+
+    let calls = 0;
+    out.sink((_value) => {
+      calls++;
+    });
+
+    b.send(2);
+    assertEqual(out.get(), 5);
+
+    assertEqual(
+      calls,
+      2,
+      "calls neighbors once per upstream output of the diamond",
+    );
   });
 });
