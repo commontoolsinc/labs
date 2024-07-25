669f3f686ad44c4ca2d78d7447f59e029b87a974
Gordon Brander <gordonbrander@users.noreply.github.com>
2024-07-25 10:10:57 -0400

    Add browser test for binding deep pathed variables

--- typescript/packages/common-html/src/test-browser/render.test.ts
+++ typescript/packages/common-html/src/test-browser/render.test.ts
@@ -1,6 +1,9 @@
 // import { equal as assertEqual } from "./assert.js";
 import render from "../render.js";
 import html from "../html.js";
+import view from "../view.js";
+import { cell } from "@commontools/common-propagator";
+import * as assert from "./assert.js";
 
 describe("render", () => {
   it("renders", () => {
@@ -11,6 +14,28 @@ describe("render", () => {
     `;
     const parent = document.createElement("div");
     render(parent, renderable);
-    console.log(parent);
+    assert.equal(parent.firstElementChild?.className, "hello");
+    assert.equal(parent.querySelector("p")?.textContent, "Hello world!");
+  });
+
+  it("binds deep paths on variables", () => {
+    const a = cell({ b: { c: "Hello world!" } });
+
+    const renderable = view(
+      `
+        <div class="hello">
+          <p>{{a.b.c}}</p>
+        </div>
+      `,
+      { a },
+    );
+    const parent = document.createElement("div");
+    render(parent, renderable);
+
+    assert.equal(parent.querySelector("p")?.textContent, "Hello world!");
+
+    a.send({ b: { c: "Goodbye world!" } });
+
+    assert.equal(parent.querySelector("p")?.textContent, "Goodbye world!");
   });
 });
