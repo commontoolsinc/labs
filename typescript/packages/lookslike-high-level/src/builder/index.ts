432ac1371bdf25a13ec4ac9573142d73488bedc6
Bernhard Seefeld <berni@common.tools>
2024-07-29 15:33:55 +0200

    export what's public about the builder

---
+++ typescript/packages/lookslike-high-level/src/builder/index.ts
@@ -0,0 +1,14 @@
+export { cell } from "./cell-proxy.js";
+export { lift } from "./module.js";
+export { recipe } from "./recipe.js";
+export {
+  Value,
+  Module,
+  Recipe,
+  CellProxy,
+  isCell,
+  isModule,
+  isRecipe,
+  JSONValue,
+  JSON,
+} from "./types.js";
