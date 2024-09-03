d84185bfcfdc72e91dbce064e8b02c71e142b829
Bernhard Seefeld <berni@common.tools>
2024-09-03 10:27:56 -0700

    - change cell() to take initial value, not default value, as parameter

--- typescript/packages/lookslike-high-level/src/builder/cell-proxy.ts
+++ typescript/packages/lookslike-high-level/src/builder/cell-proxy.ts
@@ -25,10 +25,10 @@ let mapFactory: NodeFactory<any, any>;
 //
 // The proxy yields another proxy for each nested value, but still allows the
 // methods to be called. Setters just call .set() on the nested cell.
-export function cell<T>(defaultValue?: Value<T> | T): CellProxy<T> {
+export function cell<T>(value?: Value<T> | T): CellProxy<T> {
   const store = {
-    value: undefined,
-    defaultValue,
+    value,
+    defaultValue: undefined,
     nodes: new Set<NodeProxy>(),
   };
 
