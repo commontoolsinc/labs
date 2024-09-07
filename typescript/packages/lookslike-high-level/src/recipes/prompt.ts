c43db16c7cde6c0837193d1f50e2d71650911205
Jesse Andrews <anotherjesse@gmail.com>
2024-09-06 22:15:02 -0400

    clean up

--- typescript/packages/lookslike-high-level/src/recipes/prompt.ts
+++ typescript/packages/lookslike-high-level/src/recipes/prompt.ts
@@ -20,7 +20,7 @@ export const prompt = recipe<{ title: string }>("prompt", ({ title }) => {
   )({ title });
 
   const query = lift(({ title }) => ({
-    prompt: `generate 10 image prompt variations for the current prompt: ${title}.  Some should change just the style, some should change the content, and some should change both.`,
+    prompt: `generate 10 image prompt variations for the current prompt: ${title}.  Some should change just the style, some should change the content, and some should change both. The last should be a completely different prompt.`,
     result: [],
     schema: {
       type: "array",
@@ -41,21 +41,12 @@ export const prompt = recipe<{ title: string }>("prompt", ({ title }) => {
 
   const { result: variations } = generateData<string[]>(query);
 
-  // const inner = ({ title }) => html`<div>${title}</div>`;
-  const inner = ({ title }) => html` <common-button onclick=${handler(
-    { title },
-    (_, { title }) => {
-      const newPrompt = run(prompt, {
-        title: getCellReferenceOrValue(title),
-      });
-
+  const inner = ({ title }) =>
+    html`<common-button onclick=${handler({ title }, (_, { title }) => {
+      const newPrompt = run(prompt, { title });
       addGems([newPrompt]);
-
       openSaga(newPrompt.get()[ID]);
-    },
-  )}}">
-    ${title}
-  </common-button>`;
+    })}}">${title}</common-button>`;
 
   return {
     [NAME]: title,
