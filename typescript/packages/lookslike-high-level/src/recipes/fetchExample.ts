1864544dcddd3625a557f3ff1f817b89f00b2bcc
Bernhard Seefeld <berni@common.tools>
2024-09-16 16:47:28 -0700

    match backend schema

--- typescript/packages/lookslike-high-level/src/recipes/fetchExample.ts
+++ typescript/packages/lookslike-high-level/src/recipes/fetchExample.ts
@@ -1,37 +1,46 @@
 import { html } from "@commontools/common-html";
-import { recipe, fetchData, UI, NAME, ifElse } from "@commontools/common-builder";
+import {
+  recipe,
+  fetchData,
+  UI,
+  NAME,
+  ifElse,
+} from "@commontools/common-builder";
 
 interface Item {
   id: string;
-  prompt: string;
+  title: string;
 }
 
-export const fetchExample = recipe<{ url: string }>("Fetch Example", ({ url }) => {
-  const { result } = fetchData<Item[]>({
-    url,
-    schema: {
-      type: "array",
-      items: {
-        type: "object",
-        properties: {
-          id: { type: "string" },
-          prompt: { type: "string" },
+export const fetchExample = recipe<{ url: string }>(
+  "Fetch Example",
+  ({ url }) => {
+    const { result } = fetchData<Item[]>({
+      url,
+      schema: {
+        type: "array",
+        items: {
+          type: "object",
+          properties: {
+            id: { type: "string" },
+            title: { type: "string" },
+          },
         },
       },
-    },
-  });
+    });
 
-  return {
-    [NAME]: "Fetch Example",
-    [UI]: html`<div>
-      ${ifElse(
-        result,
-        html`<div>${
-            result.map(({ prompt, id }) => html`<div>${prompt} - ${id}</div>`)
-        }</div>`,
-        html`<div>Loading...</div>`,
-      )}</div>`,
-    result,
-  };
-
-} );
+    return {
+      [NAME]: "Fetch Example",
+      [UI]: html`<div>
+        ${ifElse(
+          result,
+          html`<div>
+            ${result.map(({ title, id }) => html`<div>${title} - ${id}</div>`)}
+          </div>`,
+          html`<div>Loading...</div>`
+        )}
+      </div>`,
+      result,
+    };
+  }
+);
