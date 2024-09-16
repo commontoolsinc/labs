389e847918dfc4651fd691445c1aa3b7257c9abd
Bernhard Seefeld <berni@common.tools>
2024-09-16 12:21:17 -0700

    - window manager now ensures any ever opened gem is in the list of all gems - addGems doesn't add any dupes - launch now just uses openSaga, relying on it to add the gem to the list

--- typescript/packages/lookslike-high-level/src/recipes/home.ts
+++ typescript/packages/lookslike-high-level/src/recipes/home.ts
@@ -2,19 +2,25 @@ import { html } from "@commontools/common-html";
 import { recipe, lift, ID, UI } from "@commontools/common-builder";
 import { Gem, RecipeManifest } from "../data.js";
 
-const getIDsForSagasWithUI = lift((sagas: Gem[]) =>
-  sagas
-    .filter((saga) => saga[UI]) // Only show sagas with UI
-    .map((saga) => ({ id: saga[ID] }))
+const getIDsForSagasWithUI = lift<
+  { sagas: Gem[]; homeId: number },
+  { id: number }[]
+>(
+  ({ sagas, homeId }) =>
+    sagas
+      .filter((saga) => saga[UI]) // Only show sagas with UI
+      .map((saga) => ({ id: saga[ID] }))
+      .filter((saga) => saga.id != homeId) // Don't include the home screen
 );
 
 export const home = recipe<{
   sagas: Gem[];
   recipes: RecipeManifest[];
-}>("home screen", ({ sagas, recipes }) => {
+  [ID]: number;
+}>("home screen", ({ sagas, recipes, [ID]: homeId }) => {
   return {
     [UI]: html`<common-vstack
-      >${getIDsForSagasWithUI(sagas).map(
+      >${getIDsForSagasWithUI({ sagas, homeId }).map(
         (saga) => html`<div><common-saga-link saga=${saga.id}></sagaLink></div>`
       )}
       ${recipes.map(
@@ -23,7 +29,7 @@ export const home = recipe<{
             <common-recipe-link foo="bar" recipe=${recipe}></common-recipe-link>
           </div>`
       )}<common-annotation
-        query="dream fun things to explore, especially with tickets and reservations"
+        query="dream fun things to explore"
         target="-1"
         data=${{ sagas, recipes }}
       />
