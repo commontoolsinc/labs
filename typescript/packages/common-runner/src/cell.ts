bcc6b54527dad73403548abc5c02ac11c2ecb469
Bernhard Seefeld <berni@common.tools>
2024-09-19 12:19:20 -0700

    expose value proxies on simple cells

--- typescript/packages/common-runner/src/cell.ts
+++ typescript/packages/common-runner/src/cell.ts
@@ -36,6 +36,7 @@ export interface Cell<T> {
   send(value: T): void;
   sink(callback: (value: T) => void): () => void;
   key<K extends keyof T>(valueKey: K): Cell<T[K]>;
+  getAsProxy(path?: PropertyKey[]): CellProxy<T>;
 }
 
 export interface ReactiveCell<T> {
@@ -185,6 +186,8 @@ function simpleCell<T>(
         },
         key: <K extends keyof T>(key: K) =>
           cell.asSimpleCell([...path, key], log) as Cell<T[K]>,
+        getAsProxy: (path: PropertyKey[] = []) =>
+          createValueProxy(cell, [...path, ...keys], log),
       };
   return self;
 }
