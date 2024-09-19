2208784908705bb59f362fc52ffc9652bb89cc81
Bernhard Seefeld <berni@common.tools>
2024-09-19 13:30:15 -0700

    refactored this a bit and now directly uses the cell

--- typescript/packages/lookslike-high-level/src/components/iframe-view.ts
+++ typescript/packages/lookslike-high-level/src/components/iframe-view.ts
@@ -1,6 +1,7 @@
 import { LitElement, html } from "lit-element";
 import { customElement, property } from "lit/decorators.js";
 import { Cell } from "@commontools/common-runner";
+import { Ref, createRef, ref } from "lit/directives/ref.js";
 
 @customElement("common-iframe")
 export class CommonIframe extends LitElement {
@@ -8,23 +9,13 @@ export class CommonIframe extends LitElement {
   // HACK: The UI framework already translates the top level cell into updated
   // properties, but we want to only have to deal with one type of listening, so
   // we'll add a an extra level of indirection with the "context" property.
-  @property({ type: Object }) context?: { context: Cell<any> };
+  @property({ type: Object }) context?: Cell<any> | any;
 
-  private iframeElement: HTMLIFrameElement | null = null;
+  private iframeRef: Ref<HTMLIFrameElement> = createRef();
 
-  override firstUpdated() {
-    this.iframeElement = this.shadowRoot?.querySelector("iframe") || null;
-    window.addEventListener("message", this.handleMessage.bind(this));
-  }
-
-  override disconnectedCallback() {
-    super.disconnectedCallback();
-    window.removeEventListener("message", this.handleMessage.bind(this));
-  }
-
-  handleMessage(event: MessageEvent) {
+  private handleMessage = (event: MessageEvent) => {
     console.log("Received message", event);
-    if (event.source === this.iframeElement?.contentWindow) {
+    if (event.source === this.iframeRef.value?.contentWindow) {
       const { type, key, data } = event.data;
       if (typeof key !== "string") {
         console.error("Invalid key type. Expected string.");
@@ -33,35 +24,54 @@ export class CommonIframe extends LitElement {
       console.log(
         { type, key, data },
         this.context,
-        typeof this.context?.context.get === "function" ? "cell" : "not cell"
+        typeof this.context?.get === "function" ? "cell" : "not cell"
+      );
+      console.log(
+        "data",
+        this.context,
+        this.context?.get && this.context?.get(),
+        this.context?.getAsProxy && this.context?.getAsProxy()
       );
       if (type === "read" && this.context) {
-        const value = this.context.context.getAsProxy([key]);
+        const value = this.context?.getAsProxy
+          ? this.context?.getAsProxy([key])
+          : this.context?.[key];
         // TODO: This might cause infinite loops, since the data can be a graph.
-        const copy = JSON.parse(JSON.stringify(value));
+        const copy = JSON.parse(JSON.stringify(value ?? {}));
         console.log("readResponse", key, value);
-        this.iframeElement?.contentWindow?.postMessage(
+        this.iframeRef.value?.contentWindow?.postMessage(
           { type: "readResponse", key, data: copy },
           "*"
         );
       } else if (type === "write" && this.context) {
-        this.context.context.getAsProxy()[key] = data;
+        this.context.getAsProxy()[key] = data;
       }
     }
+  };
+  private boundHandleMessage = this.handleMessage.bind(this);
+
+  override connectedCallback() {
+    super.connectedCallback();
+    window.addEventListener("message", this.boundHandleMessage);
+  }
+
+  override disconnectedCallback() {
+    super.disconnectedCallback();
+    window.removeEventListener("message", this.boundHandleMessage);
   }
 
   override render() {
     return html`
       <iframe
+        ${ref(this.iframeRef)}
         sandbox="allow-scripts"
         .srcdoc=${this.src}
         height="512px"
         width="100%"
-        @load=${() => {
-          if (this.iframeElement?.contentWindow) {
-            this.iframeElement.contentWindow.postMessage({ type: "init" }, "*");
-          }
-        }}
+        @load=${this.iframeRef.value?.contentWindow?.postMessage(
+          { type: "init" },
+          "*"
+        )}
       ></iframe>
     `;
   }
