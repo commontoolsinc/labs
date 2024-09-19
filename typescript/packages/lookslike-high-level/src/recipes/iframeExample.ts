4a7dd66caba904aed0fa4c375115492b3e6688c3
Bernhard Seefeld <berni@common.tools>
2024-09-19 12:19:47 -0700

    use proxy to read data + related changes

--- typescript/packages/lookslike-high-level/src/recipes/iframeExample.ts
+++ typescript/packages/lookslike-high-level/src/recipes/iframeExample.ts
@@ -1,34 +1,36 @@
 import { html } from "@commontools/common-html";
 import {
   recipe,
-  fetchData,
   UI,
   NAME,
-  ifElse,
   lift,
   generateData,
   handler,
+  cell,
 } from "@commontools/common-builder";
-import { cell} from "@commontools/common-runner";
 
 const formatData = lift(({ obj }) => {
-  console.log('stringify', obj)
-  return JSON.stringify(obj, null, 2)
-})
+  console.log("stringify", obj);
+  return JSON.stringify(obj, null, 2);
+});
 
-const tap = lift(x => {
+const tap = lift((x) => {
   console.log(x);
   return x;
-})
-
-// TODO(ben): this is the shared state with the iframe, unsure if this is the right way to orchestrate
-const context = cell({ exampleKey: 456 })
-const data = context.getAsProxy()
-const getHtml = lift((obj) => obj?.html)
+});
 
 const updateValue = handler<{ detail: { value: string } }, { value: string }>(
   ({ detail }, state) => detail?.value && (state.value = detail.value)
 );
+
+const generate = handler<void, { prompt: string; query: string }>(
+  (_, state) => {
+    state.query = state.prompt;
+    console.log("generarting", state.query);
+  }
+);
+
+const maybeHTML = lift(({ result }) => result?.html ?? "");
 
 const viewSystemPrompt = `generate a complete HTML document within a json block , e.g. \`\`\`json{ html: '...'}\`\`\`
 
@@ -51,39 +53,44 @@ const viewSystemPrompt = `generate a complete HTML document within a json block 
     type: 'write',
     key: 'exampleKey',
     data: 'Example data to write'
-  }, '*');`
+  }, '*');`;
 
-export const iframeExample = recipe<{ prompt: string }>(
+export const iframeExample = recipe<{ prompt: string; data: any }>(
   "iFrame Example",
-  ({ prompt }) => {
-    const dataPayload = formatData({ obj: data })
-    tap({ data, dataPayload })
-    prompt.setDefault("make a red circle and say hello")
+  ({ prompt, data }) => {
+    tap({ data });
+    prompt.setDefault("make a red circle and say the message from the data");
+    data.setDefault({ message: "hello" });
 
-    const response = generateData({ prompt, system: viewSystemPrompt })
-    tap(response)
-    tap(response.result)
-    response.result.setDefault({ html: '' })
-    const srcHtml = getHtml(response.result)
+    const query = cell<string>();
+    const response = generateData<{ html: string }>({
+      prompt: query,
+      system: viewSystemPrompt,
+    });
+    tap({ response });
+    tap({ result: response.result });
 
-    // TOOD(ben): dataPayload does not update as I would expect when written to
-    // via the postMessage API
     return {
       [NAME]: "iFrame Example",
       [UI]: html`<div>
-        <div>${dataPayload}</div>
-        <div>${response.result}</div>
+        <div>${formatData({ obj: data })}</div>
+        <div>${maybeHTML({ result: response.result })}</div>
         <common-input
           value=${prompt}
           placeholder="Prompt"
           oncommon-input=${updateValue({ value: prompt })}
         ></common-input>
+        <common-button onclick=${generate({ prompt, query })}
+          >Generate</common-button
+        >
 
-        <common-iframe src=${srcHtml} context=${data} ></common-iframe>
+        <common-iframe
+          src=${maybeHTML({ result: response.result })}
+          context=${{ context: data }}
+        ></common-iframe>
       </div>`,
-      dataPayload,
       response,
-      data
+      data,
     };
   }
 );
