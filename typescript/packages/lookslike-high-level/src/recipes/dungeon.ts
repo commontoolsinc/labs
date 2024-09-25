b63b302b7db54614f81bce0b08f58733f5336d8b
Ben Follington <5009316+bfollington@users.noreply.github.com>
2024-09-25 12:47:13 +1000

    Add Hp, Xp and inventory

--- typescript/packages/lookslike-high-level/src/recipes/dungeon.ts
+++ typescript/packages/lookslike-high-level/src/recipes/dungeon.ts
@@ -11,14 +11,17 @@ interface DungeonGame {
   walls: Position[],
   name: string;
   player: Position;
+  hp: number;
+  xp: number;
+  inventory: string[];
 }
 
 const updateValue = handler<{ detail: { value: string } }, { value: string }>(
   ({ detail }, state) => detail?.value && (state.value = detail.value)
 );
 
-const cloneRecipe = handler<void, DungeonGame>((_, { walls, player, name, width, height }) => {
-  launch(iframeExample, { data: { walls, player, name, width, height }, title: 'Dungeon', prompt: 'top down grid view of map showing player position' });
+const cloneRecipe = handler<void, DungeonGame>((_, { walls, player, name, width, height, hp, xp, inventory }) => {
+  launch(iframeExample, { data: { walls, player, name, width, height, hp, xp, inventory }, title: 'Dungeon', prompt: 'top down grid view of map showing player position' });
 });
 
 const createGrid = (w: number, h: number, init: (x: number, y: number) => number): number[][] => {
@@ -42,12 +45,15 @@ const createNoiseGrid = (w: number, h: number): Position[] => {
   return walls;
 };
 
-export const dungeon = recipe<DungeonGame>("Import Calendar", ({ walls, player, name, width, height }) => {
+export const dungeon = recipe<DungeonGame>("Import Calendar", ({ walls, player, name, width, height, hp, xp, inventory }) => {
   walls.setDefault(createNoiseGrid(10, 10))
   player.setDefault({ x: 5, y: 5})
   width.setDefault(10)
   height.setDefault(10)
   name.setDefault("Dungeon Adventure")
+  hp.setDefault(100)
+  xp.setDefault(0)
+  inventory.setDefault(['sword', 'shield', 'crumpled felt hat'])
 
   return {
     [NAME]: "Dungeon Adventure",
@@ -59,7 +65,7 @@ export const dungeon = recipe<DungeonGame>("Import Calendar", ({ walls, player, 
             oncommon-input=${updateValue({ value: name })}
           ></common-input>
           <h1>Welcome, ${name}!</h1>
-        <common-button onclick=${cloneRecipe({ walls, player, name, width, height })}
+        <common-button onclick=${cloneRecipe({ walls, player, name, width, height, hp, xp, inventory })}
           >Begin Adventure</common-button
         >
       </div>
