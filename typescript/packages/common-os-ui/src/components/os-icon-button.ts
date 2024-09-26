87d94e317f30ee6d1326bae3c53486d47f0fb376
Gordon Brander <gordonbrander@users.noreply.github.com>
2024-09-26 15:30:57 -0400

    Add icon button active state

--- typescript/packages/common-os-ui/src/components/os-icon-button.ts
+++ typescript/packages/common-os-ui/src/components/os-icon-button.ts
@@ -8,16 +8,47 @@ export class OsIconButton extends LitElement {
     base,
     css`
       :host {
+        display: inline-block;
+        --u-button-size: var(--u-min-touch-size);
+      }
+
+      .icon-button {
         display: block;
+        cursor: pointer;
+        border: 0;
+        --webkit-appearance: none;
+        appearance: none;
         background-color: var(--bg-3);
         border-radius: var(--u-radius);
-        width: var(--u-min-touch-size);
-        height: var(--u-min-touch-size);
+        width: var(--u-button-size);
+        height: var(--u-button-size);
+        overflow: hidden;
+        position: relative;
+      }
+
+      .icon-button::before {
+        content: "";
+        background-color: var(--bg-scrim);
+        width: var(--u-button-size);
+        height: var(--u-button-size);
+        top: 0;
+        left: 0;
+        opacity: 0;
+        position: absolute;
+        pointer-events: none;
+      }
+
+      .icon-button:active::before {
+        opacity: 1;
       }
     `,
   ];
 
   override render() {
-    return html`<slot></slot> `;
+    return html`
+      <button class="icon-button">
+        <slot></slot>
+      </button>
+    `;
   }
 }
