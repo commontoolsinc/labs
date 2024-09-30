3777fb7f8817f90cd8da85e68c1a402a74ebcbd6
Gordon Brander <gordonbrander@users.noreply.github.com>
2024-09-30 09:44:48 -0400

    Add activated property
    - Allows setting activated state on button

--- typescript/packages/common-os-ui/src/components/os-icon-button.ts
+++ typescript/packages/common-os-ui/src/components/os-icon-button.ts
@@ -2,6 +2,14 @@ import { LitElement, css, html } from "lit-element";
 import { customElement, property } from "lit/decorators.js";
 import { base } from "../shared/styles.js";
 
+/**
+ * A button with an icon inside, using the `os-icon` component.
+ *
+ * @element os-icon-button
+ *
+ * @prop {string} icon - The name of the icon to display in the button.
+ * @prop {boolean} activated - toggle the button's activated state.
+ */
 @customElement("os-icon-button")
 export class OsIconButton extends LitElement {
   static override styles = [
@@ -26,22 +34,26 @@ export class OsIconButton extends LitElement {
         height: var(--u-button-size);
         overflow: hidden;
         position: relative;
-      }
 
-      .icon-button::before {
-        content: "";
-        background-color: var(--bg-scrim);
-        width: var(--u-button-size);
-        height: var(--u-button-size);
-        top: 0;
-        left: 0;
-        opacity: 0;
-        position: absolute;
-        pointer-events: none;
-      }
+        &::before {
+          content: "";
+          background-color: var(--bg-scrim);
+          width: var(--u-button-size);
+          height: var(--u-button-size);
+          top: 0;
+          left: 0;
+          opacity: 0;
+          position: absolute;
+          pointer-events: none;
+        }
 
-      .icon-button:active::before {
-        opacity: 1;
+        &:active::before {
+          opacity: 1;
+        }
+
+        :host([activated]) &::before {
+          opacity: 1;
+        }
       }
     `,
   ];
