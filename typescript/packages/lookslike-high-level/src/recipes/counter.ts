7f4df1069245a5d0cd722eac49c68d89058db792
Bernhard Seefeld <berni@common.tools>
2024-10-03 16:52:56 -0700

    fix counters by nesting value again :(

--- typescript/packages/lookslike-high-level/src/recipes/counter.ts
+++ typescript/packages/lookslike-high-level/src/recipes/counter.ts
@@ -1,27 +1,34 @@
 import { html } from "@commontools/common-html";
 import { recipe, NAME, UI, handler } from "@commontools/common-builder";
 
-const inc = handler<{}, { count: number }>(
-    ({}, state) => { state.count += 1; }
-);
+const inc = handler<{}, { count: { value: number } }>(({}, state) => {
+  state.count.value += 1;
+});
 
 const updateValue = handler<{ detail: { value: string } }, { value: string }>(
-    ({ detail }, state) => { detail?.value && (state.value = detail.value) }
+  ({ detail }, state) => {
+    detail?.value && (state.value = detail.value);
+  }
 );
 
-export const counter = recipe<{ title: string; count: number }>(
-    "counter",
-    ({ title, count }) => {
-        count.setDefault(0);
-        title.setDefault("untitled counter");
+export const counter = recipe<{ title: string; count: { value: number } }>(
+  "counter",
+  ({ title, count }) => {
+    count.setDefault({ value: 0 });
+    title.setDefault("untitled counter");
 
-        return {
-            [NAME]: title,
-            [UI]: html`<div>
-                    <common-input value=${title} placeholder="Name of counter" oncommon-input=${updateValue({ value: title })}></common-input>
-                    <p>${count}</p>
-                    <button onclick=${inc({ count })}>Inc</button>
-                </div>`,
-            count
-        }
-    })
+    return {
+      [NAME]: title,
+      [UI]: html`<div>
+        <common-input
+          value=${title}
+          placeholder="Name of counter"
+          oncommon-input=${updateValue({ value: title })}
+        ></common-input>
+        <p>${count}</p>
+        <button onclick=${inc({ count })}>Inc</button>
+      </div>`,
+      count,
+    };
+  }
+);
