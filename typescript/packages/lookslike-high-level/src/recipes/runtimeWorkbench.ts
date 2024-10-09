7dd6980ec719145630b73fb204c96b3949da5529
Bernhard Seefeld <berni@common.tools>
2024-10-09 12:14:16 -0700

    * made navigateTo a raw module (mostly so that all `lift` are indeed isolatable) * removed launch & converted remaining uses to navigateTo * fixed a bunch of typescript complaints

--- typescript/packages/lookslike-high-level/src/recipes/runtimeWorkbench.ts
+++ typescript/packages/lookslike-high-level/src/recipes/runtimeWorkbench.ts
@@ -1,5 +1,12 @@
 import { html } from "@commontools/common-html";
-import { recipe, NAME, UI, isolated, str, lift } from "@commontools/common-builder";
+import {
+  recipe,
+  NAME,
+  UI,
+  isolated,
+  str,
+  lift,
+} from "@commontools/common-builder";
 
 const uniqueUrls = isolated<{ data: string }, { result: string }>(
   { data: { tag: "string", val: "[]" } },
@@ -8,9 +15,9 @@ const uniqueUrls = isolated<{ data: string }, { result: string }>(
     const parsed = JSON.parse(data);
     const uniqueUrls = new Set();
     if (Array.isArray(parsed.items)) {
-      parsed.items.forEach(item => {
-        if (item && typeof item === 'object' && 'import/url' in item) {
-          uniqueUrls.add(item['import/url']);
+      parsed.items.forEach((item: any) => {
+        if (item && typeof item === "object" && "import/url" in item) {
+          uniqueUrls.add(item["import/url"]);
         }
       });
     }
@@ -24,23 +31,24 @@ const stringify = lift(({ obj }) => {
   return JSON.stringify(obj || {}, null, 2);
 });
 
-export const runtimeWorkbench = recipe<{ a: string; b: string; data: any; }>(
+export const runtimeWorkbench = recipe<{ a?: string; b?: string; data: any }>(
   "isolated hello world",
   ({ a, b, data }) => {
     a.setDefault("hello");
     b.setDefault("world");
     data.setDefault([{ a: "yello", b: "world" }]);
 
-    const { result: urls } = uniqueUrls({ data: stringify({ obj: { items: data } }) });
+    const { result: urls } = uniqueUrls({
+      data: stringify({ obj: { items: data } }),
+    });
 
     return {
       [NAME]: str`${a} ${b}`,
       [UI]: html`<div>
+        <div>${urls}</div>
 
-          <div>${urls}</div>
-
-          <pre>${stringify({ obj: data })}</pre></div>
-          `,
+        <pre>${stringify({ obj: data })}</pre>
+      </div> `,
       urls: urls,
     };
   }
