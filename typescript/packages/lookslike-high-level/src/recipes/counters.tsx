3ff6b10f5dc67253912f17b24c3b5c9cb36448e4
Jesse Andrews <anotherjesse@gmail.com>
2024-10-30 13:55:28 -0700

    rewrite counters to jsx and delete item

--- typescript/packages/lookslike-high-level/src/recipes/counters.tsx
+++ typescript/packages/lookslike-high-level/src/recipes/counters.tsx
@@ -1,54 +1,64 @@
 import { h } from "@commontools/common-html";
-import { recipe, NAME, UI, handler, lift } from "@commontools/common-builder";
+import { recipe, NAME, UI, handler, lift, str } from "@commontools/common-builder";
+import { z } from "zod";
 
-const inc = handler<{}, { count: number }>(({}, state) => {
-  state.count += 1;
+const Counter = z.object({ title: z.string(), count: z.number() });
+type Counter = z.infer<typeof Counter>;
+
+const Counters = z.object({
+  items: z.array(Counter).default([]),
+  title: z.string().default("Counters")
+}).describe("Counters");
+type Counters = z.infer<typeof Counters>;
+
+const inc = handler<{}, { item: Counter }>(({ }, { item }) => {
+  item.count += 1;
 });
 
-const updateRandomItem = handler<
-  {},
-  { items: { title: string; count: number }[] }
->(({}, state) => {
-  // TODO(ja): if a handler throws an exception recipes
-  // seems to stop updating / future handlers are not called.
+const updateRandomItem = handler<{}, { items: Counter[] }>(({ }, state) => {
   if (state.items.length > 0) {
     state.items[Math.floor(Math.random() * state.items.length)].count += 1;
   }
 });
 
-const addItem = handler<{}, { items: { title: string; count: number }[] }>(
-  ({}, state) => {
-    state.items.push({ title: `item ${state.items.length + 1}`, count: 0 });
-  }
+const addItem = handler<{}, { items: Counter[] }>(({ }, state) => {
+  state.items.push({ title: `item ${state.items.length + 1}`, count: 0 });
+});
+
+const removeItem = handler<{}, { items: Counter[], item: Counter }>(({ }, state) => {
+  // fixme(ja): findIndex doesn't work here
+  // fixme(ja): filter doesn't work here
+  const index = state.items.findIndex((i) => i.title === state.item.title);
+  state.items.splice(index, 1);
+});
+
+const sum = lift(({ items }: { items: Counter[] }) =>
+  items.reduce((acc: number, item: Counter) => acc + item.count, 0)
 );
 
-const sum = lift(({ items }) =>
-  items.reduce((acc: number, item: { count: number }) => acc + item.count, 0)
-);
-
-export const counters = recipe<{ items: { title: string; count: number }[] }>(
-  "counters",
-  ({ items }) => {
-    items.setDefault([]);
-
+export const counters = recipe(
+  Counters,
+  ({ items, title }) => {
     const total = sum({ items });
 
     return {
-      [NAME]: "counters",
-      [UI]: <div>
+      [NAME]: str`${title} counters`,
+      [UI]: <os-container>
+        <h1>{title}</h1>
         <ul>
           {items.map(
-            ({ title, count }) =>
+            (item) =>
               <li>
-                {title} - {count}
-                <button onclick={inc({ count })}>Inc</button>
+                {item.title} - {item.count}
+                <button onclick={inc({ item })}>inc</button>
+                <button onclick={removeItem({ item, items })}>remove</button>
               </li>
           )}
         </ul>
         <p>Total: {total}</p>
         <button onclick={updateRandomItem({ items })}>Inc random item</button>
         <button onclick={addItem({ items })}>Add new item</button>
-      </div>,
+      </os-container>,
     };
   }
 );
