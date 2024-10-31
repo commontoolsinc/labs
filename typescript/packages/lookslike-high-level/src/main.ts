0d7a275fc962d10eedc009d085f7361ab8db5924
Bernhard Seefeld <berni@common.tools>
2024-10-31 14:31:08 -0700

    control debug output via env variable

--- typescript/packages/lookslike-high-level/src/main.ts
+++ typescript/packages/lookslike-high-level/src/main.ts
@@ -7,11 +7,11 @@ export { components as myComponents } from "./components.js";
 import { openCharm } from "./data.js";
 import "./router.js";
 
-setDebug(false);
+setDebug(!!(import.meta as any).env.VITE_DEBUG);
 
 document.addEventListener("DOMContentLoaded", () => {
   const windowManager = document.getElementById(
-    "window-manager"
+    "window-manager",
   )! as CommonWindowManager;
   openCharm.set(windowManager.openCharm.bind(windowManager));
 });
