f32674bfb13c1c947f7a518e17360906683798cb
Jesse Andrews <anotherjesse@gmail.com>
2024-11-02 16:08:42 -0700

    hackathon demo

--- typescript/packages/lookslike-high-level/src/recipes/latentExplorer.tsx
+++ typescript/packages/lookslike-high-level/src/recipes/latentExplorer.tsx
@@ -11,9 +11,21 @@ const deleteItem = handler<{}, { item: Picture }>((e, state) => {
     return fetchData(prepDeleteRequest({ entity: state.item, schema: Picture }));
 })
 
-const add = handler<{ detail: { value: string, key: string } }, {}>(
-    ({ detail }, { }) => {
+const add = handler<{ detail: { value: string, key: string } }, {s: string}>(
+    ({ detail }, { s }) => {
         console.log("add", detail);
+        if (s) {
+            return fetchData(
+                prepInsertRequest({
+                    entity: {
+                        prompt: s,
+                    }
+                }),
+            );
+    
+        }
+
+        
         if (detail?.key !== "Enter") return;
         const prompt = detail.value;
         return fetchData(
@@ -44,33 +56,62 @@ const picture = recipe(Picture, ({ prompt, item }) => {
     return {
         [NAME]: "prompt",
         [UI]: <span class="latentItem">
-            <img src={genImage({ prompt })} width="200" height="200" />
+            <img src={genImage({ prompt: prompt })} />
             <button onclick={deleteItem({ item })}>Remove</button>
         </span>
     }
 });
+
+const prepSuggestions = lift(({ prompts }) => {
+    if (!prompts) return {};
+    return {
+        system: `You are a manic moderator for an entertaining discussion.
+        Suggest new guests to invite to the discussion based on the current guests provided by the user.  Reply with a list of names in JSON, then after the json describe why!`,
+        model: "groq:llama-3.1-8b-instant",
+        messages: [`Current guests: \n- ${prompts.map((p) => p.prompt).join("\n - ")}`, '```json\n{"suggestions": ["'],
+        stop: "\n```"
+    }
+});
+
+const grabJson = lift<{ result?: string }, string[]>(({ result }) => {
+    if (!result) {
+        return [];
+    }
+    const jsonMatch = result.match(/```json\n([\s\S]+?)```/);
+    if (!jsonMatch) {
+        console.error("No JSON found in text:", result);
+        return [];
+    }
+    let rawData = JSON.parse(jsonMatch[1]);
+    return rawData.suggestions;
+})
+
 
 export const latentExplorer = recipe(LatentExplorer,
     ({ prompts }) => {
 
         const { result, query } = schemaQuery(Picture)
 
-        // const suggestions = grabSuggestions(llm(prepSuggestions({ prompts })));
-        tap({ result })
+        const suggestions = grabJson(llm(prepSuggestions({ prompts: result })));
+        // tap({ result })
 
         return {
-            [NAME]: "Latent Explorer",
+            [NAME]: "Latent Guests",
             [UI]: <os-container>
                 <style type="text/css">
                     {`
                     .latentItem {
                         display: inline-block;
                         background-color: #f0f0f0;
-                        margin: 10px;
+                        margin: 2px;
                         position: relative;
                     }
                     .latentItem button {
                         display: none;
+                    }
+                    .latentItem img {
+                        width: 192px;
+                        height: 192px;
                     }
                     .latentItem:hover button {
                         display: block;
@@ -85,11 +126,15 @@ export const latentExplorer = recipe(LatentExplorer,
                   `}
                 </style>
                 <common-input
-                    placeholder="prompt"
+                    placeholder="new guest"
                     oncommon-keydown={add({ prompts })} />
                 {
                     result.map((item) => picture({ prompt: item.prompt, item })[UI])
                 }
+                <ul>
+                    {suggestions.map((s) => <li onclick={add({ prompts, s })}>{s}</li>)}
+                </ul>
+
             </os-container>,
             query,
             data: result,
