943b8154156ef8b9cca2145cbca7bdd7917aa3b1
Irakli Gozalishvili <contact@gozala.io>
2024-11-06 14:10:28 -0800

    chore: implement jsx

---
+++ typescript/packages/common-system/src/index.ts
@@ -0,0 +1,1 @@
+export { h, Fragment } from "./jsx.js";
--- typescript/packages/lookslike-high-level/src/recipes/charm.tsx
+++ typescript/packages/lookslike-high-level/src/recipes/charm.tsx
@@ -1,27 +1,8 @@
 import { NAME, UI, recipe } from "@commontools/common-builder";
 import { refer, $ } from "synopsys";
-import * as DOM from "@gozala/co-dom";
-import { h } from "@commontools/common-html";
+import { html } from "@commontools/common-html";
+import { h } from "@commontools/common-system";
 import z from "zod";
-
-export const on = (
-  event: DOM.EncodedEvent["type"],
-  attribute: string = `~/on/${event}`,
-) =>
-  DOM.on(event, {
-    /**
-     *
-     * @param {DOM.EncodedEvent} event
-     */
-    decode(event) {
-      return {
-        message: /** @type {DB.Fact} */ [
-          attribute,
-          /** @type {any & DB.Entity} */ event,
-        ],
-      };
-    },
-  });
 
 const entity = refer({ clicker: { v: 22 } });
 
@@ -42,16 +23,10 @@ const Clicker = {
           Assert: [
             entity,
             "~/common/ui",
-            DOM.div(
-              [],
-              [
-                DOM.div([], [DOM.text(String(count))]),
-                DOM.button(
-                  [on("click", "~/on/click")],
-                  [DOM.text("Click me!")],
-                ),
-              ],
-            ),
+            <div>
+              <div>{count}</div>
+              <button onclick="~/on/click">Click me!</button>
+            </div>,
           ],
         },
       ];
@@ -83,6 +58,6 @@ const Clicker = {
 export const charmExample = recipe(z.object({}), () => {
   return {
     [NAME]: "Charm",
-    [UI]: <common-charm spell={() => Clicker} entity={() => entity} />,
+    [UI]: html`<common-charm spell=${() => Clicker} entity=${() => entity} />`,
   };
 });
