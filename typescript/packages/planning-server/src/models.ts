13a4ab48fffb381e9dcbbaa03014b41da65e9d0d
jakedahn <jake@markupisart.com>
2024-11-06 11:07:16 -0700

    [planning]: model registry now has a capabilities map

--- typescript/packages/planning-server/src/models.ts
+++ typescript/packages/planning-server/src/models.ts
@@ -4,10 +4,20 @@ import { openai } from "npm:@ai-sdk/openai";
 import { vertex } from "npm:@ai-sdk/google-vertex";
 import { ollama } from "ollama-ai-provider";
 
-type ModelConfig = {
-  model: any;
+type Capabilities = {
   contextWindow: number;
   maxOutputTokens: number;
+  streaming: boolean;
+  systemPrompt: boolean;
+  systemPromptWithImages?: boolean;
+  stopSequences: boolean;
+  prefill: boolean;
+  images: boolean;
+};
+
+type ModelConfig = {
+  model: any;
+  capabilities: Capabilities;
 };
 
 export const MODELS: Record<string, ModelConfig> = {};
@@ -15,23 +25,20 @@ export const MODELS: Record<string, ModelConfig> = {};
 const addModel = ({
   provider,
   name,
-  contextWindow,
-  maxOutputTokens,
   aliases,
+  capabilities,
 }: {
   provider: typeof anthropic | typeof groq | typeof openai | typeof vertex;
   name: string;
-  contextWindow: number;
-  maxOutputTokens: number;
   aliases: string[];
+  capabilities: Capabilities;
 }) => {
   const model = provider(
     name.includes(":") ? name.split(":").slice(1).join(":") : name,
   );
   const config: ModelConfig = {
     model,
-    contextWindow,
-    maxOutputTokens,
+    capabilities,
   };
   MODELS[name] = config;
   for (const alias of aliases) {
@@ -43,34 +50,46 @@ if (Deno.env.get("ANTHROPIC_API_KEY")) {
   addModel({
     provider: anthropic,
     name: "anthropic:claude-3-5-haiku-20241022",
-    contextWindow: 200000,
-    maxOutputTokens: 8192,
-    aliases: [
-      "anthropic:claude-3-5-haiku-latest",
-      "claude-3-5-haiku",
-    ],
+    aliases: ["anthropic:claude-3-5-haiku-latest", "claude-3-5-haiku"],
+    capabilities: {
+      contextWindow: 200_000,
+      maxOutputTokens: 8192,
+      images: true,
+      prefill: true,
+      systemPrompt: true,
+      stopSequences: true,
+      streaming: true,
+    },
   });
 
   addModel({
     provider: anthropic,
     name: "anthropic:claude-3-5-sonnet-20241022",
-    contextWindow: 200000,
-    maxOutputTokens: 8192,
-    aliases: [
-      "anthropic:claude-3-5-sonnet-latest",
-      "claude-3-5-sonnet",
-    ],
+    aliases: ["anthropic:claude-3-5-sonnet-latest", "claude-3-5-sonnet"],
+    capabilities: {
+      contextWindow: 200_000,
+      maxOutputTokens: 8192,
+      images: true,
+      prefill: true,
+      systemPrompt: true,
+      stopSequences: true,
+      streaming: true,
+    },
   });
 
   addModel({
     provider: anthropic,
     name: "anthropic:claude-3-opus-20240229",
-    contextWindow: 200000,
-    maxOutputTokens: 4096,
-    aliases: [
-      "anthropic:claude-3-opus-latest",
-      "claude-3-opus",
-    ],
+    aliases: ["anthropic:claude-3-opus-latest", "claude-3-opus"],
+    capabilities: {
+      contextWindow: 200_000,
+      maxOutputTokens: 4096,
+      images: true,
+      prefill: true,
+      systemPrompt: true,
+      stopSequences: true,
+      streaming: true,
+    },
   });
 }
 
@@ -78,47 +97,76 @@ if (Deno.env.get("GROQ_API_KEY")) {
   addModel({
     provider: groq,
     name: "groq:llama-3.1-70b-versatile",
-    contextWindow: 128000,
-    maxOutputTokens: 8000,
-    aliases: [
-      "groq:llama-3.1-70b",
-    ],
+    aliases: ["groq:llama-3.1-70b"],
+    capabilities: {
+      contextWindow: 128_000,
+      maxOutputTokens: 8000,
+      images: false,
+      prefill: true,
+      systemPrompt: true,
+      stopSequences: true,
+      streaming: false,
+    },
   });
 
   addModel({
     provider: groq,
     name: "groq:llama-3.1-8b-instant",
-    contextWindow: 128000,
-    maxOutputTokens: 8000,
-    aliases: [],
+    aliases: ["llama-3.1-8b"],
+    capabilities: {
+      contextWindow: 128_000,
+      maxOutputTokens: 8000,
+      images: false,
+      prefill: true,
+      systemPrompt: true,
+      stopSequences: true,
+      streaming: false,
+    },
   });
 
   addModel({
     provider: groq,
     name: "groq:llama-3.2-11b-vision-preview",
-    contextWindow: 128000,
-    maxOutputTokens: 8000,
-    aliases: [
-      "groq:llama-3.2-11b-vision",
-    ],
+    aliases: ["groq:llama-3.2-11b-vision", "llama-3.2-11b-vision"],
+    capabilities: {
+      contextWindow: 128_000,
+      maxOutputTokens: 8000,
+      images: true,
+      prefill: true,
+      systemPrompt: true,
+      stopSequences: true,
+      streaming: false,
+    },
   });
 
   addModel({
     provider: groq,
     name: "groq:llama-3.2-90b-vision-preview",
-    contextWindow: 128000,
-    maxOutputTokens: 8000,
-    aliases: [
-      "groq:llama-3.2-90b-vision",
-    ],
+    aliases: ["groq:llama-3.2-90b-vision", "llama-3.2-90b-vision"],
+    capabilities: {
+      contextWindow: 128_000,
+      maxOutputTokens: 8000,
+      images: true,
+      prefill: true,
+      systemPrompt: true,
+      stopSequences: true,
+      streaming: false,
+    },
   });
 
   addModel({
     provider: groq,
     name: "groq:llama-3.2-3b-preview",
-    contextWindow: 128000,
-    maxOutputTokens: 8000,
-    aliases: [],
+    aliases: ["groq:llama-3.2-3b", "llama-3.2-3b"],
+    capabilities: {
+      contextWindow: 128_000,
+      maxOutputTokens: 8000,
+      images: false,
+      prefill: true,
+      systemPrompt: true,
+      stopSequences: true,
+      streaming: false,
+    },
   });
 }
 
@@ -126,61 +174,79 @@ if (Deno.env.get("OPENAI_API_KEY")) {
   addModel({
     provider: openai,
     name: "openai:gpt-4o-2024-08-06",
-    contextWindow: 128000,
-    maxOutputTokens: 16384,
-    aliases: [
-      "openai:gpt-4o",
-      "openai:gpt-4o-latest",
-      "gpt-4o",
-    ],
+    aliases: ["openai:gpt-4o", "openai:gpt-4o-latest", "gpt-4o"],
+    capabilities: {
+      contextWindow: 128_000,
+      maxOutputTokens: 16384,
+      images: true,
+      prefill: false,
+      systemPrompt: true,
+      stopSequences: true,
+      streaming: true,
+    },
   });
 
   addModel({
     provider: openai,
     name: "openai:gpt-4o-mini-2024-07-18",
-    contextWindow: 128000,
-    maxOutputTokens: 16384,
-    aliases: [
-      "openai:gpt-4o-mini-latest",
-      "openai:gpt-4o-mini",
-      "gpt-4o-mini",
-    ],
+    aliases: ["openai:gpt-4o-mini-latest", "openai:gpt-4o-mini", "gpt-4o-mini"],
+    capabilities: {
+      contextWindow: 128_000,
+      maxOutputTokens: 16384,
+      images: true,
+      prefill: false,
+      systemPrompt: true,
+      stopSequences: true,
+      streaming: true,
+    },
   });
 
   addModel({
     provider: openai,
     name: "openai:o1-preview-2024-09-12",
-    contextWindow: 128000,
-    maxOutputTokens: 32768,
-    aliases: [
-      "openai:o1-preview-latest",
-      "openai:o1-preview",
-      "o1-preview",
-    ],
+    aliases: ["openai:o1-preview-latest", "openai:o1-preview", "o1-preview"],
+    capabilities: {
+      contextWindow: 128_000,
+      maxOutputTokens: 32768,
+      images: false,
+      prefill: false,
+      systemPrompt: false,
+      stopSequences: false,
+      streaming: false,
+    },
   });
 
   addModel({
     provider: openai,
     name: "openai:o1-mini-2024-09-12",
-    contextWindow: 128000,
-    maxOutputTokens: 65536,
-    aliases: [
-      "openai:o1-mini-latest",
-      "openai:o1-mini",
-      "o1-mini",
-    ],
+    aliases: ["openai:o1-mini-latest", "openai:o1-mini", "o1-mini"],
+    capabilities: {
+      contextWindow: 128_000,
+      maxOutputTokens: 65536,
+      images: false,
+      prefill: false,
+      systemPrompt: false,
+      stopSequences: false,
+      streaming: false,
+    },
   });
 }
 
 if (Deno.env.get("OLLAMA_API_MODEL")) {
+  // NOTE: Ollama supports many models, so we leave the capabilities all true.
   addModel({
     provider: ollama,
     name: `ollama:${Deno.env.get("OLLAMA_API_MODEL")}`,
-    contextWindow: 128000,
-    maxOutputTokens: 8000,
-    aliases: [
-      `ollama`,
-    ],
+    aliases: [`ollama`],
+    capabilities: {
+      contextWindow: 128_000,
+      maxOutputTokens: 8000,
+      images: true,
+      prefill: true,
+      systemPrompt: true,
+      stopSequences: true,
+      streaming: true,
+    },
   });
 }
 
@@ -191,21 +257,31 @@ if (
   addModel({
     provider: vertex,
     name: "google:gemini-1.5-flash-002",
-    contextWindow: 1_000_000,
-    maxOutputTokens: 8192,
-    aliases: [
-      "google:gemini-1.5-flash",
-    ],
+    aliases: ["google:gemini-1.5-flash", "gemini-1.5-flash"],
+    capabilities: {
+      contextWindow: 1_000_000,
+      maxOutputTokens: 8192,
+      images: true,
+      prefill: true,
+      systemPrompt: true,
+      stopSequences: true,
+      streaming: true,
+    },
   });
 
   addModel({
     provider: vertex,
     name: "google:gemini-1.5-pro-002",
-    contextWindow: 1_000_000,
-    maxOutputTokens: 8192,
-    aliases: [
-      "google:gemini-1.5-pro",
-    ],
+    aliases: ["google:gemini-1.5-pro", "gemini-1.5-pro"],
+    capabilities: {
+      contextWindow: 1_000_000,
+      maxOutputTokens: 8192,
+      images: true,
+      prefill: true,
+      systemPrompt: true,
+      stopSequences: true,
+      streaming: true,
+    },
   });
 }
 
