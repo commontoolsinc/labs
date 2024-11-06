aaf1f4767f0fcf30b737807b6eeeb23b5e7c34a1
Bernhard Seefeld <berni@common.tools>
2024-11-05 17:22:12 -0800

    use alt key to turn on dragging

--- typescript/packages/lookslike-high-level/src/components/common-draggable.ts
+++ typescript/packages/lookslike-high-level/src/components/common-draggable.ts
@@ -1,22 +1,73 @@
-import { LitElement, html } from "lit-element";
-import { customElement, property } from "lit/decorators.js";
+import { LitElement, html, css } from "lit-element";
+import { customElement, property, state } from "lit/decorators.js";
+import { classMap } from "lit/directives/class-map.js";
 import { RendererCell } from "@commontools/common-runner";
 
 @customElement("common-draggable")
 export default class DraggableElement extends LitElement {
+  static override styles = css`
+    .draggable {
+      position: relative;
+    }
+
+    .draggable-scrim {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      z-index: 2;
+      display: none;
+    }
+
+    .draggable.dragging .draggable-scrim {
+      display: block;
+    }
+  `;
+
+  @state() _altKey = false;
   @property({ type: Object }) entity: RendererCell<any> | undefined = undefined;
 
-  override render() {
-    return html`
-      <div draggable="true" @dragstart=${this.handleDragStart}>
-        <div>Drag me</div>
+  override connectedCallback() {
+    super.connectedCallback();
+    window.addEventListener("keydown", this.#handleKeyDown);
+    window.addEventListener("keyup", this.#handleKeyUp);
+  }
 
+  override disconnectedCallback() {
+    super.disconnectedCallback();
+    window.removeEventListener("keydown", this.#handleKeyDown);
+    window.removeEventListener("keyup", this.#handleKeyUp);
+  }
+
+  override render() {
+    const classes = classMap({ dragging: this._altKey, draggable: true });
+
+    return html`
+      <div
+        draggable="true"
+        @dragstart=${this.#handleDragStart}
+        class=${classes}
+      >
+        <div class="draggable-scrim"></div>
         <slot></slot>
       </div>
     `;
   }
 
-  private handleDragStart(e: DragEvent) {
+  #handleKeyDown = (e: KeyboardEvent) => {
+    if (e.altKey) {
+      this._altKey = true;
+    }
+  };
+
+  #handleKeyUp = (e: KeyboardEvent) => {
+    if (!e.altKey) {
+      this._altKey = false;
+    }
+  };
+
+  #handleDragStart(e: DragEvent) {
     console.log("draggable dragstart", e, this.entity);
     const entityId = this.entity?.entityId;
 
@@ -26,5 +77,9 @@ export default class DraggableElement extends LitElement {
       console.log("draggable data", data, e.dataTransfer);
       e.dataTransfer?.setData("application/json", data);
     }
+  }
+
+  #handleDragEnd(e: DragEvent) {
+    console.log("draggable dragend", e);
   }
 }
