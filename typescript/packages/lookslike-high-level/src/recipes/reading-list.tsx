fd78427697e642bc26888a2d0706b18f79d9c547
Bernhard Seefeld <berni@common.tools>
2024-11-07 22:42:18 -0800

    one way sync from bookshelf

--- typescript/packages/lookslike-high-level/src/recipes/reading-list.tsx
+++ typescript/packages/lookslike-high-level/src/recipes/reading-list.tsx
@@ -1,5 +1,13 @@
 import { h } from "@commontools/common-html";
-import { recipe, handler, UI, NAME, derive } from "@commontools/common-builder";
+import {
+  recipe,
+  handler,
+  UI,
+  NAME,
+  derive,
+  lift,
+  cell,
+} from "@commontools/common-builder";
 import { z } from "zod";
 
 const BookItem = z.object({
@@ -7,13 +15,6 @@ const BookItem = z.object({
   author: z.string(),
   done: z.boolean().default(false),
 });
-
-const ReadingList = z
-  .object({
-    title: z.string().default("Reading list"),
-    items: z.array(BookItem).default([]).describe("#booklist"),
-  })
-  .describe("Reading list");
 
 export type BookItem = z.infer<typeof BookItem>;
 
@@ -30,50 +31,79 @@ const updateItem = handler<
   item.done = detail.checked;
 });
 
-const deleteItem = handler<{}, { items: BookItem[]; item: BookItem }>(
-  ({}, { item, items }) => {
-    let idx = items.findIndex((i) => i.title === item.title);
-    if (idx !== -1) items.splice(idx, 1);
+const deleteItem = handler<{}, { list: BookItem[]; item: BookItem }>(
+  ({}, { item, list }) => {
+    let idx = list.findIndex((i) => i.title === item.title);
+    if (idx !== -1) list.splice(idx, 1);
+    console.log("deleted item", item, idx);
   },
 );
 
-export default recipe(ReadingList, ({ title, items }) => {
-  return {
-    [NAME]: title,
-    [UI]: (
-      <os-container>
-        <common-input
-          value={title}
-          placeholder="List title"
-          oncommon-input={updateTitle({ title })}
-        />
-        <common-vstack gap="sm">
-          {items.map((item: BookItem) => (
-            <common-draggable $entity={item}>
-              <common-hstack>
-                <common-todo
-                  checked={item.done}
-                  value={derive(
-                    item,
-                    ({ title, author }) => `${title} by ${author}`,
-                  )}
-                  ontodo-checked={updateItem({ item })}
-                  ontodo-input={updateItem({ item })}
-                />
-                <sl-button
-                  outline
-                  variant="danger"
-                  onclick={deleteItem({ item, items })}
-                >
-                  Delete
-                </sl-button>
-              </common-hstack>
-            </common-draggable>
-          ))}
-        </common-vstack>
-      </os-container>
-    ),
-    title,
-    items,
-  };
+const oneWayCopyFn = lift(({ external, seen, internal }) => {
+  const previousCells = seen.map((item: any) => JSON.stringify(item));
+  console.log("previousCells", previousCells);
+  external.forEach((item: any) => {
+    if (!previousCells.includes(JSON.stringify(item))) {
+      console.log("new item", item, JSON.stringify(item));
+      seen.push(item);
+      internal.push(item);
+    }
+  });
 });
+
+function oneWayCopy(external: any[]) {
+  const seen = cell([]);
+  const internal = cell([]);
+  oneWayCopyFn({ external, seen, internal });
+  return internal;
+}
+
+export default recipe(
+  z
+    .object({
+      title: z.string().default("Reading list"),
+      books: z.array(BookItem).default([]).describe("#booklist"),
+    })
+    .describe("Reading list"),
+  ({ title, books }) => {
+    const list = oneWayCopy(books);
+    return {
+      [NAME]: title,
+      [UI]: (
+        <os-container>
+          <common-input
+            value={title}
+            placeholder="List title"
+            oncommon-input={updateTitle({ title })}
+          />
+          <common-vstack gap="sm">
+            {list.map((item: BookItem) => (
+              <common-draggable $entity={item}>
+                <common-hstack>
+                  <common-todo
+                    checked={item.done}
+                    value={derive(
+                      item,
+                      ({ title, author }) => `${title} by ${author}`,
+                    )}
+                    ontodo-checked={updateItem({ item })}
+                    ontodo-input={updateItem({ item })}
+                  />
+                  <sl-button
+                    outline
+                    variant="danger"
+                    onclick={deleteItem({ item, list })}
+                  >
+                    Delete
+                  </sl-button>
+                </common-hstack>
+              </common-draggable>
+            ))}
+          </common-vstack>
+        </os-container>
+      ),
+      title,
+      list,
+    };
+  },
+);
