49b4e78645af2276307c381ede3f3810c9aebc98
Ben Follington <5009316+bfollington@users.noreply.github.com>
2024-11-07 17:46:53 -0800

    Fix types

--- typescript/packages/lookslike-high-level/src/recipes/charm.tsx
+++ typescript/packages/lookslike-high-level/src/recipes/charm.tsx
@@ -8,40 +8,16 @@ const init = select({ self: $.self })
   .assert(({ self }) => [self, "clicks", 0])
   .commit();
 
-// const view = query(z.object({
-//   id: z.object({}),
-//   count: z.number(),
-// }))
-//   .render(({ count, self }: { count: number; self: Reference }) => {
-//     return (
-//       <div title={`Clicks ${count}`} entity={self}>
-//         <div>{count}</div>
-//         <button onclick="~/on/click">Click me!</button>
-//       </div>
-//     );
-//   });
-
-// const view =
-//   b.object({ clicks: b.number(), })
-//     .render(({ self, clicks }: { self: Reference; clicks: number; }) => {
-//       return (
-//         <div title={`Clicks ${clicks}`} entity={self}>
-//           <div>{clicks}</div>
-//           <button onclick="~/on/click">Click me!</button>
-//         </div>
-//       );
-//     });
-
-const view = select({ self: $.self, count: $.count })
-  .match($.self, "clicks", $.count)
-  .render(({ count, self }: { count: number; self: Reference }) => {
-    return (
-      <div title={`Clicks ${count}`} entity={self}>
-        <div>{count}</div>
-        <button onclick="~/on/click">Click me!</button>
-      </div>
-    );
-  });
+const view =
+  b.object({ clicks: b.number(), })
+    .render(({ self, clicks }) => {
+      return (
+        <div title={`Clicks ${clicks}`} entity={self}>
+          <div>{clicks}</div>
+          <button onclick="~/on/click">Click me!</button>
+        </div>
+      );
+    });
 
 const onclick = select({
   self: $.self,
