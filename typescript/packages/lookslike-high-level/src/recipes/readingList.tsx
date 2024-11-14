152129aa0a7f09c9448ac1815612adc26236970f
Ben Follington <5009316+bfollington@users.noreply.github.com>
2024-11-14 12:05:27 -0800

    Latest iteration on readingList example

--- typescript/packages/lookslike-high-level/src/recipes/readingList.tsx
+++ typescript/packages/lookslike-high-level/src/recipes/readingList.tsx
@@ -17,12 +17,10 @@ const event = <T extends Record<string, any>, D extends (name: string) => `~/on/
     [K in keyof T]: Variable<any>
   }>).match($.self, `~/on/${name}`, $.event);
 };
-
 const defaultTo = (field: string, defaultValue: any) => select({ self: $.self })
   .not(q => q.match($.self, field, $._))
   .assert(({ self }) => [self, field, defaultValue])
   .commit();
-
 const defaults = <T extends Record<string, any>>(input: T) => {
   return Object.entries(input).reduce((rules, [field, value]) => {
     const ruleName = `defaultFor${field.charAt(0).toUpperCase() + field.slice(1).replace(/\//g, '_')}`;
@@ -35,8 +33,18 @@ const defaults = <T extends Record<string, any>>(input: T) => {
 
 const createDispatch = <T extends string>(names: readonly T[]) => (name: T) => `~/on/${name}`;
 
+// bf: probably not where we want to end up here but sort of works
+// bf: there's something strange going on where new items look like clones of an existing item until you reload (I suspect local memory?)
+const charms = (items: { id: Reference }[], behaviour: any) => items.sort((a, b) => a.id.toString().localeCompare(b.id.toString())).map(a => <common-charm
+  id={a.id.toString()}
+  spell={() => behaviour}
+  entity={() => a.id}
+></common-charm>);
+
+// bf: exploring typesafe event names
 const dispatch = createDispatch([
   'add-item',
+  'delete-item',
   'change-title'
 ]);
 
@@ -45,6 +53,10 @@ const Model = {
   'draft/title': '',
   'collection/articles': []
 };
+
+const ItemModel = {
+  title: ''
+}
 
 const query = <M extends Record<string, string | number | boolean | any[]>, T extends keyof M>(model: M, ...fields: T[]) => {
   const selection = { self: $.self, ...Object.fromEntries(fields.map(name => [name, $[name]])) };
@@ -59,30 +71,39 @@ const query = <M extends Record<string, string | number | boolean | any[]>, T ex
   }, selectParams);
 };
 
+const readingListItem = behavior({
+  view: query(ItemModel, 'title')
+    .render(({ self, title }) =>
+      <li title={title} entity={self}>
+        {title}
+        <button onclick={dispatch('delete-item')} style="margin-left: 8px">Delete</button>
+      </li>
+    )
+    .commit(),
+
+  onDeleteItem: event('delete-item')
+    .upsert(({ self }) => [self, 'deleted', true])
+    .commit()
+})
+
 export const readingList = behavior({
+  // bf: we would be better served baking this behaviour into the query, rather than spamming writes on spawn
   ...defaults(Model),
 
-  // add item on click
-  onAddItem: event('add-item', { draftTitle: $.draftTitle })
-    .match($.self, "draft/title", $.draftTitle)
-    .update(({ self, event, draftTitle }) => [
-      // Q: we should probably say the name of the collection here
-      // currently it just adds it to ANY collection that listens for `NEW`
-      make(self, { title: draftTitle }),
-      // reset input field
-      { Retract: [self, "draft/title", draftTitle] }
-    ])
+  // empty state view
+  emptyStateView: query(Model, 'title', 'draft/title')
+    .not(q => q.match($.self, "collection/articles", $.article))
+    .render(({ self, 'draft/title': draftTitle, title }) =>
+      <div title={title} entity={self}>
+        <span>Empty!</span>
+        <hr />
+        <common-input value={draftTitle} oncommon-input={dispatch('change-title')} />
+        <button onclick={dispatch('add-item')}>Add</button>
+      </div>
+    )
     .commit(),
 
-  test2: query(Model, 'title', 'draft/title')
-    .update(({ self, title, 'draft/title': draftTitle }) => {
-      console.log('wat', self, title, draftTitle)
-      return []
-    })
-    .commit(),
-
-  //  list articles view
-  view: select({
+  listArticlesView: select({
     self: $.self,
     draftTitle: $.draftTitle,
     title: $.title,
@@ -95,32 +116,18 @@ export const readingList = behavior({
     .match($.self, "draft/title", $.draftTitle)
     .match($.self, "collection/articles", $.article)
     .match($.article, "title", $.articleTitle)
-    .render(({ article, self, draftTitle, title }) => <div title={title} entity={self}>
-      <ul>
-        {/* naughty, we can't do this with IFC but working around a temp view fragment limitation */}
-        {...article.map(a => <li><a href={`charm://${a.id}`}>{a.title}</a></li>)}
-      </ul>
-      <hr />
-      <common-input value={draftTitle} oncommon-input={dispatch('change-title')} />
-      <button onclick={dispatch('add-item')}>Add</button>
-    </div>)
-    .commit(),
-
-  // empty state view
-  noArticlesView: select({
-    self: $.self,
-    draftTitle: $.draftTitle,
-    title: $.title
-  })
-    .match($.self, "draft/title", $.draftTitle)
-    .match($.self, "title", $.title)
-    .not(q => q.match($.self, "collection/articles", $._))
-    .render(({ self, draftTitle, title }) => <div title={title} entity={self}>
-      <span>Empty!</span>
-      <hr />
-      <common-input value={draftTitle} oncommon-input={dispatch('change-title')} />
-      <button onclick={dispatch('add-item')}>Add</button>
-    </div>)
+    .not(q => q.match($.article, "deleted", true))
+    .render(({ article, self, draftTitle, title }) =>
+      <div title={title} entity={self}>
+        <h1>Unordered Collection (Set)</h1>
+        <ul>
+          {...charms(article, readingListItem)}
+        </ul>
+        <hr />
+        <common-input value={draftTitle} oncommon-input={dispatch('change-title')} />
+        <button onclick={dispatch('add-item')}>Add</button>
+      </div>
+    )
     .commit(),
 
   onChangeTitle: event('change-title')
@@ -128,8 +135,20 @@ export const readingList = behavior({
       return [self, 'draft/title', event.detail.value]
     })
     .commit(),
+
+  onAddItem: event('add-item', { draftTitle: $.draftTitle })
+    .match($.self, "draft/title", $.draftTitle)
+    .update(({ self, event, draftTitle }) => [
+      // bf: we should probably say the name of the collection here
+      // currently it just adds it to ANY collection that listens for `NEW`
+      make(self, { title: draftTitle }),
+      // reset input field
+      { Retract: [self, "draft/title", draftTitle] }
+    ])
+    .commit(),
 })
 
 console.log(readingList)
+console.log(readingListItem)
 
 export const spawn = (input: {} = source) => readingList.spawn(input);
