5951965d8f202dd74ffa7e84332aba5a9e3bb5db
Ben Follington <5009316+bfollington@users.noreply.github.com>
2024-11-20 10:47:29 +1000

    Export Session from common-system

--- typescript/packages/common-system/src/index.ts
+++ typescript/packages/common-system/src/index.ts
@@ -2,6 +2,7 @@ export { h, Fragment } from "./jsx.js";
 export * from "./adapter.js";
 export * from "./rule-builder.js";
 export * from "./view.js";
+export * as Session from "./session.js";
 import UI from "./ui.js";
 
 export { UI };
