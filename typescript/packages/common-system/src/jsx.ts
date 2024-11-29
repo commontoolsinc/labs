e4eefe9ca60014e3ab58ffd0118156f9f670b857
Ben Follington <5009316+bfollington@users.noreply.github.com>
2024-11-29 17:18:12 +1000

    JSX support for map and <Function /> components

--- typescript/packages/common-system/src/jsx.ts
+++ typescript/packages/common-system/src/jsx.ts
@@ -17,6 +17,10 @@ declare global {
     interface IntrinsicElements {
       [elemName: string]: any;
     }
+    interface Element<T> extends Node<T> {
+      type: string | Function;
+      props: any;
+    }
   }
 }
 
@@ -71,25 +75,33 @@ const toChild = <T>(child: Node<T>): Node<T> => {
 };
 
 export const h = <T>(
-  localName: string,
+  type: string | ((props: any) => Node<T>),
   settings: { [key: string]: any } | null,
   ...children: Node<T>[]
-): Node<T> =>
-  {
-    const ourSettings = Object.entries(settings ?? {}).map(([key, value]) => setting(key, value))
-
-    // bf: fix any typing here
-    const allHaveKeys = children.every(c => (c.settings as any)?.['key'])
-    if (allHaveKeys) {
-      return keyedNode(localName, ourSettings, children.map(c => {
-        return [(c.settings as any)['key'], toChild(c)] as const
-      })) as Node<T>
-    }
-
-    return node(
-      localName,
-      ourSettings,
-      children.map(toChild),
-    ) as Node<T>;
-
+): Node<T> => {
+  // Check if type is a function (component)
+  if (typeof type === "function") {
+    // Call the function component with props (settings) and children
+    return type({
+      ...settings,
+      children: children.length === 1 ? children[0] : children,
+    });
   }
+
+  const ourSettings = Object.entries(settings ?? {}).map(([key, value]) =>
+    setting(key, value),
+  );
+
+  const allHaveKeys = children.every(c => (c.settings as any)?.["key"]);
+  if (allHaveKeys) {
+    return keyedNode(
+      type,
+      ourSettings,
+      children.map(c => {
+        return [(c.settings as any)["key"], toChild(c)] as const;
+      }),
+    ) as Node<T>;
+  }
+
+  return node(type, ourSettings, children.map(toChild).flat(1)) as Node<T>;
+};
