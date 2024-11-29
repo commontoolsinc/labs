8be110de70970a5b6d26966df9c7655912414d21
Ben Follington <5009316+bfollington@users.noreply.github.com>
2024-11-29 17:35:01 +1000

    Disable mutation descriptions

--- typescript/packages/common-system/src/db.ts
+++ typescript/packages/common-system/src/db.ts
@@ -713,28 +713,28 @@ class Subscription<Select extends Selector = Selector> {
         }
         changes.push(...matchChanges);
 
-        if (matchChanges.length > 0) {
-          explainMutation({
-            query: this.query,
-            selection,
-            changes,
-          }).then(explanation => {
-            window.dispatchEvent(
-              new CustomEvent("mutation", {
-                detail: {
-                  rule: this.name,
-                  spell: this.id.toString(),
-                  entity: self?.toString(),
-                  query: this.query,
-                  selection,
-                  changes,
-                  explanation,
-                  revision,
-                },
-              }),
-            );
-          });
-        }
+        // if (matchChanges.length > 0) {
+        //   explainMutation({
+        //     query: this.query,
+        //     selection,
+        //     changes,
+        //   }).then(explanation => {
+        //     window.dispatchEvent(
+        //       new CustomEvent("mutation", {
+        //         detail: {
+        //           rule: this.name,
+        //           spell: this.id.toString(),
+        //           entity: self?.toString(),
+        //           query: this.query,
+        //           selection,
+        //           changes,
+        //           explanation,
+        //           revision,
+        //         },
+        //       }),
+        //     );
+        //   });
+        // }
       }
       if (selection.length > 0) {
         const totalTime = performance.now() - processStartTime;
