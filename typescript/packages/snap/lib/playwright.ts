9c7f337b2d3ef000752bd0903a78398fd2507b38
jakedahn <jake@common.tools>
2024-12-03 13:25:01 -0700

    [snap]: adding a semaphore to limit the number of concurrent chromes to spawn from playwright

--- typescript/packages/snap/lib/playwright.ts
+++ typescript/packages/snap/lib/playwright.ts
@@ -1,41 +1,48 @@
 import { chromium } from "playwright";
+import { browserSemaphore } from "./queue.ts";
 
 export async function takeScreenshot(
   url: string,
   { outputPath, fullPage }: { outputPath: string; fullPage: boolean },
 ) {
-  const browser = await chromium.launch();
-  const context = await browser.newContext({
-    viewport: {
-      width: 1024,
-      height: 768,
-    },
-  });
+  await browserSemaphore.acquire();
+  
+  try {
+    const browser = await chromium.launch();
+    const context = await browser.newContext({
+      viewport: {
+        width: 1024,
+        height: 768,
+      },
+    });
 
-  const page = await context.newPage();
+    const page = await context.newPage();
 
-  // Wait for network to be idle to ensure all content is loaded
-  await page.goto(url, {
-    waitUntil: "networkidle",
-  });
+    // Wait for network to be idle to ensure all content is loaded
+    await page.goto(url, {
+      waitUntil: "networkidle",
+    });
 
-  // Close the sidebar
-  const selector = "os-navstack > os-navpanel > os-sidebar-close-button";
-  await page.waitForSelector(selector);
-  await page.click(selector);
-  await page.waitForTimeout(500);
-  await page.waitForLoadState("networkidle");
+    // Close the sidebar
+    const selector = "os-navstack > os-navpanel > os-sidebar-close-button";
+    await page.waitForSelector(selector);
+    await page.click(selector);
+    await page.waitForTimeout(500);
+    await page.waitForLoadState("networkidle");
 
-  // Take screenshot
-  const screenshot = await page.screenshot({
-    path: outputPath,
-    fullPage,
-  });
+    // Take screenshot
+    const screenshot = await page.screenshot({
+      path: outputPath,
+      fullPage,
+    });
 
-  console.log(`Saved screenshot of '${url}' to ${outputPath}`);
+    console.log(`Saved screenshot of '${url}' to ${outputPath}`);
 
-  await context.close();
-  await browser.close();
+    await context.close();
+    await browser.close();
 
-  return screenshot;
+    return screenshot;
+  } finally {
+    browserSemaphore.release();
+  }
 }
