cc8c2ce1c02f6f9bc818d7aa983cadfcb65ced37
jakedahn <jake@common.tools>
2024-12-06 16:05:43 -0700

    saving progress

--- typescript/packages/lookslike-high-level/src/spells/myFoodPal.tsx
+++ typescript/packages/lookslike-high-level/src/spells/myFoodPal.tsx
@@ -81,26 +81,7 @@ const foodItems = [
   },
 ];
 
-const entry = {
-  id: "meal_123",
-  timestamp: "2024-12-06T07:30:00Z",
-  mealType: "breakfast",
-  foods: [
-    {
-      foodId: "food_125",
-      qty: 1, // one serving of Greek Yogurt
-    },
-    {
-      foodId: "food_127",
-      qty: 1, // one banana
-    },
-  ],
-  notes: "Quick breakfast before work",
-};
-
-// export const foo = field("foo", 0);
-
-export const myFoodPal = behavior({
+export const myFoodPalFoodGrid = behavior({
   importFoods: select({ self: $.self })
     .not(q => q.match($.self, "foods", $._))
     .update(({ self }) =>
@@ -136,16 +117,15 @@ export const myFoodPal = behavior({
     .match($.foodId, "nutrients", $.nutrient)
     .match($.nutrient, "calories", $.calories)
     .match($.nutrient, "protein", $.protein)
-    .match($.nutrient, "carbohydrates", $.carbohydrates)
-    // .match($.nutrient, "fat", $.fat)
+    .match($.nutrient, "carbohydrates", $.carbohydrates) // This is an example of how to handle optional fields. Maybe the default should be falsy like null?
     .clause(defaultTo($.nutrient, "fat", $.fat, 0))
     .match($.foodId, "servingSize", $.servingSize)
     .match($.foodId, "servingUnit", $.servingUnit)
-    .render(EmptyState)
+    .render(MyFoodPalFoodGrid)
     .commit(),
 });
 
-function EmptyState({
+function MyFoodPalFoodGrid({
   self,
   foods,
 }: {
@@ -209,5 +189,134 @@ function EmptyState({
   );
 }
 
+const entry = {
+  timestamp: "2024-12-06T07:30:00Z",
+  mealType: "breakfast",
+  notes: "Quick breakfast before work",
+
+  foods: ["ref1", "ref2"],
+};
+
+// const entryDraftRef = refer({ name: "entryDraft" });
+// const DraftTitle = select({ self: $.self, draftTitle: $.draftTitle }).clause(
+//   defaultTo($.self, "draft/title", $.draftTitle, ""),
+// );
+// export const mealType = select({ self: entryDraftRef, notes: $.notes }).clause(
+// defaultTo($.self, "notes", $.notes, ""),
+// );
+// NOTE(jake): Can these use local ~/mealType and ~/notes?
+export const mealType = field("mealType", "breakfast");
+export const notes = field("notes", "");
+export const mealFoods = field("mealFoods", []);
+
+export const myFoodPalEntryView = behavior({
+  importFoods: select({ self: $.self })
+    .not(q => q.match($.self, "foods", $._))
+    .update(({ self }) =>
+      foodItems.flatMap(food => {
+        const id = refer(food);
+        return [
+          { Import: food },
+          { Assert: [self, "foods", id] } as Instruction,
+        ];
+      }),
+    )
+    .commit(),
+
+  render: select({
+    self: $.self,
+    mealType: $.mealType,
+    notes: $.notes,
+
+    foods: [
+      {
+        id: $.foodId,
+        name: $.name,
+      },
+    ],
+  })
+    .match($.self, "foods", $.foodId)
+    .match($.foodId, "name", $.name)
+    .with(mealType)
+    .with(notes)
+    .render(MyFoodPalEntryView)
+    .commit(),
+
+  onChangeMealType: event("~/on/change-meal-type")
+    .update(({ self, event }) => {
+      const val = Session.resolve<CommonInputEvent>(event).detail.value;
+      return [{ Upsert: [self, "mealType", val] }];
+    })
+    .commit(),
+
+  onChangeNotes: event("~/on/change-notes")
+    .update(({ self, event }) => {
+      const val = Session.resolve<CommonInputEvent>(event).detail.value;
+      return [{ Upsert: [self, "notes", val] }];
+    })
+    .commit(),
+
+  onAddEntry: event("~/on/add-entry")
+    .update(({ self, event }) => {
+      const ref = Session.resolve<CommonInputEvent>(event).detail.value;
+      return [{ Assert: [self, "entryDraft", ref] }];
+    })
+    .commit(),
+});
+
+function MyFoodPalEntryView({
+  self,
+  foods,
+  notes,
+  mealType,
+}: {
+  self: Reference;
+  notes: string;
+  mealType: string;
+  foods: {
+    id: Reference;
+    name: string;
+  }[];
+}) {
+  return (
+    <div title={"My Food Pal"} entity={self}>
+      <h1>Add meal entry</h1>
+
+      <div>
+        <div>
+          <label>Meal Type</label>
+          <common-input
+            value={mealType}
+            oncommon-blur="~/on/change-meal-type"
+          />
+        </div>
+        <div>
+          <label>Notes</label>
+          <common-input value={notes} oncommon-blur="~/on/change-notes" />
+        </div>
+
+        <common-button onclick="~/on/add-entry">Add Entry</common-button>
+      </div>
+
+      <div style="display: grid; grid-template-columns: repeat(3, 1fr); gap: 20px;">
+        {foods.map(({ id, name }) => (
+          <div
+            key={id}
+            style="border: 2px solid #ccc; border-radius: 8px; padding: 20px;"
+          >
+            <h2>{name}</h2>
+            <img
+              width={250}
+              height={250}
+              style="object-fit: cover; border-radius: 8px;"
+              src={`/api/img?prompt=${encodeURIComponent("cute hello kitty themed food illustration of a " + name)}`}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
 export const spawn = (source: {} = { myFoodPal: 5 }) =>
-  myFoodPal.spawn(source, "MyFoodPal");
+  myFoodPalEntryView.spawn(source, "MyFoodPal");
