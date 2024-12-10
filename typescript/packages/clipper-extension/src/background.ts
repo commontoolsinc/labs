c8f05b0eba25b9c106b84e1f42c1ecf09d188c90
Ben Follington <5009316+bfollington@users.noreply.github.com>
2024-12-10 11:16:33 +1000

    Extension UX, select content or clip entire page

--- typescript/packages/clipper-extension/src/background.ts
+++ typescript/packages/clipper-extension/src/background.ts
@@ -8,24 +8,37 @@ browser.runtime.onInstalled.addListener(() => {
   });
 });
 
-browser.contextMenus.onClicked.addListener((info, tab) => {
+browser.contextMenus.onClicked.addListener((info, _tab) => {
   if (info.menuItemId === "clipToCollectathon") {
-    let content: any = {};
+    let content: any = {
+      pageUrl: info.pageUrl
+    };
 
     if (info.selectionText) {
-      content.type = "text";
-      content.text = info.selectionText;
+      content = {
+        type: "text",
+        pageUrl: info.pageUrl,
+        selectedContent: {
+          text: info.selectionText,
+          html: info.selectionText // Basic HTML wrapping could be added here if needed
+        }
+      };
     } else if (info.linkUrl) {
-      content.type = "link";
-      content.url = info.linkUrl;
+      content = {
+        type: "link",
+        pageUrl: info.pageUrl,
+        url: info.linkUrl
+      };
     } else if (info.srcUrl) {
-      content.type = info.mediaType || "image";
-      content.url = info.srcUrl;
+      content = {
+        type: info.mediaType || "image",
+        pageUrl: info.pageUrl,
+        url: info.srcUrl
+      };
     }
 
-    content.pageUrl = info.pageUrl;
-
     browser.storage.local.set({ clipContent: content }).then(() => {
+      console.log('saved content', content)
       browser.action.openPopup();
     });
   }
@@ -34,6 +47,7 @@ browser.contextMenus.onClicked.addListener((info, tab) => {
 browser.runtime.onConnect.addListener(function(port) {
   if (port.name === "popup") {
     port.onDisconnect.addListener(function() {
+      // Clear stored content when popup is closed
       browser.storage.local.remove('clipContent');
     });
   }
