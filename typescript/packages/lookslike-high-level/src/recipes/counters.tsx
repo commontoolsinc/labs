3286616cf74a4b8b330a2659b67faccd088ff84e
jakedahn <jake@common.tools>
2025-01-09 12:38:13 -0700

    autoformatting

--- typescript/packages/lookslike-high-level/src/recipes/counters.tsx
+++ typescript/packages/lookslike-high-level/src/recipes/counters.tsx
@@ -1,5 +1,11 @@
 import { h } from "@commontools/common-html";
-import { Spell, type OpaqueRef, handler, select, $ } from "@commontools/common-builder";
+import {
+  Spell,
+  type OpaqueRef,
+  handler,
+  select,
+  $,
+} from "@commontools/common-builder";
 
 type Counter = {
   title: string;
@@ -12,23 +18,33 @@ type Counters = {
   total: number;
 };
 
-const incrementHandler = handler<{}, { counter: Counter }>(function ({ }, { counter }) {
+const incrementHandler = handler<{}, { counter: Counter }>(function (
+  {},
+  { counter },
+) {
   console.log("incrementHandler", counter);
   counter.count += 1;
 });
 
-const renameHandler = handler<{ detail: { value: string } }, { counter: Counter }>(function ({ detail: { value } }, { counter }) {
+const renameHandler = handler<
+  { detail: { value: string } },
+  { counter: Counter }
+>(function ({ detail: { value } }, { counter }) {
   counter.title = value;
 });
 
-const removeHandler = handler<{}, { counter: Counter, counters: Counter[] }>(function ({ }, { counter, counters }) {
-  console.log("removeHandler", counter, counters);
-  // FIXME(ja): not having equality check on objects is a problem
-  const index = counters.findIndex((i) => i.title === counter.title && i.count === counter.count);
-  if (index !== -1) {
-    counters.splice(index, 1);
-  }
-});
+const removeHandler = handler<{}, { counter: Counter; counters: Counter[] }>(
+  function ({}, { counter, counters }) {
+    console.log("removeHandler", counter, counters);
+    // FIXME(ja): not having equality check on objects is a problem
+    const index = counters.findIndex(
+      i => i.title === counter.title && i.count === counter.count,
+    );
+    if (index !== -1) {
+      counters.splice(index, 1);
+    }
+  },
+);
 
 export class CountersSpell extends Spell<Counters> {
   constructor() {
@@ -39,24 +55,26 @@ export class CountersSpell extends Spell<Counters> {
     });
 
     this.addEventListener("add", self => {
-      self.counters.push({ title: "untitled counter " + self.counters.length, count: 0 });
+      self.counters.push({
+        title: "untitled counter " + self.counters.length,
+        count: 0,
+      });
     });
 
-    this.addRule(
-      select({ counters: $.counters }),
-      ({ self, counters }) => {
-        self.total = counters.reduce((acc: number, counter: Counter) => acc + counter.count, 0);
-      }
-    );
-
+    this.addRule(select({ counters: $.counters }), ({ self, counters }) => {
+      self.total = counters.reduce(
+        (acc: number, counter: Counter) => acc + counter.count,
+        0,
+      );
+    });
   }
 
   override init() {
     return {
       title: "untitled counters",
-      counters: [], // FIXME(ja): sending in default values like { title: "untitled counter", count: 0 } doesn't convert them to a cell
+      counters: [],
       $NAME: "counters name",
-      total: 0
+      total: 0,
     };
   }
 
@@ -64,12 +82,19 @@ export class CountersSpell extends Spell<Counters> {
     return (
       <div>
         <common-input value={title} oncommon-input={this.dispatch("title")} />
-        {counters.map((counter) => (
+        {counters.map(counter => (
           <div>
-            <common-input value={counter.title} oncommon-input={renameHandler.with({ counter })} />
+            <common-input
+              value={counter.title}
+              oncommon-input={renameHandler.with({ counter })}
+            />
             {counter.count}
-            <button onclick={incrementHandler.with({ counter })}>Increment</button>
-            <button onclick={removeHandler.with({ counter, counters })}>Remove</button>
+            <button onclick={incrementHandler.with({ counter })}>
+              Increment
+            </button>
+            <button onclick={removeHandler.with({ counter, counters })}>
+              Remove
+            </button>
           </div>
         ))}
         <p>Total: {total}</p>
