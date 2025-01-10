aae812ab1ab698ffbecd3b3013f0c3ac48614cb8
jakedahn <jake@common.tools>
2025-01-09 20:41:59 -0700

    fixing hstack and vstack to allow for gap/pad props

--- typescript/packages/common-ui/src/components/common-hstack.ts
+++ typescript/packages/common-ui/src/components/common-hstack.ts
@@ -1,12 +1,20 @@
 import { LitElement, html, css } from "lit";
-import { customElement } from "lit/decorators.js";
+import { customElement, property } from "lit/decorators.js";
 import { view } from "../hyperscript/render.js";
 import { baseStyles } from "./style.js";
+import { eventProps } from "../hyperscript/schema-helpers.js";
 
-export const hstack = view("common-hstack", {});
+export const hstack = view("common-hstack", {
+  ...eventProps(),
+  gap: { type: "string" },
+  pad: { type: "string" },
+});
 
 @customElement("common-hstack")
 export class CommonHstackElement extends LitElement {
+  @property({ type: String, reflect: true }) gap?: string;
+  @property({ type: String, reflect: true }) pad?: string;
+
   static override styles = [
     baseStyles,
     css`
@@ -20,12 +28,44 @@ export class CommonHstackElement extends LitElement {
         align-items: center;
       }
 
+      :host([gap="none"]) .hstack {
+        gap: 0;
+      }
+
+      :host([gap="sm"]) .hstack {
+        gap: var(--gap-sm);
+      }
+
       :host([gap="md"]) .hstack {
-        gap: var(--gap);
+        gap: var(--gap-md);
+      }
+
+      :host([gap="lg"]) .hstack {
+        gap: var(--gap-lg);
+      }
+
+      :host([gap="xl"]) .hstack {
+        gap: var(--gap-xl);
+      }
+
+      :host([gap="2xl"]) .hstack {
+        gap: var(--gap-2xl);
       }
 
       :host([pad="md"]) .hstack {
-        padding: var(--gap);
+        padding: var(--pad-md);
+      }
+
+      :host([pad="lg"]) .hstack {
+        padding: var(--pad-lg);
+      }
+
+      :host([pad="xl"]) .hstack {
+        padding: var(--pad-xl);
+      }
+
+      :host([pad="2xl"]) .hstack {
+        padding: var(--pad-2xl);
       }
     `,
   ];
--- typescript/packages/common-ui/src/components/style.ts
+++ typescript/packages/common-ui/src/components/style.ts
@@ -1,31 +1,39 @@
-import { css } from 'lit';
+import { css } from "lit";
 
 export const baseStyles = css`
-:host {
-  --unit: 4px;
-  --radius: calc(var(--unit) * 2);
-  --color: #000;
-  --secondary-background: #f5f5f5;
-  --secondary-color: #777;
-  --input-background: #fff;
-  --input-color: #000;
-  --button-background: #000;
-  --button-color: #fff;
-  --border-color: #ddd;
-  --pad-sm: calc(var(--unit) * 2);
-  --pad: calc(var(--unit) * 4);
-  --gap: var(--pad);
-  --body-size: calc(var(--unit) * 4);
-  --body-line: calc(var(--unit) * 6);
-  --title-size: calc(var(--unit) * 5);
-  --title-line: calc(var(--unit) * 6);
-  --min-touch-size: calc(var(--unit) * 11);
+  :host {
+    --unit: 4px;
+    --radius: calc(var(--unit) * 2);
+    --color: #000;
+    --secondary-background: #f5f5f5;
+    --secondary-color: #777;
+    --input-background: #fff;
+    --input-color: #000;
+    --button-background: #000;
+    --button-color: #fff;
+    --border-color: #ddd;
+    --pad-sm: calc(var(--unit) * 2);
+    --pad-md: calc(var(--unit) * 4);
+    --pad-lg: calc(var(--unit) * 8);
+    --pad-xl: calc(var(--unit) * 16);
+    --pad-2xl: calc(var(--unit) * 32);
+    --gap: var(--pad);
+    --gap-sm: calc(var(--unit) * 2);
+    --gap-md: calc(var(--unit) * 4);
+    --gap-lg: calc(var(--unit) * 8);
+    --gap-xl: calc(var(--unit) * 16);
+    --gap-2xl: calc(var(--unit) * 32);
+    --body-size: calc(var(--unit) * 4);
+    --body-line: calc(var(--unit) * 6);
+    --title-size: calc(var(--unit) * 5);
+    --title-line: calc(var(--unit) * 6);
+    --min-touch-size: calc(var(--unit) * 11);
 
-  font-family: sans-serif;
-}
+    font-family: sans-serif;
+  }
 
-.body {
-  font-size: var(--body-size);
-  line-height: var(--body-line);
-}
+  .body {
+    font-size: var(--body-size);
+    line-height: var(--body-line);
+  }
 `;
