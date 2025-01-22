d0685acf750cfcfff382f9da7ac5bb7801bf21e0
jakedahn <jake@common.tools>
2025-01-22 13:30:36 -0700

    testing ci flow, needed a whitespace change, but we arent actually using eslint

--- typescript/packages/toolshed/index.ts
+++ typescript/packages/toolshed/index.ts
@@ -2,7 +2,7 @@ import app from "@/app.ts";
 import env from "@/env.ts";
 
 const port = env.PORT;
-// eslint-disable-next-line no-console
+
 console.log(`Server is running on port http://localhost:${port}`);
 
 export type AppType = typeof app;
