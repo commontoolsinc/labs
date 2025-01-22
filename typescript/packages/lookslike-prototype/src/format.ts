fd0a29f39db2f998095141e8f612ab9d827a1491
Jesse Andrews <anotherjesse@gmail.com>
2025-01-22 09:42:45 -0500

    remove some unused directories

--- typescript/packages/lookslike-prototype/src/format.ts
+++
@@ -1,7 +0,0 @@
-import * as prettier from 'prettier'
-import prettierPluginBabel from 'prettier/plugins/babel'
-import prettierPluginEstree from "prettier/plugins/estree";
-
-export async function format(code: string) {
-  return await prettier.format(code, { semi: true, parser: "babel", plugins: [prettierPluginBabel, prettierPluginEstree] });
-}
--- typescript/packages/usuba-ses/src/index.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="ses" />
-import 'ses';
-import { CompartmentAlias } from './ses.js';
-
-export const Compartment = CompartmentAlias;
-export const lockdown = globalThis.lockdown;
-
-export { StaticModuleRecord } from '@endo/static-module-record';
