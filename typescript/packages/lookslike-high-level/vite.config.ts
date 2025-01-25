9c9dbcb8cf45efd2c31224526bc348dc0af21f01
Jesse Andrews <anotherjesse@gmail.com>
2025-01-25 13:17:33 -0500

    re-connect toolshed in vite

--- typescript/packages/lookslike-high-level/vite.config.ts
+++ typescript/packages/lookslike-high-level/vite.config.ts
@@ -7,4 +7,43 @@ export default defineConfig({
     build: { lib: { entry: resolve(__dirname, 'src/index.ts') } },
     resolve: { alias: { src: resolve('src/') } },
     plugins: [dts()],
+    server: {
+        proxy: {
+            "/api/ai/llm": {
+              target: process.env.TOOLSHED_API_URL ?? "http://localhost:8000/api/ai/llm",
+              changeOrigin: true,
+            },
+            "/api/ai/img": {
+              target: process.env.TOOLSHED_API_URL ?? "http://localhost:8000/api/ai/img",
+              changeOrigin: true,
+            },
+            "/api/ai/voice": {
+              target: process.env.TOOLSHED_API_URL ?? "http://localhost:8000/api/ai/voice",
+              changeOrigin: true,
+            },
+            "/api/ai/webreader": {
+              target: process.env.TOOLSHED_API_URL ?? "http://localhost:8000/api/ai/webreader",
+              changeOrigin: true,
+            },
+            "/api/data": {
+              target: process.env.SYNOPSYS_API_URL ?? "http://localhost:8080",
+              changeOrigin: true,
+              rewrite: path => path.replace(/^\/api\/data/, ""),
+            },
+            "/api/storage/blobby": {
+              target: process.env.TOOLSHED_API_URL ?? "http://localhost:8000/api/storage/blobby",
+              changeOrigin: true,
+            },
+          },
+          headers: {
+            "*.wasm": {
+              "Content-Type": "application/wasm",
+            },
+            "Service-Worker-Allowed": "/data/",
+          },
+        },
+        test: {
+          environment: "node",
+        },
+      
 });
