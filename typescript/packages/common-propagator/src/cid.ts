fc94d1bd028cc9f930226e2443bb94359c02efe9
Jordan Santell <jsantell@users.noreply.github.com>
2025-01-27 15:26:50 -0800

    chore: remove vestigial common-propagator (#291)

--- typescript/packages/common-propagator/src/cid.ts
+++
@@ -1,6 +0,0 @@
-let _cid = 0;
-
-// Generate client ID
-export const cid = () => `cid${_cid++}`;
-
-export default cid;
--- typescript/packages/common-propagator/src/lens.ts
+++
@@ -1,4 +0,0 @@
-export type Lens<Big, Small> = {
-  get: (big: Big) => Small;
-  update: (big: Big, small: Small) => Big;
-};
