7965856a27c58967cecacc05dc25cc6b77501432
Jesse Andrews <anotherjesse@gmail.com>
2025-01-27 15:29:52 -0500

    expanding the iframe recipe structure

--- typescript/packages/lookslike-high-level/src/recipes/smolIframe.tsx
+++ typescript/packages/lookslike-high-level/src/recipes/smolIframe.tsx
@@ -2,10 +2,18 @@ import { h } from "@commontools/html";
 import { recipe, UI, NAME } from "@commontools/builder";
 import type { JSONSchema } from "@commontools/builder";
 
+type Recipe = {
+  type: "iframe",
+  src: string,
+  argumentSchema: JSONSchema,
+  resultSchema: JSONSchema,
+  spec: string,
+  name: string,
+}
+
 // @ts-ignore this loads the html file using VITE.js as a string from the html file on disk
 import src from "./smolIframe.html?raw";
-
-const jsonSchema = {
+const argumentSchema = {
   type: "object",
   properties: {
     count: {
@@ -16,13 +24,37 @@ const jsonSchema = {
   description: "SMOL Counter demo"
 } as JSONSchema;
 
+const resultSchema = {
+  type: "object",
+  properties: {
+    text: {
+      type: "string",
+      default: "(empty)"
+    },
+  },
+  description: "SMOL Counter demo"
+} as JSONSchema;
 
-export default recipe(jsonSchema, (data) => {
+const spec = "emoji style counter that increments by 1 when clicked"
+
+const iframeRecipe: Recipe = {
+  type: "iframe",
+  src,
+  argumentSchema,
+  resultSchema,
+  spec,
+  name: "smol iframe",
+}
+
+const runIframeRecipe = ({argumentSchema, resultSchema, src, name}: Recipe) => 
+  recipe(argumentSchema, resultSchema, (data) => {
+
   return {
-    type: "iframe",
-    [NAME]: "smol iframe",
+    [NAME]: name,
     [UI]: (
       <common-iframe src={src} $context={data}></common-iframe>
     ),
   };
 });
+
+export default runIframeRecipe(iframeRecipe);
