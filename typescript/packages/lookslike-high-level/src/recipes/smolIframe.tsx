c2858d42963f5177c107028a2438318c35053513
Jesse Andrews <anotherjesse@gmail.com>
2025-01-27 11:01:46 -0500

    further prep for iframe recipe pattern
    expose the schema (arguments)

--- typescript/packages/lookslike-high-level/src/recipes/smolIframe.tsx
+++ typescript/packages/lookslike-high-level/src/recipes/smolIframe.tsx
@@ -1,20 +1,28 @@
 import { h } from "@commontools/html";
 import { recipe, UI, NAME, derive } from "@commontools/builder";
+import { z } from "zod";
 
 // @ts-ignore this loads the html file using VITE.js as a string from the html file on disk
 import src from "./smolIframe.html?raw";
 
-export default recipe<{
-  data: { count: number };
-}>("smol-iframe", ({ data }) => {
+const DataSchema = z.object({
+  data: z
+    .object({
+      count: z.number().default(0),
+    })
+})
+  .describe("SMOL Counter demo");
+
+export default recipe(DataSchema, ({ data }) => {
+  const { count } = data;
   return {
     [NAME]: "smol iframe",
     [UI]: (
       <div style="height: 100%">
-        <p>outside of iframe, data: {derive(data, (data) => JSON.stringify(data))}</p>
+        <p>outside of iframe, data: {derive({ count }, (data) => JSON.stringify(data))}</p>
         <common-iframe src={src} $context={data}></common-iframe>
       </div>
     ),
-    data,
+    count,
   };
 });
