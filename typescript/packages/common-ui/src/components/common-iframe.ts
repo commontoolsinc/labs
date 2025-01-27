fcf9f76b79fcbba8e45032e64799b70e560eb7bd
Jordan Santell <jsantell@users.noreply.github.com>
2025-01-27 10:44:09 -0800

    common-iframe work (#290)
    common-iframe work:
    
    * Clean up `common-iframe`. Add types. Align event names with web
      standards.
    * Remove `common-runner` as a dependency of `common-iframe`. LLHL now
      defines a handler to resolve cell values.
    * Introduce some basic CSP policies.
    * Changes to iframe guest API: some simplification here. Only one
      subscription per key allowed. Read requests and subscription updates
      share host events.
    * Some initial work on writing CSP tests, using web-test-runner.

--- typescript/packages/common-ui/src/components/common-iframe.ts
+++ typescript/packages/common-ui/src/components/common-iframe.ts
@@ -1,33 +1,51 @@
 import { LitElement, html, css } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
-import {
-  Cell,
-  addAction,
-  removeAction,
-  type Action,
-  type ReactivityLog,
-} from "@commontools/runner";
 import { Ref, createRef, ref } from "lit/directives/ref.js";
+import { IframeIPC } from "../index.js";
 
+// This CSP directive uses 'unsafe-inline' to allow
+// origin-less styles and scripts to be used, defeating
+// many traditional uses of CSP.
+const CSP = "" +
+  // Disable all fetch directives by default
+  "default-src 'none';" +
+  // Allow CDN scripts, unsafe inline.
+  "script-src 'unsafe-inline' unpkg.com cdn.tailwindcss.com;" +
+  // unsafe inline.
+  "style-src 'unsafe-inline';";
+
+// @summary A sandboxed iframe to execute arbitrary scripts.
+// @tag common-iframe
+// @prop {string} src - String representation of HTML content to load within an iframe.
+// @prop context - Cell context.
+// @event {CustomEvent} error - An error from the iframe.
+// @event {CustomEvent} load - The iframe was successfully loaded.
+//
+// ## Missing Functionality
+//
+// * Support updating the `src` property.
+// * Flushing subscriptions inbetween frame loads.
+//
+// ## Incomplete Security Considerations
+//
+// * Exposing iframe status to outer content could be considered leaky,
+//   though all content is inlined, not HTTP URLs.
+//   https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe#error_and_load_event_behavior
+//
 @customElement("common-iframe")
 export class CommonIframeElement extends LitElement {
   @property({ type: String }) src = "";
   // HACK: The UI framework already translates the top level cell into updated
   // properties, but we want to only have to deal with one type of listening, so
   // we'll add a an extra level of indirection with the "context" property.
-  @property({ type: Object }) context?: Cell<any> | any;
+  //@property({ type: Object }) context?: Cell<any> | any;
+  @property({ type: Object }) context?: object;
 
-  @state() private errorDetails: {
-    message: string;
-    source: string;
-    lineno: number;
-    colno: number;
-    stacktrace: string;
-  } | null = null;
+  @state() private errorDetails: IframeIPC.GuestError | null = null;
 
   private iframeRef: Ref<HTMLIFrameElement> = createRef();
 
-  private subscriptions: Map<string, Action[]> = new Map();
+  private subscriptions: Map<string, any> = new Map();
 
   static override styles = css`
     .error-modal {
@@ -63,77 +81,104 @@ export class CommonIframeElement extends LitElement {
   `;
 
   private handleMessage = (event: MessageEvent) => {
-    console.log("Received message", event);
     if (event.data?.source == "react-devtools-content-script") {
-      console.log("ignore react devtools");
       return;
     }
 
-    if (event.source === this.iframeRef.value?.contentWindow) {
-      const { type, key, value } = event.data;
+    if (event.source !== this.iframeRef.value?.contentWindow) {
+      return;
+    }
 
-      if (type === "error") {
-        const { message, source, lineno, colno, stacktrace } = value;
-        this.errorDetails = { message, source, lineno, colno, stacktrace };
+    const IframeHandler = IframeIPC.getIframeContextHandler();
+    if (IframeHandler == null) {
+      console.error("common-iframe: No iframe handler defined.");
+      return;
+    }
+
+    if (!this.context) {
+      console.error("common-iframe: missing `context`.");
+      return;
+    }
+
+    if (!IframeIPC.isGuestMessage(event.data)) {
+      console.error("common-iframe: Malformed message from guest.");
+      return;
+    }
+
+    const message: IframeIPC.GuestMessage = event.data;
+
+    switch (message.type) {
+      case IframeIPC.GuestMessageType.Error: {
+        const { description, source, lineno, colno, stacktrace } = message.data;
+        this.errorDetails = { description, source, lineno, colno, stacktrace };
         console.error("Error details:", {
-          message,
+          description,
           source,
           lineno,
           colno,
           stacktrace,
-          error: value.error ? value.error.stack || value.error.toString() : null,
         });
-      }
-
-      if (typeof key !== "string") {
-        console.error("Invalid key type. Expected string.");
+        this.dispatchEvent(new CustomEvent("error", {
+          detail: this.errorDetails,
+        }));
         return;
       }
-      if (type === "read" && this.context) {
-        const value = this.context?.getAsQueryResult
-          ? this.context?.getAsQueryResult([key])
-          : this.context?.[key];
+
+      case IframeIPC.GuestMessageType.Read: {
+        const key = message.data;
+        const value = IframeHandler.read(this.context, key);
         // TODO: This might cause infinite loops, since the data can be a graph.
-        console.log("readResponse", key, value);
-        const copy =
-          typeof value === "string" && value.includes("{")
-            ? JSON.parse(value)
-            : JSON.parse(JSON.stringify(value));
-        this.iframeRef.value?.contentWindow?.postMessage(
-          { type: "readResponse", key, value: copy },
-          "*",
-        );
-      } else if (type === "write" && this.context) {
-        const updated =
-          typeof value === "string" && value.includes("{")
-            ? JSON.parse(value)
-            : JSON.parse(JSON.stringify(value));
-        console.log("write", key, updated);
-        this.context.getAsQueryResult()[key] = updated;
-      } else if (type === "subscribe" && this.context) {
-        console.log("subscribing", key, this.context);
-
-        const action: Action = (log: ReactivityLog) =>
-          this.notifySubscribers(key, this.context.getAsQueryResult([key], log));
-
-        addAction(action);
-        if (!this.subscriptions.has(key)) this.subscriptions.set(key, [action]);
-        else this.subscriptions.get(key)!.push(action);
-      } else if (type === "unsubscribe" && this.context) {
-        if (this.subscriptions && this.subscriptions.has(key)) {
-          const actions = this.subscriptions.get(key);
-          if (actions && actions.length) removeAction(actions.pop()!);
+        const response: IframeIPC.HostMessage = {
+          type: IframeIPC.HostMessageType.Update,
+          data: [key, copyJSON(value)]
         }
+        this.iframeRef.value?.contentWindow?.postMessage(response, "*");
+        return;
       }
-    }
-  };
+
+      case IframeIPC.GuestMessageType.Write: {
+        const [key, value] = message.data;
+        IframeHandler.write(this.context, key, value);
+        return;
+      }
+
+      case IframeIPC.GuestMessageType.Subscribe: {
+        const key = message.data;
+
+        if (this.subscriptions.has(key)) {
+          console.warn("common-iframe: Already subscribed to `${key}`");
+          return;
+        }
+        let receipt = IframeHandler.subscribe(this.context, key, (key, value) => this.notifySubscribers(key, value));
+        this.subscriptions.set(key, receipt);
+        return;
+      }
+
+      case IframeIPC.GuestMessageType.Unsubscribe: {
+        const key = message.data;
+        let receipt = this.subscriptions.get(key);
+        if (!receipt) {
+          return;
+        }
+        IframeHandler.unsubscribe(this.context, receipt);
+        this.subscriptions.delete(key);
+        return;
+      }
+    };
+  }
 
   private notifySubscribers(key: string, value: any) {
-    console.log("notifySubscribers", key, value);
     // TODO: This might cause infinite loops, since the data can be a graph.
-    const copy = value !== undefined ? JSON.parse(JSON.stringify(value)) : undefined;
-    this.iframeRef.value?.contentWindow?.postMessage({ type: "update", key, value: copy }, "*");
+    // /!\ Why is this serialized?
+    const copy =
+      value !== undefined ? JSON.parse(JSON.stringify(value)) : undefined;
+    const response: IframeIPC.HostMessage = {
+      type: IframeIPC.HostMessageType.Update,
+      data: [key, copy],
+    }
+    this.iframeRef.value?.contentWindow?.postMessage(response, "*");
   }
+
   private boundHandleMessage = this.handleMessage.bind(this);
 
   override connectedCallback() {
@@ -147,9 +192,7 @@ export class CommonIframeElement extends LitElement {
   }
 
   private handleLoad() {
-    console.log("iframe loaded");
-    this.iframeRef.value?.contentWindow?.postMessage({ type: "init" }, "*");
-    this.dispatchEvent(new CustomEvent("loaded"));
+    this.dispatchEvent(new CustomEvent("load"));
   }
 
   private dismissError() {
@@ -157,7 +200,9 @@ export class CommonIframeElement extends LitElement {
   }
 
   private fixError() {
-    this.dispatchEvent(new CustomEvent("fix", { detail: this.errorDetails, bubbles: true }));
+    this.dispatchEvent(
+      new CustomEvent("fix", { detail: this.errorDetails, bubbles: true })
+    );
     this.errorDetails = null;
   }
 
@@ -166,6 +211,7 @@ export class CommonIframeElement extends LitElement {
       <iframe
         ${ref(this.iframeRef)}
         sandbox="allow-scripts allow-forms allow-pointer-lock"
+        csp="${CSP}"
         .srcdoc=${this.src}
         height="100%"
         width="100%"
@@ -177,7 +223,7 @@ export class CommonIframeElement extends LitElement {
             <div class="error-modal">
               <div class="error-content">
                 <h2>Error</h2>
-                <p><strong>Message:</strong> ${this.errorDetails.message}</p>
+                <p><strong>Description:</strong> ${this.errorDetails.description}</p>
                 <p><strong>Source:</strong> ${this.errorDetails.source}</p>
                 <p><strong>Line:</strong> ${this.errorDetails.lineno}</p>
                 <p><strong>Column:</strong> ${this.errorDetails.colno}</p>
@@ -192,4 +238,18 @@ export class CommonIframeElement extends LitElement {
         : ""}
     `;
   }
+}
+
+// This applies a de/serialization cycle on `input` to create
+// a clone.
+//
+// /!\ Lacking context for the original purpose of this function.
+// /!\ I suspect this could be removed as unclear where we use/need
+// /!\ auto-parsing JSON-ish strings, and unnecessary cloning
+// /!\ an object before sending over iframe, invoking structured clone algorithm,
+// /!\ duplicating this potentially costly serialization.
+function copyJSON(input: any): any {
+  return typeof input === "string" && input.includes("{")
+    ? JSON.parse(input)
+    : JSON.parse(JSON.stringify(input));
 }
