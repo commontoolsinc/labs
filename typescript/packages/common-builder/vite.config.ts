396bd389b906a935633ad88dfa2e8b5c5f60b1d4
Jesse Andrews <anotherjesse@gmail.com>
2025-01-29 10:27:28 -0500

    fix pnpm run build for builder and llhl

--- typescript/packages/common-builder/vite.config.ts
+++ typescript/packages/common-builder/vite.config.ts
@@ -4,7 +4,9 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: { lib: { entry: resolve(__dirname, "src/index.ts") } },
+  build: {
+    lib: { entry: resolve(__dirname, "src/index.ts"), formats: ["es"] },
+  },
   resolve: { alias: { src: resolve("src/") } },
   plugins: [dts()],
 });
--- typescript/packages/lookslike-high-level/vite.config.ts
+++ typescript/packages/lookslike-high-level/vite.config.ts
@@ -4,7 +4,7 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: { lib: { entry: resolve(__dirname, "src/index.ts") } },
+  build: { lib: { entry: resolve(__dirname, "src/main.ts"), formats: ["es"] } },
   resolve: { alias: { src: resolve("src/") } },
   plugins: [dts()],
   server: {
