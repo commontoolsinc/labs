38b5f483b74595e0e30c5c04ac36061880d82843
Irakli Gozalishvili <contact@gozala.io>
2025-01-31 17:16:40 -0800

    chore: remove redundant import

--- typescript/packages/toolshed/routes/storage/memory/memory.handlers.ts
+++ typescript/packages/toolshed/routes/storage/memory/memory.handlers.ts
@@ -1,7 +1,6 @@
 import type { AppRouteHandler } from "@/lib/types.ts";
 import type * as Routes from "./memory.routes.ts";
 import * as Memory from "@commontools/memory";
-import * as Path from "jsr:@std/path";
 import env from "@/env.ts";
 
 const { ok: memory, error } = await Memory.open({
@@ -12,9 +11,9 @@ if (error) {
   throw error;
 }
 
-// @ts-expect-error - AppRouteHandler does not like Promise<Response> here as
-// it wants to know status code and not sure how to use my own response
 export const transact: AppRouteHandler<typeof Routes.transact> = (c) =>
+  // @ts-expect-error - AppRouteHandler does not like Promise<Response> here as
+  // it wants to know status code and not sure how to use my own response
   memory.patch(c.req.raw);
 
 export const subscribe: AppRouteHandler<typeof Routes.subscribe> = (
