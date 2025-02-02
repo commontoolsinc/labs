f2913c0a66858b34b35554f5c97504ca6f34ee96
Jesse Andrews <anotherjesse@gmail.com>
2025-02-02 09:21:36 -0500

    rendering works

--- typescript/packages/jumble/src/views/Shell.tsx
+++ typescript/packages/jumble/src/views/Shell.tsx
@@ -1,44 +1,36 @@
 // This is all you need to import/register the @commontools/ui web components
 import "@commontools/ui";
-import React from "react";
-import { useCharms } from "@/contexts/CharmsContext";
-import RunnerWrapper from "@/components/RunnerWrapper";
-import CharmRunner from "@/components/CharmRunner";
-
-const handleClick = () => {
-  console.log("clicked");
-};
-
-import { runPersistent } from "@commontools/lookslike-high-level";
+import React, { useRef } from "react";
+import { type Charm, runPersistent } from "@commontools/lookslike-high-level";
+import { effect, idle, run } from "@commontools/runner";
+import { render } from "@commontools/html";
 
 export default function Shell() {
-  const { charms, focusedCharm, addCharm, removeCharm, runCharm } = useCharms();
-
-  console.log("charms", charms);
-  console.log("focusedCharm", focusedCharm);
-
-  const handleAddDummyCharm = () => {
-    const dummyCharm = {
-      entityId: Date.now().toString(),
-      name: `Charm ${Date.now()}`,
-      ui: <div>Dummy charm UI</div>,
-    };
-    addCharm(dummyCharm);
-  };
+  const containerRef = useRef<HTMLDivElement>(null);
 
   const handleLoadSmolCharm = async () => {
     try {
+      // load the recipe, BUT you can't use JSX because
+      // JSX here would be react JSX, not common/html JSX
+      // even though the recipe imports our `h` function
       const mod = await import("@/recipes/smol.tsx");
       const smolFactory = mod.default;
-      console.log(smolFactory);
-      // const smolCharm = {
-      //   entityId: `smol-${Date.now()}`,
-      //   name: "smol Charm",
-      //   ui: smolFactory,
-      // };
+
+      // run the charm (this makes the logic go, cells, etc)
+      // but nothing about rendering...
       const charm = await runPersistent(smolFactory);
-      console.log(charm)
-      await runCharm(charm);
+      await idle();
+      run(undefined, undefined, charm);
+      await idle();
+
+      // connect the cells of the charm (reactive docs) and the 
+      // view (recipe VDOM) to be rendered using common/html
+      // into a specific DOM element (created in react)
+      effect(charm.asCell<Charm>(), (charm) => {
+        effect(charm['$UI'], (view) => {
+          render(containerRef.current as HTMLElement, view);
+        });
+      });
     } catch (error) {
       console.error("Failed to load counter charm", error);
     }
@@ -46,16 +38,6 @@ export default function Shell() {
 
   return (
     <div className="h-full relative">
-      {/* You still use class="foo" with web components. */}
-      <common-button class="wat" onClick={handleClick}>
-        click me
-      </common-button>
-      <button
-        onClick={handleAddDummyCharm}
-        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
-      >
-        Add Dummy Charm
-      </button>
       <button
         onClick={handleLoadSmolCharm}
         className="mt-4 ml-4 px-4 py-2 bg-green-500 text-white rounded"
@@ -64,23 +46,8 @@ export default function Shell() {
       </button>
 
       <div className="border border-red-500 mt-4 p-2">
-        {focusedCharm ? <CharmRunner charm={focusedCharm} /> : <div>No focused charm</div>}
+        <div ref={containerRef}></div>
       </div>
-
-      {/* <div className="mt-4">
-        {charms.map((charm) => (
-          <div key={charm.entityId} className="p-4 mb-4 border rounded">
-            <h2 className="text-lg font-bold">{charm.name}</h2>
-            <div>{charm.ui}</div>
-            <button
-              onClick={() => removeCharm(charm.entityId)}
-              className="mt-2 px-2 py-1 bg-red-500 text-white rounded"
-            >
-              Close
-            </button>
-          </div>
-        ))}
-      </div> */}
     </div>
   );
 }
