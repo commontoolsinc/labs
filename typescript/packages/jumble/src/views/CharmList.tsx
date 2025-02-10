02100855a436a79f405964ebca86fca4c16d59eb
Jesse Andrews <anotherjesse@gmail.com>
2025-02-10 13:54:07 -0500

    don't break if charm has empty cell

--- typescript/packages/jumble/src/views/CharmList.tsx
+++ typescript/packages/jumble/src/views/CharmList.tsx
@@ -66,20 +66,24 @@ export default function CharmList() {
         </div>
       </os-common-import>
       {charms.map((charm, index) => (
-        <Card details key={index}>
-          <NavLink to={`/charm/${charmId(charm)}`}>
-            <div className="p-4" style={{ viewTransitionName: `charm-${charmId(charm)}` }}>
-              <h3 className="text-xl font-semibold text-gray-800 mb-4">
-                {charm.cell.get()[NAME] || "Unnamed Charm"}
-              </h3>
-              <div className="w-full bg-gray-50 rounded border border-gray-100 p-3">
-                <pre className="w-full h-24 overflow-hidden whitespace-pre-wrap text-xs text-gray-500">
-                  {JSON.stringify(charm.cell.get()[UI], null, 2)}
-                </pre>
-              </div>
-            </div>
-          </NavLink>
-        </Card>
+        <>{
+          charm.cell && (
+            <Card details key={index}>
+              <NavLink to={`/charm/${charmId(charm)}`}>
+                <div className="p-4" style={{ viewTransitionName: `charm-${charmId(charm)}` }}>
+                  <h3 className="text-xl font-semibold text-gray-800 mb-4">
+                    {charm.cell.get()?.[NAME] || "Unnamed Charm"}
+                  </h3>
+                  <div className="w-full bg-gray-50 rounded border border-gray-100 p-3">
+                    <pre className="w-full h-24 overflow-hidden whitespace-pre-wrap text-xs text-gray-500">
+                      {JSON.stringify(charm.cell.get()?.[UI], null, 2)}
+                    </pre>
+                  </div>
+                </div>
+              </NavLink>
+            </Card>
+          )
+        }</>
       ))}
     </div>
   );
