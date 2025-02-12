02f50e2e029d5fdb86ffa56d597af4edbd81229a
Jesse Andrews <anotherjesse@gmail.com>
2025-02-12 11:47:46 -0500

    running charms in deno works

--- typescript/packages/common-cli/main.ts
+++ typescript/packages/common-cli/main.ts
@@ -1,12 +1,23 @@
-// mod.ts in your Deno CLI project
-import { CharmManager } from "@commontools/charm";
+import { CharmManager, createStorage } from "@commontools/charm";
 
-// Your Deno CLI logic here
 async function main() {
-  const charm = new CharmManager("testing", "remote");
-  await charm.init();
-  const charms = await charm.getCharms();
-  console.log(JSON.stringify(charms, null, 2));
+  const storage = createStorage({
+    type: "remote",
+    replica: "anotherjesse-test5",
+    url: new URL("https://toolshed.saga-castor.ts.net/"),
+  });
+  const manager = new CharmManager(storage);
+  const charms = await manager.getCharms();
+
+  charms.sink((charms) => {
+  //   console.log(JSON.stringify({charms}, null, 2));
+    charms.forEach((charm) => {
+      manager.get(charm.cell.entityId["/"]);
+    });
+  });
+
+  // const charm = await charms.get("baedreihwuw4dbkvcel76siqztlxvloddfahgu535yupgxvkh5ml3wtqgqu");
+  // console.log({charm});
 }
 
 main();
