b9c3e4992f39a0078f92f596cb42a4d7d78be2b8
Ben Follington <5009316+bfollington@users.noreply.github.com>
2025-02-18 20:41:44 -0800

    Render semi-interactive charm preview in main feed (#415)

--- typescript/packages/jumble/src/components/Card.tsx
+++ typescript/packages/jumble/src/components/Card.tsx
@@ -1,16 +1,17 @@
 import { animated } from "@react-spring/web";
-
-interface CardProps {
+type CardProps = {
   details?: boolean;
   children: React.ReactNode;
-}
-export function Card({ details = false, children }: CardProps) {
+  className?: string;
+};
+export function Card({ details = false, children, className }: CardProps) {
   return (
     <animated.div
       className={`
-        ${details ? "border border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,0.5)] rounded-[4px] p-4 hover:translate-y-[-2px] hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,0.7)] transition-all" : ""}
-        transition-[border,box-shadow,transform] duration-100 ease-in-out
-      `}
+      ${details ? "border border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,0.5)] rounded-[4px] hover:translate-y-[-2px] hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,0.7)] transition-all" : ""}
+      transition-[border,box-shadow,transform] duration-100 ease-in-out
+      ${className || ""}
+    `}
     >
       {children}
     </animated.div>
