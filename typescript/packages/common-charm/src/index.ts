f444e35fa96d8e87d93f4492a63651e5c9f8d3aa
Jesse Andrews <anotherjesse@gmail.com>
2025-02-25 10:22:11 -0500

    allow compiling recipe without running

--- typescript/packages/common-charm/src/index.ts
+++ typescript/packages/common-charm/src/index.ts
@@ -1,6 +1,6 @@
 export { CharmManager, type Charm } from "./charm.js";
 export { saveRecipe } from "./syncRecipe.js";
 export { buildRecipe, tsToExports } from "./localBuild.js";
-export { iterate, castNewRecipe, saveNewRecipeVersion, compileAndRunRecipe } from "./iterate.js";
+export { iterate, castNewRecipe, saveNewRecipeVersion, compileAndRunRecipe, compileRecipe } from "./iterate.js";
 export { getIframeRecipe, type IFrameRecipe } from "./iframe/recipe.js";
 export { createStorage, type StorageConfig } from "./storage.js";
