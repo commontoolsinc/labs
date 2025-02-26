b5a8cde19e105c294eb9cba447887dd0e8c556e6
Ben Follington <5009316+bfollington@users.noreply.github.com>
2025-02-26 13:29:10 -0800

    Require file extensions on non-package imports (#468)

--- typescript/packages/jumble/eslint.config.js
+++ typescript/packages/jumble/eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import importPlugin from "eslint-plugin-import";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
@@ -16,6 +17,7 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      "import": importPlugin
     },
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
@@ -23,6 +25,13 @@ export default tseslint.config(
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
+      ],
+      "import/extensions": [
+        "error",
+        "always",
+        {
+          "ignorePackages": true,
+        }
       ],
     },
   },
