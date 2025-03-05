fa4d2ecb8b6b0e462db85a0724616148d7803ce7
Jesse Andrews <anotherjesse@gmail.com>
2025-03-05 10:39:09 -0500

    close testing browser after integration tests

--- typescript/packages/jumble/integration/smoke-test.ts
+++ typescript/packages/jumble/integration/smoke-test.ts
@@ -9,52 +9,56 @@ async function main() {
   const { charmId, space } = await addCharm();
   console.log(`Charm added`, { charmId, space });
 
-  const browser = await launch();
-  console.log(`Waiting to open website at ${FRONTEND_URL}`);
-  const page = await browser.newPage(FRONTEND_URL);
-  console.log(`Opened website at ${FRONTEND_URL}`);
+  let browser;
+  try {
+    browser = await launch();
+    console.log(`Waiting to open website at ${FRONTEND_URL}`);
+    const page = await browser.newPage(FRONTEND_URL);
+    console.log(`Opened website at ${FRONTEND_URL}`);
 
-  console.log("Logging in");
-  await login(page);
+    console.log("Logging in");
+    await login(page);
 
-  // await sleep(1000);
+    console.log("Checking if logged in");
+    const anchor = await page.waitForSelector("nav a");
+    assert(
+      (await anchor.innerText()) === "common-knowledge",
+      "Logged in and Common Knowledge title renders",
+    );
 
-  console.log("Checking if logged in");
-  const anchor = await page.waitForSelector("nav a");
-  assert(
-    (await anchor.innerText()) === "common-knowledge",
-    "Logged in and Common Knowledge title renders",
-  );
+    await page.goto(`${FRONTEND_URL}${space}/${charmId}`);
+    console.log(`Waiting for charm to render`);
 
-  await page.goto(`${FRONTEND_URL}${space}/${charmId}`);
-  console.log(`Waiting for charm to render`);
+    await sleep(5000);
+    await page.waitForSelector("a[aria-current='charm-title']");
+    const el = await page.$("a[aria-current='charm-title']");
+    console.log(await el?.innerText());
 
-  await sleep(5000);
-  await page.waitForSelector("a[aria-current='charm-title']");
-  const el = await page.$("a[aria-current='charm-title']");
-  console.log(await el?.innerText());
-  assert(
-    (await el!.innerText()) === "Simple Value: 1",
-    "Charm renders",
-  );
+    assert(
+      (await el!.innerText()) === "Simple Value: 1",
+      "Charm renders",
+    );
 
-  console.log("Clicking button");
-  const button = await page.waitForSelector(
-    "div[aria-label='charm-content'] button",
-  );
-  await button.click();
+    console.log("Clicking button");
+    const button = await page.waitForSelector(
+      "div[aria-label='charm-content'] button",
+    );
+    await button.click();
 
-  // FIXME(ja): sleep to let changes propagate to remote storage
-  await sleep(5000);
+    // FIXME(ja): sleep to let changes propagate to remote storage
+    await sleep(5000);
 
-  console.log("Checking if title changed");
-  const el2 = await page.$("a[aria-current='charm-title']");
-  assert(
-    (await el2!.innerText()) === "Simple Value: 2",
-    "Title changed",
-  );
-
-  await browser.close();
+    console.log("Checking if title changed");
+    const el2 = await page.$("a[aria-current='charm-title']");
+    assert(
+      (await el2!.innerText()) === "Simple Value: 2",
+      "Title changed",
+    );
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
+  }
 }
 
 try {
