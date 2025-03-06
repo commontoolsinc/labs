65dd1a298b29c385c259eef172553f3254de512c
Irakli Gozalishvili <contact@gozala.io>
2025-03-05 16:45:32 -0800

    feat: update schema removing need for coalesce (#525)

--- typescript/packages/common-memory/migrate.ts
+++ typescript/packages/common-memory/migrate.ts
@@ -1,8 +1,4 @@
-import {
-  Database,
-  SqliteError,
-  Transaction as DBTransaction,
-} from "@db/sqlite";
+import { Database } from "@db/sqlite";
 
 const MIGRATIONS = new URL("./migrations/", import.meta.url);
 
