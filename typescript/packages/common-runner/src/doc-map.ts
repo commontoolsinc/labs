1537ee38f0ae5f976a5722d78f8d8aa139e34d69
Bernhard Seefeld <berni@common.tools>
2025-03-06 10:06:12 -0800

    fix: createDoc already calls setDocByEntityId

--- typescript/packages/common-runner/src/doc-map.ts
+++ typescript/packages/common-runner/src/doc-map.ts
@@ -183,7 +183,6 @@ export function getDocByEntityId<T = any>(
   if (typeof entityId === "string") entityId = JSON.parse(entityId) as EntityId;
   doc = createDoc<T>(undefined as T, entityId, space);
   doc.sourceCell = sourceIfCreated;
-  setDocByEntityId(space, entityId, doc);
   return doc;
 }
 
