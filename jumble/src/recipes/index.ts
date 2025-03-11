b78a8a77f0a09a2a33073511c60adde865c5a01f
Jesse Andrews <anotherjesse@gmail.com>
2025-03-11 10:47:56 -0700

    we don't import recipes by default

--- jumble/src/recipes/index.ts
+++
@@ -1,7 +0,0 @@
-import smolIframeSrc from "./smolIframe.tsx?raw";
-import smolIframe from "./smolIframe.tsx";
-import { addRecipe } from "@commontools/runner";
-
-// Cast the source to string to fix TypeScript error
-addRecipe(smolIframe, smolIframeSrc as unknown as string);
-export { smolIframe };
