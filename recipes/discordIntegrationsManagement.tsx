aaf5ae3c3375b21e07c22e19ed0d731a201bafbf
jakedahn <jake@common.tools>
2025-03-18 00:17:08 -0700

    adding discord integrations manager recipe, and updating deno.json to add discord scripts

---
+++ recipes/discordIntegrationsManagement.tsx
@@ -0,0 +1,59 @@
+import { h } from "@commontools/html";
+import {
+  derive,
+  JSONSchema,
+  NAME,
+  recipe,
+  Schema,
+  UI,
+} from "@commontools/builder";
+
+const IntegrationSpaceCharmSchema = {
+  type: "object",
+  properties: {
+    space: { type: "string" },
+    charmId: { type: "string" },
+  },
+} as const satisfies JSONSchema;
+type IntegrationSpaceCell = Schema<typeof IntegrationSpaceCharmSchema>;
+
+const InputSchema = {
+  type: "object",
+  properties: {
+    charms: { type: "array", items: IntegrationSpaceCharmSchema },
+  },
+} as const satisfies JSONSchema;
+
+const ResultSchema = {
+  type: "object",
+  properties: {
+    charms: {
+      type: "array",
+      items: IntegrationSpaceCharmSchema,
+    },
+  },
+} as const satisfies JSONSchema;
+
+export default recipe(
+  InputSchema,
+  ResultSchema,
+  ({ charms }) => {
+    derive(charms, (charms) => {
+      console.log("charms", charms);
+    });
+    return {
+      [NAME]: "Discord Integration Management",
+      [UI]: (
+        <div>
+          <h1>Discord Integration Management</h1>
+          <pre>
+            {derive(charms, (charms) => {
+              return JSON.stringify(charms, null, 2);
+            })}
+          </pre>
+        </div>
+      ),
+      charms,
+    };
+  },
+);
