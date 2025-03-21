2f33f60695ce68dcdb0a86a363edf977cbb352ad
Bernhard Seefeld <berni@common.tools>
2025-03-21 11:36:16 -0700

    better comments in test

--- runner/test/push-conflict.test.ts
+++ runner/test/push-conflict.test.ts
@@ -10,35 +10,42 @@ storage.setSigner(await Identity.fromPassphrase("test operator"));
 
 describe("Push conflict", () => {
   it("should resolve push conflicts", async () => {
-    const charmsDoc = getDoc<any[]>([], "charms", "push conflict");
-    const charms = charmsDoc.asCell();
-    await storage.syncCell(charms);
+    const listDoc = getDoc<any[]>([], "list", "push conflict");
+    const list = listDoc.asCell();
+    await storage.syncCell(list);
 
     const memory = new VolatileStorageProvider("push conflict");
 
-    console.log("sending");
-    await memory.sync(charmsDoc.entityId, true);
+    // Update memory without notifying main storage
+    await memory.sync(listDoc.entityId, true); // Get current value
     await memory.send([{
-      entityId: charmsDoc.entityId,
+      entityId: listDoc.entityId,
       value: { value: [1, 2, 3] },
-    }], true); // Update memory without notifying
+    }], true); // true = do not notify main storage
 
     let retryCalled = false;
-    charmsDoc.retry = [(value) => {
+    listDoc.retry = [(value) => {
       retryCalled = true;
       return value;
     }];
 
-    console.log("pushing");
-    charms.push(4);
-    expect(charms.get()).toEqual([4]);
+    list.push(4);
+
+    // This is locally ahead of the db, and retry wasn't called yet.
+    expect(list.get()).toEqual([4]);
+    expect(retryCalled).toEqual(false);
 
     await storage.synced();
 
+    // We successfully replayed the change on top of the db:
+    expect(list.get()).toEqual([1, 2, 3, 4]);
     expect(retryCalled).toEqual(true);
-    expect(charms.get()).toEqual([1, 2, 3, 4]);
 
-    // Clears timers
+    // Retry list should be empty now, since the change was applied.
+    expect(!!listDoc.retry?.length).toBe(false);
+
+    // Wait for database to settle any follow-up batches, which the system will
+    // realize are all redundant. But otherwise the test suite complains.
     await storage.synced();
   });
 });
