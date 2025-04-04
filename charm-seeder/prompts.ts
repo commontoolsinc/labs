2b253f7e6ce0e151bcd735639b0c657027e48082
Jesse Andrews <anotherjesse@gmail.com>
2025-04-04 11:19:01 -0400

    ideas from jake

--- charm-seeder/prompts.ts
+++ charm-seeder/prompts.ts
@@ -1,6 +1,6 @@
 import { CommandType } from "./commands.ts";
 
-export const prompts = [{
+export const scenarios = [{
   type: CommandType.New,
   prompt: "a 2048 game",
 }, {
@@ -9,4 +9,11 @@ export const prompts = [{
 }, {
   type: CommandType.New,
   prompt: "todo list",
+}, {
+  type: CommandType.New,
+  prompt: "create json to describe 25 mexican meal recipes",
+}, {
+  type: CommandType.New,
+  prompt:
+    "i'd like to keep a shopping list.  let me import from markdown with existing selections.  keep it clean and simple!",
 }];
