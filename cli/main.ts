653a0a4bcb96cdda9d3ae6929990f1ca5a301347
Bernhard Seefeld <berni@common.tools>
2025-04-08 14:57:13 -0700

    * rename "name" to "spaceName" and add ability to provide DID * add ability to provide path to key * add ability to pass inputs (will be parsed as JSON) * add ability to reference other charms via "@#.../path"

--- cli/main.ts
+++ cli/main.ts
@@ -3,14 +3,38 @@ import { parseArgs } from "@std/cli/parse-args";
 import { CharmManager, compileRecipe } from "@commontools/charm";
 import {
   getEntityId,
+  idle,
   isStream,
   setBobbyServerUrl,
   storage,
 } from "@commontools/runner";
-import { createSession, Identity } from "@commontools/identity";
+import {
+  createSession,
+  type DID,
+  Identity,
+  type Session,
+} from "@commontools/identity";
+import { assert } from "@commontools/memory/fact";
 
-const { name, charmId, recipeFile, cause, quit } = parseArgs(Deno.args, {
-  string: ["name", "charmId", "recipeFile", "cause"],
+const {
+  spaceName,
+  spaceDID,
+  charmId,
+  recipeFile,
+  cause,
+  input,
+  keyPath,
+  quit,
+} = parseArgs(Deno.args, {
+  string: [
+    "spaceName",
+    "spaceDID",
+    "charmId",
+    "recipeFile",
+    "cause",
+    "input",
+    "keyPath",
+  ],
   boolean: ["quit"],
   default: { quit: false },
 });
@@ -24,20 +48,46 @@ storage.setRemoteStorage(new URL(toolshedUrl));
 setBobbyServerUrl(toolshedUrl);
 
 async function main() {
-  const identity = await Identity.fromPassphrase(OPERATOR_PASS);
-  const session = await createSession({
-    identity,
-    name: name!,
-  });
+  if (!spaceName && !spaceDID) {
+    console.error("No space name or space DID provided");
+    Deno.exit(1);
+  }
 
-  console.log("params:", {
-    session,
-    charmId,
-    recipeFile,
-    cause,
-    quit,
-    toolshedUrl,
-  });
+  if (spaceName?.startsWith("~") && !spaceDID) {
+    console.error(
+      "If space name starts with ~, then space DID must be provided",
+    );
+    Deno.exit(1);
+  }
+
+  if (spaceDID && !spaceDID.startsWith("did:key:")) {
+    console.error("Space DID must start with did:key:");
+    Deno.exit(1);
+  }
+
+  let identity: Identity;
+  if (keyPath) {
+    try {
+      const pkcs8Key = await Deno.readFile(keyPath);
+      identity = await Identity.fromPkcs8(pkcs8Key, {
+        implementation: "noble",
+      });
+    } catch (e) {
+      throw new Error(`Could not read key at ${keyPath}.`);
+    }
+  } else {
+    identity = await Identity.fromPassphrase(OPERATOR_PASS);
+  }
+
+  const session = {
+    private: spaceName?.startsWith("~") ?? false,
+    // TODO(seefeld): See what happens if we don't provide a name.
+    name: spaceName ?? undefined as unknown as string,
+    space: spaceDID as DID ?? await identity.derive(spaceName!),
+    as: identity,
+  } satisfies Session;
+
+  // TODO(seefeld): It only wants the space, so maybe we simplify the above and just space the space did?
   const manager = new CharmManager(session);
   const charms = manager.getCharms();
   charms.sink((charms) => {
@@ -67,11 +117,31 @@ async function main() {
     });
   }
 
+  let inputValue: unknown;
+
+  if (input !== undefined && input !== "") {
+    // Find all `@#<hex hash>[/<url escaoped path[/<more paths>[/...]]]`
+    // and replace them with the corresponding JSON object.
+    //
+    // Example: "@#bafed0de/path/to/value" and "{ foo: @#bafed0de/a/path }"
+    const regex = /(?<!"[^"]*?)@#([a-f0-9]+)((?:\/[^\/\s"',}]+)*?)(?![^"]*?")/g;
+    inputValue = input.replace(regex, (_, hash, path) =>
+      JSON.stringify({
+        cell: { "/": hash, path: path.split("/").map(decodeURIComponent) },
+      }));
+    try {
+      inputValue = JSON.parse(input);
+    } catch (error) {
+      console.error("Error parsing input:", error);
+      Deno.exit(1);
+    }
+  }
+
   if (recipeFile) {
     try {
       const recipeSrc = await Deno.readTextFile(recipeFile);
       const recipe = await compileRecipe(recipeSrc, "recipe", []);
-      const charm = await manager.runPersistent(recipe, undefined, cause);
+      const charm = await manager.runPersistent(recipe, inputValue, cause);
       const charmWithSchema = (await manager.get(charm))!;
       charmWithSchema.sink((value) => {
         console.log("running charm:", getEntityId(charm), value);
@@ -82,6 +152,7 @@ async function main() {
         updater.send({ newValues: ["test"] });
       }
       if (quit) {
+        await idle();
         await storage.synced();
         Deno.exit(0);
       }
