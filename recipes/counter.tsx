0988327ae6a762d92d7a02b12095726db65f1284
Irakli Gozalishvili <contact@gozala.io>
2025-05-02 12:35:15 -0700

    fix: counter example

--- recipes/counter.tsx
+++ recipes/counter.tsx
@@ -14,57 +14,38 @@ import {
 
 // Different way to define the same schema, using 'schema' helper function,
 // let's as leave off `as const satisfies JSONSchema`.
-const inputSchema = schema({
+const model = schema({
   type: "object",
   properties: {
-    value: { type: "number", default: 0 },
+    value: { type: "number", default: 0, asCell: true },
   },
   default: { value: 0 },
 });
 
-const outputSchema = {
-  type: "object",
-  properties: {
-    value: { type: "number", default: 0 },
-  },
-} as const satisfies JSONSchema;
-
-const updateSchema = {
-  type: "object",
-  properties: {
-    value: { type: "number", default: 0 },
-  },
-  title: "update values",
-} as const satisfies JSONSchema;
-
-const increment = handler(updateSchema, inputSchema, (_, state) => {
-  debugger;
-  console.log("increment");
-  state.value = state.value + 1;
-  console.log(state.value);
+const increment = handler({}, model, (_, state) => {
+  state.value.set(state.value.get() + 1);
 });
 
-const decrement = handler(updateSchema, inputSchema, (_, state) => {
-  console.log("decrement");
-  state.value = state.value - 1;
+const decrement = handler({}, model, (_, state) => {
+  state.value.set(state.value.get() - 1);
 });
 
 const isOdd = (n: number) => n % 2 > 0;
 
-export default recipe(inputSchema, outputSchema, (cell) => {
+export default recipe(model, model, (cell) => {
   return {
     [NAME]: str`Simple counter: ${derive(cell.value, String)}`,
     [UI]: (
       <div>
         <button type="button" onClick={increment(cell)}>+</button>
-        <p>{cell.value}</p>
-        {
-          /* {ifElse(
+        {/* <span>{derive(cell.value, String)}</p> */}
+
+        {ifElse(
           derive(cell.value, isOdd),
           <i>{cell.value}</i>,
           <b>{cell.value}</b>,
-        )} */
-        }
+        )}
+
         <button type="button" onClick={decrement(cell)}>-</button>
       </div>
     ),
