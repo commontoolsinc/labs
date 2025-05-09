fc373099e2e1f3158366a3baa33d27c685333b46
Jesse Andrews <anotherjesse@gmail.com>
2025-05-09 13:51:24 -0400

    wip

--- packages/runner/src/builtins/llm.ts
+++ packages/runner/src/builtins/llm.ts
@@ -1,14 +1,20 @@
 import { type DocImpl, getDoc } from "../doc.ts";
-import { DEFAULT_MODEL_NAME, LLMClient, LLMRequest } from "@commontools/llm";
+import {
+  DEFAULT_MODEL_NAME,
+  LLMClient,
+  LLMGenerateObjectRequest,
+  LLMGenerateObjectResponse,
+  LLMRequest,
+} from "@commontools/llm";
 import { type Action, idle } from "../scheduler.ts";
 import { refer } from "merkle-reference";
 import { type ReactivityLog } from "../scheduler.ts";
-import { BuiltInLLMParams, BuiltInLLMState } from "@commontools/builder";
+import {
+  BuiltInGenerateObjectParams,
+  BuiltInLLMParams,
+} from "@commontools/builder";
 
 const client = new LLMClient();
-
-// TODO(ja): investigate if generateText should be replaced by
-// fetchData with streaming support
 
 /**
  * Generate data via an LLM.
@@ -78,7 +84,6 @@ export function llm(
       })),
       stop: stop ?? "",
       maxTokens: maxTokens ?? 4096,
-      stream: true,
       model: model ?? DEFAULT_MODEL_NAME,
       metadata: {
         // FIXME(ja): how do we get the context of space/charm id here
@@ -127,6 +132,133 @@ export function llm(
         pending.setAtPath([], false, log);
         result.setAtPath([], text, log);
         partial.setAtPath([], text, log);
+        requestHash.setAtPath([], hash, log);
+      })
+      .catch(async (error) => {
+        if (thisRun !== currentRun) return;
+
+        console.error("Error generating data", error);
+
+        await idle();
+
+        pending.setAtPath([], false, log);
+        result.setAtPath([], undefined, log);
+        partial.setAtPath([], undefined, log);
+
+        // TODO(seefeld): Not writing now, so we retry the request after failure.
+        // Replace this with more fine-grained retry logic.
+        // requestHash.setAtPath([], hash, log);
+      });
+  };
+}
+
+/**
+ * Generate data via an LLM.
+ *
+ * Returns the complete result as `result` and the incremental result as
+ * `partial`. `pending` is true while a request is pending.
+ *
+ * @param messages - list of messages to send to the LLM. - alternating user and assistant messages.
+ *  - if you end with an assistant message, the LLM will continue from there.
+ *  - if both prompt and messages are empty, no LLM call will be made,
+ *    result and partial will be undefined.
+ * @param model - A doc to store the model to use.
+ * @param system - A doc to store the system message.
+ * @param stop - A doc to store (optional) stop sequence.
+ * @param maxTokens - A doc to store the maximum number of tokens to generate.
+ *
+ * @returns { pending: boolean, result?: string, partial?: string } - As individual
+ *   docs, representing `pending` state, final `result` and incrementally
+ *   updating `partial` result.
+ */
+export function generateObject(
+  inputsCell: DocImpl<BuiltInGenerateObjectParams>,
+  sendResult: (result: any) => void,
+  _addCancel: (cancel: () => void) => void,
+  cause: any,
+  parentDoc: DocImpl<any>,
+): Action {
+  const pending = getDoc(
+    false,
+    { generateObject: { pending: cause } },
+    parentDoc.space,
+  );
+  const result = getDoc<string | undefined>(
+    undefined,
+    {
+      generateObject: { result: cause },
+    },
+    parentDoc.space,
+  );
+  const partial = getDoc<string | undefined>(
+    undefined,
+    {
+      generateObject: { partial: cause },
+    },
+    parentDoc.space,
+  );
+  const requestHash = getDoc<string | undefined>(
+    undefined,
+    {
+      generateObject: { requestHash: cause },
+    },
+    parentDoc.space,
+  );
+
+  sendResult({ pending, result, partial, requestHash });
+
+  let currentRun = 0;
+  let previousCallHash: string | undefined = undefined;
+
+  return (log: ReactivityLog) => {
+    const thisRun = ++currentRun;
+
+    const { prompt, maxTokens, model, schema } =
+      inputsCell.getAsQueryResult([], log) ?? {};
+
+    if (!prompt || !schema) {
+      pending.setAtPath([], false, log);
+      return;
+    }
+
+    const generateObjectParams: LLMGenerateObjectRequest = {
+      prompt,
+      maxTokens: maxTokens ?? 8192,
+      model,
+      schema,
+      metadata: {
+        context: "charm",
+      },
+      cache: true,
+    };
+
+    const hash = refer(generateObjectParams).toString();
+
+    // Return if the same request is being made again, either concurrently (same
+    // as previousCallHash) or when rehydrated from storage (same as the
+    // contents of the requestHash doc).
+    if (hash === previousCallHash || hash === requestHash.get()) return;
+    previousCallHash = hash;
+
+    result.setAtPath([], undefined, log);
+    partial.setAtPath([], undefined, log);
+
+    const updatePartial = (text: string) => {
+      if (thisRun != currentRun) return;
+      partial.setAtPath([], text, log);
+    };
+
+    const resultPromise = client.generateObject(generateObjectParams);
+
+    resultPromise
+      .then(async (generateObjectResult) => {
+        const object = generateObjectResult.object;
+        if (thisRun !== currentRun) return;
+
+        await idle();
+
+        pending.setAtPath([], false, log);
+        result.setAtPath([], object, log);
         requestHash.setAtPath([], hash, log);
       })
       .catch(async (error) => {
