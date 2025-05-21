bcb5695dc42eefcb0a9a78de8fb75e6e0fef9fae
Robin McCollum <robin.mccollum@gmail.com>
2025-05-21 15:53:43 -0700

    Move this check out of the list check The list check will get some extra code that we don't want to run just because result.get() is undefind.

--- packages/runner/src/builtins/map.ts
+++ packages/runner/src/builtins/map.ts
@@ -60,10 +60,14 @@ export function map(
   return (log: ReactivityLog) => {
     let { list, op } = inputsCell.getAsQueryResult([], log);
 
+    // If the result's value is undefined, set it to the empty array.
+    if (result.get() === undefined) {
+      result.setAtPath([], [], log);
+    }
     // If the list is undefined it means the input isn't available yet.
     // Correspondingly, the result should be []. TODO: Maybe it's important to
     // distinguish empty inputs from undefined inputs?
-    if (list === undefined || result.get() === undefined) {
+    if (list === undefined) {
       result.setAtPath([], [], log);
       return;
     }
