2557fda6d07ecadae49b19ae3123626ba6c9777d
Ellyse <ellyse@common.tools>
2025-05-22 12:19:15 -0700

    made a small recipe to test ifElse and everything worked. i think it'd be good for us to have more small examples.

---
+++ recipes/ifelse_test.tsx
@@ -0,0 +1,82 @@
+import { h } from "@commontools/html";
+import {
+  derive,
+  handler,
+  ifElse,
+  JSONSchema,
+  lift,
+  NAME,
+  recipe,
+  schema,
+  str,
+  UI,
+} from "@commontools/builder";
+
+const inputSchema = schema({
+  type: "object",
+  properties: {
+    counter: {
+      type: "integer",
+      default: 0,
+      asCell: true
+    },
+  },
+  default: { counter: 0 },
+});
+
+const outputSchema = schema({
+  type: "object",
+  properties: {},
+});
+
+const incCounter = handler({}, {
+  type: "object",
+  properties: {
+    counter: {
+      type: "integer",
+      asCell: true
+    },
+  },
+}, (_event, state) => {
+  if (state.counter) {
+    const current_count = state.counter.value;
+    console.log("current count=", current_count);
+    state.counter.set(current_count + 1);
+  }
+  else {
+    console.log("counter is undefined, ignoring");
+  }
+});
+
+const isEven = lift(({ counter }) => {
+  return counter % 2;
+})
+
+export default recipe(
+  inputSchema,
+  outputSchema,
+  ({ counter }) => {
+    const isEvenVal = isEven({ counter });
+    derive(counter, (c) => {
+      console.log("derive counter: ", c);
+    });
+    return {
+      [NAME]: str`counter: ${counter}`,
+      [UI]: (
+        <div>
+          {ifElse(isEvenVal,
+            <p>counter is odd  : {counter}</p>,
+            <p>counter is even : {counter}</p>,
+          )}
+          <p />
+          <button
+            type="button"
+            onClick={incCounter({ counter })}
+          >
+            |click me|
+          </button>
+        </div>
+      ),
+    };
+  }
+);
