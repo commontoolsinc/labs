285717a4211b7b1797fbf87bd0842a8bb2a31e54
Robin McCollum <robin.mccollum@gmail.com>
2025-05-29 10:28:10 -0700

    If all the charms in a space become disabled, stop the scheduler for that space.

--- packages/background-charm-service/src/service.ts
+++ packages/background-charm-service/src/service.ts
@@ -110,6 +110,15 @@ export class BackgroundCharmService {
       addCancel(scheduler.watch(didCharms));
     }
 
+    const removedSpaces = new Set(this.charmSchedulers.keys()).difference(dids);
+    for (const did of removedSpaces.values()) {
+      // we are no longer monitoring this space
+      const scheduler = this.charmSchedulers.get(did);
+      this.charmSchedulers.delete(did);
+      scheduler?.stop();
+      // TODO(@ubik2) I'm not sure if we need to call the cancel function returned by scheduler.watch
+    }
+
     return cancel;
   }
 }
