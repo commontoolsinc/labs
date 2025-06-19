9580e4fa6a4500d006a6cfb6379c60c19e759ecb
ubik2 <ubik2@users.noreply.github.com>
2025-06-18 21:59:06 -0700

    CT-475: Add format and content attributes to JSONSchema (#1260)
    * Expanded the JSONSchema definition to allow for the properties mentioned in the core or validation specifications

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -196,6 +196,15 @@ export type JSONSchema = {
   readonly minProperties?: number; // not validated
   readonly required?: readonly string[];
   readonly dependentRequired?: Readonly<Record<string, readonly string[]>>; // not validated
+
+  // Format annotations
+  readonly format?: string; // not validated
+
+  // Contents - none applied
+  readonly contentEncoding?: string;
+  readonly contentMediaType?: string;
+  readonly contentSchema?: JSONSchema | boolean;
+
   // Meta-Data
   readonly title?: string;
   readonly description?: string;
@@ -207,9 +216,12 @@ export type JSONSchema = {
   // Common Tools extensions
   readonly [ID]?: unknown;
   readonly [ID_FIELD]?: unknown;
+  // makes it so that your handler gets a Cell object for that property. So you can call .set()/.update()/.push()/etc on it.
   readonly asCell?: boolean;
+  // streams are what handler returns. if you pass that to another handler/lift and declare it as asSteam, you can call .send on it
   readonly asStream?: boolean;
-  readonly ifc?: { classification?: string[]; integrity?: string[] }; // temporarily used to assign labels like "confidential"
+  // temporarily used to assign labels like "confidential"
+  readonly ifc?: { classification?: string[]; integrity?: string[] };
 };
 
 // Built-in types
