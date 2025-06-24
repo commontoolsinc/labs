6c86b259e09c6a7573b9db3c148796ffe23d77fb
Jordan Santell <jsantell@users.noreply.github.com>
2025-06-24 14:56:41 -0700

    chore: Use local workspace when running CLI integration tests via deno (#1288)
    chore: Use local workspace when running CLI integration tests via deno. Otherwise, use ct binary found in path.

--- packages/cli/integration/integration.sh
+++ packages/cli/integration/integration.sh
@@ -6,10 +6,11 @@ error () {
   exit 1
 }
 
+
+CT="$([[ -n "$CT_CLI_INTEGRATION_USE_LOCAL" ]] && echo "deno task cli" || echo "ct")"
 if [ "$#" -eq 0 ]; then
   error "Missing required argument: API_URL"
 fi
-
 API_URL="$1"
 SPACE=$(mktemp -u XXXXXXXXXX) # generates a random space
 IDENTITY=$(mktemp)
@@ -23,19 +24,19 @@ echo "IDENTITY=$IDENTITY"
 echo "WORK_DIR=$WORK_DIR"
 
 # Create a key
-ct id new > $IDENTITY
+$CT id new > $IDENTITY
 
 # Check space is empty
-if [ "$(ct charm ls $SPACE_ARGS)" != "" ]; then
+if [ "$($CT charm ls $SPACE_ARGS)" != "" ]; then
   error "Space not empty." 
 fi
 
 # Create a new charm with {value:5} as input
-CHARM_ID=$(ct charm new $SPACE_ARGS $RECIPE_SRC)
+CHARM_ID=$($CT charm new $SPACE_ARGS $RECIPE_SRC)
 echo "Created charm: $CHARM_ID"
 
 # Retrieve the source code for $CHARM_ID to $WORK_DIR
-ct charm getsrc $SPACE_ARGS --charm $CHARM_ID $WORK_DIR
+$CT charm getsrc $SPACE_ARGS --charm $CHARM_ID $WORK_DIR
 
 # Check file was retrieved
 if [ ! -f "$WORK_DIR/main.tsx" ]; then
@@ -44,11 +45,11 @@ fi
 
 # Update the charm's source code
 sed -i 's/Simple counter:/Simple counter 2:/g' "$WORK_DIR/main.tsx"
-ct charm setsrc $SPACE_ARGS --charm $CHARM_ID $WORK_DIR/main.tsx
+$CT charm setsrc $SPACE_ARGS --charm $CHARM_ID $WORK_DIR/main.tsx
 
 # (Again) Retrieve the source code for $CHARM_ID to $WORK_DIR
 rm "$WORK_DIR/main.tsx"
-ct charm getsrc $SPACE_ARGS --charm $CHARM_ID $WORK_DIR
+$CT charm getsrc $SPACE_ARGS --charm $CHARM_ID $WORK_DIR
 
 # Check file was retrieved with modifications
 grep -q "Simple counter 2" "$WORK_DIR/main.tsx"
@@ -57,11 +58,11 @@ if [ $? -ne 0 ]; then
 fi
 
 # Apply new input to charm
-echo '{"value":5}' | ct charm apply $SPACE_ARGS --charm $CHARM_ID
+echo '{"value":5}' | $CT charm apply $SPACE_ARGS --charm $CHARM_ID
 
 # Check space has new charm with correct inputs and title
 TITLE="Simple counter 2: 5"
-ct charm ls $SPACE_ARGS | grep -q "$CHARM_ID $TITLE <unnamed>"
+$CT charm ls $SPACE_ARGS | grep -q "$CHARM_ID $TITLE <unnamed>"
 if [ $? -ne 0 ]; then
   error "Charm did not appear in list of space charms."
 fi
