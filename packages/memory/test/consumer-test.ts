6c7d9d97cefc4d7604edbb2337102f65260f354d
Robin McCollum <robin.mccollum@gmail.com>
2025-07-01 19:42:50 -0700

    tests that the label is included

--- packages/memory/test/consumer-test.ts
+++ packages/memory/test/consumer-test.ts
@@ -1142,7 +1142,11 @@ test(
                       changes: {
                         [doc]: {
                           "application/json": {},
-                          "application/label+json": {},
+                          "application/label+json": {
+                            [v3_label.cause.toString()]: {
+                              is: { classification: ["confidential"] },
+                            },
+                          },
                         },
                       },
                     },
