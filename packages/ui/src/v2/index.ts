ebdadb4975e486ed14a8324d3717b432874a2315
Ben Follington <5009316+bfollington@users.noreply.github.com>
2025-07-11 15:05:31 +1000

    Hack in `<ct-cell-tester />` as a simplified repro of `<ct-outliner />`

--- packages/ui/src/v2/index.ts
+++ packages/ui/src/v2/index.ts
@@ -60,6 +60,7 @@ export * from "./components/ct-vstack/index.ts";
 export * from "./components/ct-select/index.ts";
 export * from "./components/ct-message-input/ct-message-input.ts";
 export * from "./components/ct-outliner/index.ts";
+export * from "./components/ct-cell-tester/index.ts";
 
 // Type exports
 export type * from "./utils/types.ts";
