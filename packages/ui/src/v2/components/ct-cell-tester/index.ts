8bfb79e734cc5acd843771076becfc9d10c906a1
Ben Follington <5009316+bfollington@users.noreply.github.com>
2025-07-11 16:47:03 +1000

    Clean up

--- packages/ui/src/v2/components/ct-cell-tester/index.ts
+++
@@ -1,7 +0,0 @@
-import { CTCellTester } from "./ct-cell-tester.ts";
-
-if (!customElements.get("ct-cell-tester")) {
-  customElements.define("ct-cell-tester", CTCellTester);
-}
-
-export { CTCellTester };
