dc6dfcec455e58ed8c8a040d63df96889deff459
Jesse Andrews <anotherjesse@gmail.com>
2025-07-15 17:01:36 -0700

    update api

--- packages/static/assets/types/commontools.d.ts
+++ packages/static/assets/types/commontools.d.ts
@@ -182,10 +182,13 @@ export type LiftFunction = {
     <T>(implementation: (input: T) => any): ModuleFactory<T, ReturnType<typeof implementation>>;
     <T extends (...args: any[]) => any>(implementation: T): ModuleFactory<Parameters<T>[0], ReturnType<T>>;
 };
+export type HandlerState<T> = T extends Cell<any> ? T : T extends Stream<any> ? T : T extends Array<infer U> ? ReadonlyArray<HandlerState<U>> : T extends object ? {
+    readonly [K in keyof T]: HandlerState<T[K]>;
+} : T;
 export type HandlerFunction = {
-    <E extends JSONSchema = JSONSchema, T extends JSONSchema = JSONSchema>(eventSchema: E, stateSchema: T, handler: (event: Schema<E>, props: Schema<T>) => any): ModuleFactory<CellToOpaque<SchemaWithoutCell<T>>, SchemaWithoutCell<E>>;
-    <E, T>(eventSchema: JSONSchema, stateSchema: JSONSchema, handler: (event: E, props: T) => any): ModuleFactory<CellToOpaque<T>, E>;
-    <E, T>(handler: (event: E, props: T) => any): ModuleFactory<CellToOpaque<T>, E>;
+    <E extends JSONSchema = JSONSchema, T extends JSONSchema = JSONSchema>(eventSchema: E, stateSchema: T, handler: (event: Schema<E>, props: HandlerState<Schema<T>>) => any): ModuleFactory<CellToOpaque<SchemaWithoutCell<T>>, SchemaWithoutCell<E>>;
+    <E, T>(eventSchema: JSONSchema, stateSchema: JSONSchema, handler: (event: E, props: HandlerState<T>) => any): ModuleFactory<CellToOpaque<T>, E>;
+    <E, T>(handler: (event: E, props: HandlerState<T>) => any): ModuleFactory<CellToOpaque<T>, E>;
 };
 export type DeriveFunction = <In, Out>(input: Opaque<In>, f: (input: In) => Out | Promise<Out>) => OpaqueRef<Out>;
 export type ComputeFunction = <T>(fn: () => T) => OpaqueRef<T>;
