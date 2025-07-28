4e9617dff2f1cc21a82f5f899ed5d3c1add8c669
Ben Follington <5009316+bfollington@users.noreply.github.com>
2025-07-28 16:25:44 +1000

    WIP `<ct-kanban />`

--- packages/ui/src/v2/index.ts
+++ packages/ui/src/v2/index.ts
@@ -61,6 +61,7 @@ export * from "./components/ct-vstack/index.ts";
 export * from "./components/ct-select/index.ts";
 export * from "./components/ct-message-input/ct-message-input.ts";
 export * from "./components/ct-outliner/index.ts";
+export * from "./components/ct-kanban/index.ts";
 
 // Type exports
 export type * from "./utils/types.ts";
