63f31ac1e0dd60c4a43ce53178ec546770ba9095
Bernhard Seefeld <berni@common.tools>
2025-07-29 14:49:38 -0500

    fix test for shim flow

--- packages/runner/test/schema.test.ts
+++ packages/runner/test/schema.test.ts
@@ -371,7 +371,6 @@ describe("Schema Support", () => {
       const log = txToReactivityLog(tx);
       const reads = sortAndCompactPaths(log.reads);
       console.log(reads);
-      expect(reads.length).toEqual(9);
       expect(reads).toContainEqual({
         space,
         id: toURI(linkEntityId),
