f5c6cb939afb4ece67094b4e84a09a67d2a74b4b
Ben Follington <5009316+bfollington@users.noreply.github.com>
2025-08-01 12:12:22 +1000

    Add basic `ct-list` types to `jsx.d.ts`

--- packages/static/assets/types/jsx.d.ts
+++ packages/static/assets/types/jsx.d.ts
@@ -1,7 +1,23 @@
+type Child = {
+  children: string[];
+};
+
+type Cell<T> = {
+  get(): T,
+  set(value: T): void;
+}
+
 declare global {
   namespace JSX {
     interface IntrinsicElements {
       [elemName: string]: any;
+      "ct-list": {
+              value?: { title: string }[],
+              $value?: Cell<{ title: string }[]>,
+              editable?: boolean,
+              title?: string,
+              onct-remove-item?: (e: { detail: { item: Cell<{ title: string }> } }) => void,
+            } & Child;
     }
   }
 }
