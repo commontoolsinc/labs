49c8ee74aca1762eef0319f2bfbbfcbc6c9ed0c9
jakedahn <jake@common.tools>
2025-08-04 10:43:30 -0600

    Revert "without CTS, our types allow invalid handler (#1551)"
    This reverts commit 2b7fa35868b379ae06b15ec959b34048383edeea.

--- packages/runner/test/module.test.ts
+++ packages/runner/test/module.test.ts
@@ -100,7 +100,6 @@ describe("module", () => {
           props.x = event.clientX;
           props.y = event.clientY;
         },
-        { proxy: true },
       );
       expect(typeof clickHandler).toBe("function");
       expect(isModule(clickHandler)).toBe(true);
@@ -112,7 +111,6 @@ describe("module", () => {
           props.x = event.clientX;
           props.y = event.clientY;
         },
-        { proxy: true },
       );
       const stream = clickHandler({ x: opaqueRef(10), y: opaqueRef(20) });
       expect(isOpaqueRef(stream)).toBe(true);
@@ -203,7 +201,6 @@ describe("module", () => {
           props.x = event.clientX;
           props.y = event.clientY;
         },
-        { proxy: true },
       );
       const stream = clickHandler.with({ x: opaqueRef(10), y: opaqueRef(20) });
       expect(isOpaqueRef(stream)).toBe(true);
