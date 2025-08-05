6fa90bb627ff59f2904e93b566240d4296c6d84a
Ben Follington <5009316+bfollington@users.noreply.github.com>
2025-08-05 15:23:40 +1000

    Add `<ct-outliner />` types

--- packages/static/assets/types/jsx.d.ts
+++ packages/static/assets/types/jsx.d.ts
@@ -1,7 +1,30 @@
+type Child = {
+  children: string[];
+};
+
+type Cell<T> = {
+  get(): T,
+  set(value: T): void;
+}
+
+type Charm = any
+
+type OutlinerNode = {
+  body: string,
+  children: OutlinerNode[],
+  attachments: Charm[]
+}
+
+
 declare global {
   namespace JSX {
     interface IntrinsicElements {
       [elemName: string]: any;
+      "ct-outliner": {
+              $value: Cell<{ root: OutlinerNode }>,
+              $mentionable: Cell<{"$NAME": string, "$UI": any }[]>
+              'oncharm-link-click'?: (e: { detail: { item: Cell<Charm> } }) => void,
+            } & Child;
     }
   }
 }
