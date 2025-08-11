d5360d7984bcc1ac03a0e626fb096bfbf02f904a
Ben Follington <5009316+bfollington@users.noreply.github.com>
2025-08-11 15:04:53 +1000

    Nested counter pattern

---
+++ packages/patterns/nested-counter.tsx
@@ -0,0 +1,75 @@
+/// <cts-enable />
+import {
+  Cell,
+  Default,
+  derive,
+  h,
+  handler,
+  NAME,
+  Opaque,
+  OpaqueRef,
+  recipe,
+  str,
+  UI,
+} from "commontools";
+
+interface RecipeState {
+  value: Default<number, 0>;
+}
+
+const increment = handler<unknown, { value: Cell<number> }>((_, state) => {
+  state.value.set(state.value.get() + 1);
+});
+
+const decrement = handler((_, state: { value: Cell<number> }) => {
+  state.value.set(state.value.get() - 1);
+});
+
+function previous(value: number) {
+  return value - 1;
+}
+
+function nth(value: number) {
+  if (value === 1) {
+    return "1st";
+  }
+  if (value === 2) {
+    return "2nd";
+  }
+  if (value === 3) {
+    return "3rd";
+  }
+  return `${value}th`;
+}
+
+export const Counter = recipe<RecipeState>("Counter", (state) => {
+  return {
+    [NAME]: str`Simple counter: ${state.value}`,
+    [UI]: (
+      <div>
+        <ct-button onClick={decrement(state)}>
+          dec to {previous(state.value)}
+        </ct-button>
+        <span id="counter-result">
+          Counter is the {nth(state.value)} number
+        </span>
+        <ct-button onClick={increment({ value: state.value })}>
+          inc to {state.value + 1}
+        </ct-button>
+      </div>
+    ),
+    value: state.value,
+  };
+});
+
+export default recipe<RecipeState>("Counter", (state) => {
+  return {
+    [NAME]: str`Double counter: ${state.value}`,
+    [UI]: (
+      <div>
+        <Counter value={state.value} />
+      </div>
+    ),
+    value: state.value,
+  };
+});
