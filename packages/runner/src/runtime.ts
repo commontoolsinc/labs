c899e2fa473c4a08e5715d23fb9ee175083a8728
Ben Follington <5009316+bfollington@users.noreply.github.com>
2025-08-13 12:08:46 +1000

    Restore change to runtime.ts

--- packages/runner/src/runtime.ts
+++ packages/runner/src/runtime.ts
@@ -50,7 +50,9 @@ import { RuntimeTelemetry } from "@commontools/runner";
 // @ts-ignore - This is temporary to debug integration test
 Error.stackTraceLimit = 500;
 
-const DEFAULT_USE_REAL_TRANSACTIONS = false;
+const DEFAULT_USE_REAL_TRANSACTIONS = isDeno()
+  ? !["1", "true", "on", "yes"].includes(Deno.env.get("USE_TRANSACTIONS_SHIM")!)
+  : true;
 
 export type { IExtendedStorageTransaction, IStorageProvider, MemorySpace };
 
