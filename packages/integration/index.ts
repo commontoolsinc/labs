9a71e07385fe133b9993973d5865997d0f284aa7
Jordan Santell <jordan@common.tools>
2025-08-26 10:24:40 -0700

    chore: Fail on error logs in browser integration tests, move waitFor to an integration utility

--- packages/integration/index.ts
+++ packages/integration/index.ts
@@ -1,3 +1,4 @@
 export { Browser } from "./browser.ts";
 export { dismissDialogs, Page, pipeConsole } from "./page.ts";
 export * as env from "./env.ts";
+export * from "./utils.ts";
--- packages/utils/src/sleep.ts
+++ packages/utils/src/sleep.ts
@@ -5,31 +5,3 @@
  */
 export const sleep = (timeout: number) =>
   new Promise((resolve) => setTimeout(resolve, timeout));
-
-/**
- * Receives an async predicate function to executed repeatedly
- * until either the predicate returns `true`, or throws once
- * the timeout limit has been reached.
- *
- * @param predicate - The predicate callback.
- * @param config.timeout - The number of milliseconds to wait before throwing. [60000]
- * @param config.delay - The number of milliseconds to wait between predicate calls. [500]
- */
-export const waitFor = async (
-  predicate: () => Promise<boolean>,
-  { timeout: _timeout, delay: _delay }: { timeout?: number; delay?: number } =
-    {},
-): Promise<void> => {
-  const timeout = _timeout ?? 60_000;
-  const delay = _delay ?? 500;
-  const end = Date.now() + timeout;
-  while (Date.now() < end) {
-    if ((await predicate())) {
-      return;
-    }
-    await sleep(delay);
-  }
-  throw new Error(
-    `Timeout: waitFor predicate could not complete after ${timeout}ms.`,
-  );
-};
