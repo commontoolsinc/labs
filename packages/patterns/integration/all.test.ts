5908c3812eafe72925b0f918305df41b0c181447
Jordan Santell <jsantell@users.noreply.github.com>
2025-09-03 09:27:30 -0700

    chore: Re-enable CI compilation tests for side effect patterns (#1716)

--- packages/patterns/integration/all.test.ts
+++ packages/patterns/integration/all.test.ts
@@ -7,21 +7,10 @@ import { Identity } from "@commontools/identity";
 
 const { API_URL, SPACE_NAME } = env;
 
-// TODO(CT-844): Re-enable these patterns once
-// we can submit charms without running them locally.
-const DISABLED_PATTERNS = (await Deno.readTextFile(
-  join(
-    import.meta.dirname!,
-    "..",
-    "DISABLED_PATTERNS.txt",
-  ),
-)).split("\n").map((p: string) => p.trim());
-
 describe("Compile all recipes", () => {
   for (const file of Deno.readDirSync(join(import.meta.dirname!, ".."))) {
     const { name } = file;
     if (!name.endsWith(".tsx")) continue;
-    if (DISABLED_PATTERNS.includes(name)) continue;
 
     let cc: CharmsController;
     let identity: Identity;
