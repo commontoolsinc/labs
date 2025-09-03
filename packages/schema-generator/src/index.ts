d038f522f30e3aaf273b55fa8c0109f055701f18
Gideon Wald <gideon.wald@gmail.com>
2025-09-03 14:04:47 -0700

    streamline exports and delete unuseful test file

--- packages/schema-generator/src/index.ts
+++ packages/schema-generator/src/index.ts
@@ -1,25 +1,9 @@
-// Main exports
+// Main API exports
 export { SchemaGenerator } from "./schema-generator.ts";
 export { createSchemaTransformerV2 } from "./plugin.ts";
 
-// Interface exports
+// Public types for API consumers
 export type {
-  FormatterContext,
   SchemaDefinition,
   SchemaGenerator as ISchemaGenerator,
-  TypeFormatter,
 } from "./interface.ts";
-
-// Utility exports
-export {
-  extractValueFromTypeNode,
-  getArrayElementInfo,
-  isDefaultTypeRef,
-  safeGetPropertyType,
-} from "./type-utils.ts";
-
-// Formatter exports
-export { PrimitiveFormatter } from "./formatters/primitive-formatter.ts";
-export { ObjectFormatter } from "./formatters/object-formatter.ts";
-export { ArrayFormatter } from "./formatters/array-formatter.ts";
-export { CommonToolsFormatter } from "./formatters/common-tools-formatter.ts";
