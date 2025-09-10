61049a0c54a914af7a1d4f835c04e5a08c5ab337
Jordan Santell <jordan@common.tools>
2025-09-10 15:10:32 -0700

    chore: Migrate @luca/esbuild-deno-loader to @deno/esbuild-plugin

--- scripts/bundle.ts
+++ scripts/bundle.ts
@@ -1,34 +1,25 @@
 #!/usr/bin/env -S deno run --allow-read --allow-write --allow-env --allow-run
 import * as path from "@std/path";
-import * as esbuild from "esbuild";
-import { denoPlugins } from "@luca/esbuild-deno-loader";
+import { build } from "@commontools/felt";
 
 if (Deno.args.length < 1) {
   console.error("[USAGE]: bundle.ts path/to/entry.ts [dist/output.js]");
   Deno.exit(1);
 }
 
-const entry = Deno.args[0];
-const out = Deno.args[1] ?? path.join(Deno.cwd(), "dist", "index.js");
+const input = Deno.args[0];
+const output = Deno.args[1] ?? path.join(Deno.cwd(), "dist", "index.js");
 
-console.log(`Bundling graph at ${entry} to ${out}...`);
+console.log(`Bundling graph at ${input} to ${output}...`);
 
-const result = await esbuild.build({
-  plugins: [...denoPlugins()],
-  entryPoints: [entry],
-  outfile: out,
-  bundle: true,
-  format: "esm",
-  // Explicitly compile decorators, as this what Jumble->Vite
-  // does, and no browsers currently support (any form of) decorators,
-  // and if we're bundling, we're probably running in a browser.
+await build({
+  entryPoints: [input],
+  outfile: output,
   tsconfigRaw: {
     compilerOptions: {
       experimentalDecorators: true,
     },
   },
 });
-
-esbuild.stop();
 
 console.log(`Successfully bundled!`);
