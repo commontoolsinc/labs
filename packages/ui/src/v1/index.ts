b407bbbe8b7da96478b45a6f55d16681b68c6658
Jordan Santell <jordan@common.tools>
2025-09-16 11:53:22 -0700

    chore: Remove Shoelace themes and its dependencies, including compromised @ctrl/tinycolor (though a previous version)

--- packages/ui/src/v1/index.ts
+++ packages/ui/src/v1/index.ts
@@ -1,5 +1,2 @@
 export * as components from "./components/index.ts";
 export * as style from "./components/style.ts";
-import { setupShoelace } from "./components/shoelace/index.ts";
-
-setupShoelace();
