d345e45fa333315eb991f4196705c11fa5e9e1a3
Bernhard Seefeld <berni@common.tools>
2025-10-01 16:00:33 -0700

    fmt fix

--- packages/patterns/note.tsx
+++ packages/patterns/note.tsx
@@ -102,7 +102,7 @@ const computeBacklinks = lift<
     if (!cs) return [];
 
     const results = [];
-    
+
     for (let i = 0; i < cs.length; i++) {
       const c = cs[i];
       if (c.mentioned?.some((m) => self.equals(allCharms.key(i)))) {
