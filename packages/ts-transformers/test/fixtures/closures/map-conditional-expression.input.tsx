a44415a70199ec52723978bdb3b6dc410e3c434f
Gideon Wald <gideon.wald@gmail.com>
2025-10-02 14:04:38 -0700

    final set of fixtures plus another resulting bug fix

---
+++ packages/ts-transformers/test/fixtures/closures/map-conditional-expression.input.tsx
@@ -0,0 +1,30 @@
+/// <cts-enable />
+import { h, recipe, UI } from "commontools";
+
+interface Item {
+  id: number;
+  price: number;
+}
+
+interface State {
+  items: Item[];
+  discount: number;
+  threshold: number;
+}
+
+export default recipe<State>("ConditionalExpression", (state) => {
+  return {
+    [UI]: (
+      <div>
+        {/* Ternary with captures in map callback */}
+        {state.items.map((item) => (
+          <div>
+            Price: ${item.price > state.threshold
+              ? item.price * (1 - state.discount)
+              : item.price}
+          </div>
+        ))}
+      </div>
+    ),
+  };
+});
