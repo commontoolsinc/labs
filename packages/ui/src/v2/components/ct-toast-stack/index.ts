444e14fa8238b3c3a4392af96627212582029efc
Ben Follington <5009316+bfollington@users.noreply.github.com>
2025-10-29 12:47:01 +1000

    Remove unused ct-toast

--- packages/ui/src/v2/components/ct-toast-stack/index.ts
+++
@@ -1,8 +0,0 @@
-import { CTToastStack } from "./ct-toast-stack.ts";
-
-if (!customElements.get("ct-toast-stack")) {
-  customElements.define("ct-toast-stack", CTToastStack);
-}
-
-export { CTToastStack };
-export type { ToastPosition } from "./ct-toast-stack.ts";
