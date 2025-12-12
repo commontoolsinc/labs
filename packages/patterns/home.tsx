aaabca58e31eacf75f59458ede01c7e652a76dea
Ben Follington <5009316+bfollington@users.noreply.github.com>
2025-12-12 12:22:34 +1000

    Render `favorites-manager.tsx` on `home.tsx` (#2259)

--- packages/patterns/home.tsx
+++ packages/patterns/home.tsx
@@ -1,13 +1,22 @@
 /// <cts-enable />
 import { NAME, pattern, UI } from "commontools";
+import FavoritesManager from "./favorites-manager.tsx";
 
 export default pattern((_) => {
+  const favorites = FavoritesManager({});
+
   return {
     [NAME]: `Home`,
     [UI]: (
-      <h1>
-        home<strong>space</strong>
-      </h1>
+      <ct-screen>
+        <h1>
+          home<strong>space</strong>
+        </h1>
+
+        <ct-card>
+          {favorites}
+        </ct-card>
+      </ct-screen>
     ),
   };
 });
