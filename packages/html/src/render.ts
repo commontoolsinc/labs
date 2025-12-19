c6c2d9863d80158ce9a75182fffa053070fa0b86
Robin McCollum <robin.mccollum@gmail.com>
2025-12-19 15:44:53 -0800

    Add some better type information to allCells in recipe.ts Change vdomSchema to use $defs so our references are more resilient to schema combination/slicing

--- packages/html/src/render.ts
+++ packages/html/src/render.ts
@@ -25,29 +25,34 @@ export interface RenderOptions {
 }
 
 export const vdomSchema: JSONSchema = {
-  type: "object",
-  properties: {
-    type: { type: "string" },
-    name: { type: "string" },
-    props: {
+  $defs: {
+    VNode: {
       type: "object",
-      additionalProperties: { asCell: true },
-    },
-    children: {
-      type: "array",
-      items: {
-        anyOf: [
-          { $ref: "#", asCell: true },
-          { type: "string", asCell: true },
-          { type: "number", asCell: true },
-          { type: "boolean", asCell: true },
-          { type: "array", items: { $ref: "#", asCell: true } },
-        ],
+      properties: {
+        type: { type: "string" },
+        name: { type: "string" },
+        props: {
+          type: "object",
+          additionalProperties: { asCell: true },
+        },
+        children: {
+          type: "array",
+          items: {
+            anyOf: [
+              { $ref: "#/$defs/VNode", asCell: true },
+              { type: "string", asCell: true },
+              { type: "number", asCell: true },
+              { type: "boolean", asCell: true },
+              { type: "array", items: { $ref: "#/$defs/VNode", asCell: true } },
+            ],
+          },
+          asCell: true,
+        },
+        [UI]: { $ref: "#/$defs/VNode" },
       },
-      asCell: true,
     },
-    [UI]: { $ref: "#" },
   },
+  $ref: "#/$defs/VNode",
 } as const;
 
 /**
