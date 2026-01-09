f6dbf0b8a467a3aaee41dd8be50d61a5d7cb018f
Ben Follington <5009316+bfollington@users.noreply.github.com>
2026-01-09 14:41:03 +1000

    Improve cross-shell support for globbing

--- docs/check.sh
+++ docs/check.sh
@@ -1,7 +1,8 @@
+#!/bin/sh
 cd "$(dirname "$0")"
 
 exit_code=0
-for file in **/*.md; do
+for file in $(find . -name '*.md'); do
   deno check --doc-only "$file" || exit_code=1
 done
 exit $exit_code
