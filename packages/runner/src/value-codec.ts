29b4e18f418da3ff100843a69e7e0d62709f5416
Dan Bornstein <danfuzz@murtbo.com>
2026-01-09 12:46:59 -0800

    Expand `toStorableValue()` to better handle non-encodable core types.

--- packages/runner/src/value-codec.ts
+++ packages/runner/src/value-codec.ts
@@ -13,15 +13,63 @@ import { isFunction, isInstance } from "@commontools/utils/types";
  * @throws Error if the value is a function/instance that can't be converted.
  */
 export function toStorableValue(value: unknown): unknown {
-  const valueIsFunction = isFunction(value);
-  const valueIsInstance = isInstance(value);
+  const typeName = typeof value;
 
-  if (!valueIsFunction && !valueIsInstance) {
-    return value;
+  switch (typeName) {
+    case "boolean":
+    case "string": {
+      return value;
+    }
+
+    case "number": {
+      if (Number.isFinite(value) && !Object.is(value, -0)) {
+        return value;
+      } else {
+        // TODO(@danfuzz): This is allowed for now, even though it isn't
+        // JSON-encodable, specifically because there is a test which explicitly
+        // expects `NaN` to become `null`. To be clear, this _does_ match the
+        // behavior of `JSON.stringify()`, however in the spirit of "Death
+        // before confusion!" I think the test in question should get tweaked,
+        // and then this clause should `throw`.
+        return value;
+        // throw new Error("Cannot store non-finite number or negative zero");
+      }
+    }
+
+    case "function": {
+      // Break out for the more involved work.
+      break;
+    }
+
+    case "object": {
+      if (!isInstance(value)) {
+        return value;
+      }
+
+      // Break out for the more involved work.
+      break;
+    }
+
+    // TODO(@danfuzz): This is allowed for now, even though it isn't
+    // JSON-encodable, specifically because many tests try to store `undefined`
+    // in `Cell`s. I believe the right answer is to stop doing that and then
+    // make this case be part of the error block below.
+    case "undefined": {
+      return undefined;
+      // throw new Error(`Cannot store ${typeName}`);
+    }
+
+    case "bigint":
+    case "symbol": {
+      throw new Error(`Cannot store ${typeName}`);
+    }
+
+    default: {
+      throw new Error(`Shouldn't happen: Unrecognized type ${typeName}`);
+    }
   }
 
-  const typeName = valueIsFunction ? "function" : "instance";
-  const valueObj = value as object; // Safe: guarded by isFunction || isInstance
+  const valueObj = value as object; // Safe, given the `switch` above.
 
   if (!("toJSON" in valueObj && typeof valueObj.toJSON === "function")) {
     throw new Error(
