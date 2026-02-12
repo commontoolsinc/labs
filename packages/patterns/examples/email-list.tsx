8cf33e7102c66a1c5afa628b4b7dca930f3fa224
Ben Follington <5009316+bfollington@users.noreply.github.com>
2026-02-13 09:40:06 +1000

    Example of email-list computed bug

--- packages/patterns/examples/email-list.tsx
+++ packages/patterns/examples/email-list.tsx
@@ -10,26 +10,40 @@ import {
 } from "commontools";
 import { Email } from "../google/core/gmail-importer.tsx";
 import { Contact } from "../contacts/contact-detail.tsx";
+import Suggestion from "../system/suggestion.tsx";
 
 export default pattern<Record<string, never>>((_) => {
-  const emailResult = wish<
-    (Email & { [UI]: VNode })
-  >({
+  const emailResult = wish<Email & { [UI]: VNode }>({
     query: "#email",
     scope: ["."],
   });
 
   const emails = emailResult.candidates;
 
-  const peopleResult = wish<
-    (Contact & { [NAME]: string })
-  >({
+  const peopleResult = wish<Contact & { [NAME]: string }>({
     query: "#person",
     scope: ["."],
   });
 
   const people = peopleResult.candidates;
   const selectedPerson = Writable.of<Contact | null>(null);
+  const emailString = computed(() => {
+    const result = [];
+    for (const email of emailResult.candidates) {
+      result.push(
+        `Subject: ${email.subject}\nFrom: ${email.from}\nBody: ${email.plainText}`,
+      );
+    }
+    return result;
+  });
+
+  const summary = Suggestion({
+    situation:
+      "Summarize the content of this email list to give the receiver a briefing",
+    context: {
+      emails: emailString,
+    },
+  });
 
   return {
     [NAME]: computed(() => "Email list (" + emails.length + ")"),
@@ -43,7 +57,11 @@ export default pattern<Record<string, never>>((_) => {
           $value={selectedPerson}
         />
         <hr />
+        {summary}
+        <hr />
         {emails}
+        <hr />
+        <pre>{emailString}</pre>
       </div>
     ),
   };
