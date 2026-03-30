6163ee7e45207d47fdcb3e5ffa445694516743ab
Dan Bornstein <danfuzz@murtbo.com>
2026-03-30 15:59:32 -0700

    Split out base64url handling, and use the web-standard functionality.

--- packages/data-model/json-type-handlers.ts
+++ packages/data-model/json-type-handlers.ts
@@ -23,10 +23,12 @@ import { FabricEpochDays, FabricEpochNsec } from "./fabric-epoch.ts";
 import { FabricBytes } from "./fabric-bytes.ts";
 import { TAGS } from "./fabric-type-tags.ts";
 import {
-  bigintFromMinimalTwosComplement,
-  bigintToMinimalTwosComplement,
   fromBase64url,
   toUnpaddedBase64url,
+} from "./base64url.ts";
+import {
+  bigintFromMinimalTwosComplement,
+  bigintToMinimalTwosComplement,
 } from "./bigint-encoding.ts";
 
 /**
