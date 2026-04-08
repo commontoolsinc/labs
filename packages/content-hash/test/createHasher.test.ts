e7c9b8194b4745f3d7b369a01438c7ec27466cad
Dan Bornstein <danfuzz@murtbo.com>
2026-04-08 12:41:20 -0700

    More tests.

--- packages/content-hash/test/createHasher.test.ts
+++ packages/content-hash/test/createHasher.test.ts
@@ -5,24 +5,35 @@ import { createHasher } from "@commonfabric/content-hash";
 import { FIXTURES } from "./fixtures.ts";
 
 describe("createHasher()", () => {
-  describe("one-shot action", () => {
-    let i = 1;
-    for (const { bytes, sha256: hashStr } of FIXTURES) {
-      const hashMsg = `${hashStr.slice(0, 8)}...`;
-      const hashBytes = fromBase64url(hashStr);
-      const testId = i++;
-      it(`produces expected string hash #${testId}: \`${hashMsg}...\``, () => {
+  let testId = -1;
+  for (const { bytes, sha256: hashStr } of FIXTURES) {
+    const hashMsg = `\`${hashStr.slice(0, 8)}...\``;
+    const hashBytes = fromBase64url(hashStr);
+    testId++;
+
+    describe(`for fixture #${testId}, hash ${hashMsg}`, () => {
+      it("one-shot use produces expected string hash", () => {
         const hasher = createHasher();
         hasher.update(bytes);
         const got = hasher.digest("base64url");
         expect(got).toBe(hashStr);
       });
-      it(`produces expected byte-array hash #${testId}}: \`${hashMsg}...\``, () => {
+
+      it("one-shot use produces expected byte-array hash", () => {
         const hasher = createHasher();
         hasher.update(bytes);
         const got = hasher.digest();
         expect(got).toEqual(hashBytes);
       });
-    }
-  });
+
+      it("byte-at-a-time use produces expected string hash", () => {
+        const hasher = createHasher();
+        for (let i = 0; i < bytes.length; i++) {
+          hasher.update(bytes.subarray(i, i + 1));
+        }
+        const got = hasher.digest();
+        expect(got).toEqual(hashBytes);
+      });
+    });
+  }
 });
