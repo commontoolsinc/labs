b7690e1f613836a9c181b7fdefe6c410b9d06ae8
Dan Bornstein <danfuzz@murtbo.com>
2026-04-08 12:52:28 -0700

    More tests.

--- packages/content-hash/test/createHasher.test.ts
+++ packages/content-hash/test/createHasher.test.ts
@@ -6,6 +6,8 @@ import { FIXTURES } from "./fixtures.ts";
 
 describe("createHasher()", () => {
   let testId = -1;
+  let oneLength = 10; // For multi-byte variety; updated pseudorandomly.
+
   for (const { bytes, sha256: hashStr } of FIXTURES) {
     const hashMsg = `\`${hashStr.slice(0, 8)}...\``;
     const hashBytes = fromBase64url(hashStr);
@@ -30,6 +32,19 @@ describe("createHasher()", () => {
         const hasher = createHasher();
         for (let i = 0; i < bytes.length; i++) {
           hasher.update(bytes.subarray(i, i + 1));
+        }
+        const got = hasher.digest();
+        expect(got).toEqual(hashBytes);
+      });
+
+      it("multi-byte variety use produces expected string hash", () => {
+        const hasher = createHasher();
+        let i = 0;
+        while (i < bytes.length) {
+          const someBytes = bytes.subarray(i, i + oneLength);
+          hasher.update(someBytes);
+          i += someBytes.length;
+          oneLength = ((oneLength + 7) * 1123) % (bytes.length - i + 1) + 1;
         }
         const got = hasher.digest();
         expect(got).toEqual(hashBytes);
