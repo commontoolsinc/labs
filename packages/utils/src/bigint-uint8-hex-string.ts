27627719087217727561900719aaf2e1687992ef
Dan Bornstein <danfuzz@murtbo.com>
2026-05-19 15:11:38 -0700

    Do the same small-length fast pass for the hex-string implementation.

--- packages/utils/src/bigint-uint8-hex-string.ts
+++ packages/utils/src/bigint-uint8-hex-string.ts
@@ -5,24 +5,99 @@
  * environments.
  */
 
-import { hexStringFromPositiveValue } from "./bigint-shared-impl.ts";
+import {
+  encode5To8Bytes,
+  hexStringFromPositiveValue,
+} from "./bigint-shared-impl.ts";
 
 /**
  * Version of `bigintToMinimalTwosComplement()` which uses the `Uint8Array`
  * hex-string methods.
  */
 export function bigintToMtcHex(value: bigint): Uint8Array {
-  if (value >= 0) {
+  if (value >= 0n) {
+    if (value <= 0x7fff_ffffn) {
+      const num = Number(value);
+      if (num <= 0x7fff) {
+        if (num <= 0x7f) {
+          const result = new Uint8Array(1);
+          result[0] = num;
+          return result;
+        } else {
+          const result = new Uint8Array(2);
+          result[0] = num >> 8;
+          result[1] = num;
+          return result;
+        }
+      } else {
+        if (num <= 0x7f_ffff) {
+          const result = new Uint8Array(3);
+          result[0] = num >> 16;
+          result[1] = num >> 8;
+          result[2] = num;
+          return result;
+        } else {
+          const result = new Uint8Array(4);
+          result[0] = num >> 24;
+          result[1] = num >> 16;
+          result[2] = num >> 8;
+          result[3] = num;
+          return result;
+        }
+      }
+    } else if (value <= 0x7fff_ffff_ffff_ffffn) {
+      return encode5To8Bytes(value, false);
+    }
+
+    // Slow path for positive numbers, using string manipulation.
+
     const hexString = hexStringFromPositiveValue(value);
     return Uint8Array.fromHex(hexString);
   } else {
-    // Negative value. We ones-complement it before conversion to string, then
-    // undo the conversion in the array result.
+    if (value >= -0x8000_0000n) {
+      const num = Number(value);
+      if (num >= -0x8000) {
+        if (num >= -0x80) {
+          const result = new Uint8Array(1);
+          result[0] = num;
+          return result;
+        } else {
+          const result = new Uint8Array(2);
+          result[0] = num >> 8;
+          result[1] = num;
+          return result;
+        }
+      } else {
+        if (num >= -0x80_0000) {
+          const result = new Uint8Array(3);
+          result[0] = num >> 16;
+          result[1] = num >> 8;
+          result[2] = num;
+          return result;
+        } else {
+          const result = new Uint8Array(4);
+          result[0] = num >> 24;
+          result[1] = num >> 16;
+          result[2] = num >> 8;
+          result[3] = num;
+          return result;
+        }
+      }
+    } else if (value >= -0x8000_0000_0000_0000n) {
+      return encode5To8Bytes(value, true);
+    }
+
+    // Slow path for negative numbers, using string manipulation. We
+    // ones-complement it before conversion to string, then undo the conversion
+    // in the array result.
+
     const hexString = hexStringFromPositiveValue(~value);
     const result = Uint8Array.fromHex(hexString);
+
     for (let i = 0; i < result.length; i++) {
       result[i] = ~result[i];
     }
+
     return result;
   }
 }
@@ -42,8 +117,8 @@ export function bigintFromMtcHex(bytes: Uint8Array): bigint {
   if (bytes[0] <= 0x7f) {
     return positiveResult;
   } else {
-    // Negative number. We need to make the positive result of conversion
-    // negative.
+    // Negative number. We make the initial positive result of conversion
+    // negative by OR-ing on the sign bit.
     const signBit = -1n << (BigInt(bytes.length) * 8n);
     return positiveResult | signBit;
   }
