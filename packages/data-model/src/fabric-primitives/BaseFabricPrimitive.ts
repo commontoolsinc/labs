4ea3de8aa99502767b696b012f13296412df47d5
Dan Bornstein <danfuzz@murtbo.com>
2026-06-04 16:02:48 -0700

    Add `wireTypeTagOf()`.
    This is very un-DRY, mostly a duplicate of the same method in
    `BaseFabricInstance`.

--- packages/data-model/src/fabric-primitives/BaseFabricPrimitive.ts
+++ packages/data-model/src/fabric-primitives/BaseFabricPrimitive.ts
@@ -8,9 +8,41 @@ import { FabricPrimitive } from "../interface.ts";
  * is where shared implementation lives.
  */
 export abstract class BaseFabricPrimitive extends FabricPrimitive {
+  //
+  // Instance members
+  //
+
   /**
    * The type tag to use for this instance to identify it in wire protocols
    * for which instances of this type do not have a protocol-specific form.
    */
   abstract get wireTypeTag(): string;
+
+  //
+  // Static members
+  //
+
+  /**
+   * Gets the `.wireTypeTag` from a value which is _supposed_ to be an instance
+   * of this class but is only statically known / assumed to be an instance of
+   * the fully abstract `FabricPrimitive`. Throws a "shouldn't happen" error if
+   * there's trouble.
+   */
+  static wireTypeTagOf(value: FabricPrimitive): string {
+    if (!(value instanceof BaseFabricPrimitive)) {
+      throw new Error(
+        "Shouldn't happen: Encountered a `FabricPrimitive` which is not a `BaseFabricPrimitive`.",
+      );
+    }
+
+    const result = value.wireTypeTag;
+
+    if (typeof result !== "string") {
+      throw new Error(
+        "Shouldn't happen: Encountered a `FabricPrimitive` with a non-string `wireTypeTag`.",
+      );
+    }
+
+    return result;
+  }
 }
