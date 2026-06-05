492557ab0910217b1713444de3109c5d5545543b
Dan Bornstein <danfuzz@murtbo.com>
2026-06-05 11:38:22 -0700

    Appease the auto-formatter.

--- packages/data-model/test/EmptyReconstructionContext.test.ts
+++ packages/data-model/test/EmptyReconstructionContext.test.ts
@@ -8,8 +8,9 @@ import {
 describe("EmptyReconstructionContext", () => {
   describe("EMPTY_RECONSTRUCTION_CONTEXT", () => {
     it("is a singleton (re-import yields the same instance)", async () => {
-      const reimported = (await import("../src/wire-common/EmptyReconstructionContext.ts"))
-        .EMPTY_RECONSTRUCTION_CONTEXT;
+      const reimported =
+        (await import("../src/wire-common/EmptyReconstructionContext.ts"))
+          .EMPTY_RECONSTRUCTION_CONTEXT;
       expect(reimported).toBe(EMPTY_RECONSTRUCTION_CONTEXT);
     });
 
