df549516c8594678acbc2fff7585c456391ff7de
Dan Bornstein <danfuzz@murtbo.com>
2026-06-05 10:40:18 -0700

    Barrel `export` of `wire-common`.

---
+++ packages/data-model/src/wire-common/index.ts
@@ -0,0 +1,12 @@
+export {
+  DECONSTRUCT,
+  RECONSTRUCT,
+  type FabricDeconstructable,
+  type FabricClass,
+  type FabricValueConverter,
+  type ReconstructionContext,
+  type SerializationContext,
+} from "./interface.ts";
+
+export { WIRE_META_TAGS } from "./wire-meta-tags.ts";
+export { WIRE_TYPE_TAGS } from "./wire-type-tags.ts";
