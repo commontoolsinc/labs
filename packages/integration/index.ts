bd03de3a38be238176f172fcfb3fa9f98c284b55
Bernhard Seefeld <berni@common.tools>
2026-07-12 19:44:47 -0700

    fix(integration): export CPU profile summaries

--- packages/integration/index.ts
+++ packages/integration/index.ts
@@ -1,5 +1,11 @@
 export { Browser } from "./browser.ts";
-export { CdpWorkerProfiler, renderProfileReport } from "./cdp-profiler.ts";
+export {
+  CdpWorkerProfiler,
+  type CPUProfile,
+  type CPUProfileSummary,
+  renderProfileReport,
+  summarizeCPUProfile,
+} from "./cdp-profiler.ts";
 export { dismissDialogs, Page, pipeConsole } from "./page.ts";
 export * from "./presentation/mod.ts";
 export * as env from "./env.ts";
