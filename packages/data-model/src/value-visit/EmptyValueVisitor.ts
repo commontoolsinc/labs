cb5c9f52e4febf888abe2ef841eb1d0d4c318fce
Dan Bornstein <danfuzz@murtbo.com>
2026-09-11 15:06:00 -0700

    Switch polarity.

--- packages/data-model/src/value-visit/EmptyValueVisitor.ts
+++ packages/data-model/src/value-visit/EmptyValueVisitor.ts
@@ -21,14 +21,15 @@ import {
 /**
  * No-op (empty implementation) of `ValueVisitor`: Every method is implemented
  * and just returns `undefined`, except for `isDomainExtra()` which returns
- * `true`. This is meant to be a reasonable base implementation for more useful
- * visitors, not to be particularly useful by itself.
+ * `false` (the safe choice which also aligns with the default binding for
+ * `DomainExtra`). This is meant to be a reasonable base implementation for more
+ * useful visitors, not to be particularly useful by itself.
  */
 export class EmptyValueVisitor<DomainExtra = never, ResultType = FabricValue>
   extends BaseValueVisitor<DomainExtra, ResultType> {
   /** @inheritDoc */
   isDomainExtra(_value: DomainFor<DomainExtra>): _value is DomainExtra {
-    return true;
+    return false;
   }
 
   /** @inheritDoc */
