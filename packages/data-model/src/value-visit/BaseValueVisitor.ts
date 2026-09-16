6385a66544501e2bf717348aed80a1b44d2e630f
Dan Bornstein <danfuzz@murtbo.com>
2026-09-16 16:16:59 -0700

    Appease the auto-formatter.

--- packages/data-model/src/value-visit/BaseValueVisitor.ts
+++ packages/data-model/src/value-visit/BaseValueVisitor.ts
@@ -117,7 +117,7 @@ export abstract class BaseValueVisitor<
 
   /** @inheritDoc */
   visitedFabricArrayElement(
-   _array: FabricArrayPlus<PlusType>,
+    _array: FabricArrayPlus<PlusType>,
     _index: number,
     _value: FabricValuePlus<PlusType>,
   ): BaselineVisitResult<ResultType> {
