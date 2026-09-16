e050fff7f94b256b576c9c336e19a00b36bd397d
Dan Bornstein <danfuzz@murtbo.com>
2026-09-16 12:14:51 -0700

    Widen `value` to `unknown`, and explain a bit.

--- packages/data-model/src/types/interface.ts
+++ packages/data-model/src/types/interface.ts
@@ -1,10 +1,18 @@
 import type { FabricValuePlusLayer } from "@/interface.ts";
 
 /**
- * Type predicate function used to determine if a presumed-valid value of type
- * `FabricValuePlus` or `FabricValuePlusLayer` of a given `PlusType` is in fact
- * considered to be the `PlusType`.
+ * Type predicate function used to determine if an arbitrary value is in fact
+ * considered to be the `PlusType`. The `data-model` generally aims to only ever
+ * pass `value`s that it receives as being typed to be in the `FabricValuePlus`
+ * family, narrowed to the same `PlusType`, and only in cases where a shallow
+ * inspection of the value indicates that it is not possibly a valid
+ * `FabricValue` or `FabricValueLayer`, or the _container_ options of
+ * `FabricValuePlus` or `FabricValuePlusLayer`. That said, by the time a
+ * function of this type is called, there is an open question as to what the
+ * actual type is, and it should be prepared to deal with a "typesystem lie" of
+ * some sort, hence `value` is typed as `unknown` instead of a more-specific
+ * `FabricValue`-ish type.
  */
 export type PlusTypePredicate<PlusType> = (
-  value: FabricValuePlusLayer<PlusType>,
+  value: unknown,
 ) => value is PlusType;
