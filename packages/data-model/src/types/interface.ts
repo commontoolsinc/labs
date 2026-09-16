8c787a05313a90a9ecf0b0fc56922637195398e4
Dan Bornstein <danfuzz@murtbo.com>
2026-09-16 09:28:13 -0700

    Expand to cover layers.

--- packages/data-model/src/types/interface.ts
+++ packages/data-model/src/types/interface.ts
@@ -1,9 +1,9 @@
-import type { FabricValuePlus } from "@/interface.ts";
+import type { FabricValuePlusLayer } from "@/interface.ts";
 
 /**
- * Type predicate function used to determine if a presumed-valid value is
- * considered to be a `PlusType`, in the context of the `FabricValuePlus` family
- * of types.
+ * Type predicate function used to determine if a presumed-valid value of type
+ * `FabricValuePlus` or `FabricValuePlusLayer` of a given `PlusType` is in fact
+ * considered to be the `PlusType`.
  */
 export type PlusTypePredicate<PlusType> =
-  (value: FabricValuePlus<PlusType>) => value is PlusType;
+  (value: FabricValuePlusLayer<PlusType>) => value is PlusType;
